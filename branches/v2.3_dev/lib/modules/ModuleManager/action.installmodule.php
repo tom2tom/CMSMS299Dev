fd48ec0a4b1c9377edac1b74aec7c8bd320a078f
calguy1000 <calguy1000@3d254a34-79dc-0310-9e5f-be208747d8a0>
2017-08-25 23:14:26 +0000

    remove debug statement.

--- branches/v2.3_dev/lib/modules/ModuleManager/action.installmodule.php
+++ branches/v2.3_dev/lib/modules/ModuleManager/action.installmodule.php
@@ -58,7 +58,6 @@ try {
     if( isset($params['submit']) ) {
         // phase one... organize and download
         set_time_limit(9999);
-        echo 'DEBUG: downloading...<br/>';
         if( isset($params['modlist']) && $params['modlist'] != '' ) {
             $modlist = unserialize(base64_decode($params['modlist']));
             if( !is_array($modlist) || count($modlist) == 0 ) throw new CmsInvalidDataException( $this->Lang('error_missingparams') );
