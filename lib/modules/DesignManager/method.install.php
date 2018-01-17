48f4284ba4e53c55375cfd8008c04e876279198a
tom2tom <tpgww@onepost.net>
2018-01-17 19:22:02 +1100

    create module-related permissions create 'Designer' group

--- lib/modules/DesignManager/method.install.php
+++ lib/modules/DesignManager/method.install.php
@@ -1,7 +1,7 @@
 <?php
 #-------------------------------------------------------------------------
-# Module: AdminSearch - A CMSMS addon module to provide template management.
-# (c) 2012 by Robert Campbell <calguy1000@cmsmadesimple.org>
+# DesignManager - A CMSMS module to provide template management.
+# Copyright (C) 2012-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,16 +14,37 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
-#
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# Or read it online, at https://www.gnu.org/licenses/gpl-2.0.html
 #-------------------------------------------------------------------------
-if( !isset($gCms) ) exit;
+if (!isset($gCms)) {
+    exit;
+}
 
-$this->SetPreference('lock_timeout',60);
-$this->SetPreference('lock_refresh',120);
+$this->SetPreference('lock_timeout', 60);
+$this->SetPreference('lock_refresh', 120);
 
-#
-# EOF
-#
-?>
+$this->CreatePermission('Add Templates', $this->Lang('perm_add'));
+$this->CreatePermission('Manage Designs', $this->Lang('perm_designs'));
+$this->CreatePermission('Manage Stylesheets', $this->Lang('perm_styles'));
+$this->CreatePermission('Modify Templates', $this->Lang('perm_modify'));
+$this->CreatePermission('View Tag Help', $this->Lang('perm_viewhelp'));
+
+$designer_group = new Group();
+$designer_group->name = 'Designer';
+$designer_group->description = $this->Lang('group_desc');
+$designer_group->active = 1;
+CMSMS\HookManager::do_hook('Core::AddGroupPre', ['group'=>&$designer_group]);
+$designer_group->Save();
+CMSMS\HookManager::do_hook('Core::AddGroupPost', ['group'=>&$designer_group]);
+
+$designer_group->GrantPermission('Add Templates');
+$designer_group->GrantPermission('Manage All Content'); //CHECKME ContentManager race when 1st installing?
+$designer_group->GrantPermission('Manage Designs');
+$designer_group->GrantPermission('Manage My Account');
+$designer_group->GrantPermission('Manage My Bookmarks');
+$designer_group->GrantPermission('Manage My Settings');
+$designer_group->GrantPermission('Manage Stylesheets');
+$designer_group->GrantPermission('Modify Files');
+$designer_group->GrantPermission('Modify Templates');
+
