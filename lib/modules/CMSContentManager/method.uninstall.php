d4f0d77362f628f3def640ba81956f93380a3993
tom2tom <tpgww@onepost.net>
2018-01-17 19:33:25 +1100

    remove permissions & Editor group

---
+++ lib/modules/CMSContentManager/method.uninstall.php
@@ -0,0 +1,41 @@
+<?php
+#-------------------------------------------------------------------------
+# CMSContentManager - A CMSMS module to provide page-content management.
+# Copyright (C) 2013-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# Or read it online, at https://www.gnu.org/licenses/gpl-2.0.html
+#-------------------------------------------------------------------------
+if (!isset($gCms)) {
+    exit;
+}
+
+$group = new Group();
+$group->name = 'Editor';
+try {
+    CMSMS\HookManager::do_hook('Core::DeleteGroupPre', ['group' => &$group]);
+    if ($group->Delete()) {
+        CMSMS\HookManager::do_hook('Core::DeleteGroupPost', ['group' => &$group]);
+    }
+} catch (Exception $e) {
+}
+
+$this->RemovePreference();
+
+$this->RemovePermission('Add Pages');
+$this->RemovePermission('Manage All Content');
+$this->RemovePermission('Modify Any Page');
+$this->RemovePermission('Remove Pages');
+$this->RemovePermission('Reorder Content');
+
