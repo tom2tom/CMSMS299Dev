300e7e9060ffd4bf85a1417e353b6f5b665e2137
tom2tom <tpgww@onepost.net>
2018-01-17 19:22:49 +1100

    remove permissions & Designer group

--- lib/modules/DesignManager/method.uninstall.php
+++ lib/modules/DesignManager/method.uninstall.php
@@ -1,4 +1,42 @@
 <?php
-if( !isset($gCms) ) exit;
+#-------------------------------------------------------------------------
+# DesignManager - A CMSMS module to provide template management.
+# Copyright (C) 2012-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# Or read it online, at https://www.gnu.org/licenses/gpl-2.0.html
+#-------------------------------------------------------------------------
+if (!isset($gCms)) {
+    exit;
+}
 
-?>
+$group = new Group();
+$group->name = 'Designer';
+try {
+    CMSMS\HookManager::do_hook('Core::DeleteGroupPre', ['group'=>&$group]);
+    if ($group->Delete()) {
+        CMSMS\HookManager::do_hook('Core::DeleteGroupPost', ['group'=>&$group]);
+    }
+} catch (Exception $e) {
+    return 2;
+}
+
+$this->RemovePreference();
+
+$this->RemovePermission('Add Templates');
+$this->RemovePermission('Manage Designs');
+$this->RemovePermission('Manage Stylesheets');
+$this->RemovePermission('Modify Templates');
+$this->RemovePermission('View Tag Help');
+
