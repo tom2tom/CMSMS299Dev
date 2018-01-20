1143be0c13fbd194e88583eb515e20372dc389eb
tom2tom <tpgww@onepost.net>
2018-01-20 18:50:25 +1100

    add script for updating licences

---
+++ scripts/update_licences.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# script to update copyright-year strings in .php source files
+# uses sed & gawk, scans the first 5 lines of relevant files
+
+#this is where the things are executed from, must be parent of twigs
+SHAREDROOT='..'
+#dirs to scan
+#TWIGS="lib admin assets uploads tests phar_installer"
+TWIGS="admin"
+#TWIGS="tests"
+
+THISYEAR=$(date +%04Y)
+KEYYEAR=$(date +%-y)
+LASTYEAR=$((${KEYYEAR}-1))
+if [ "$LASTYEAR" -lt "10" ]; then
+ PATTERN=0[0-$LASTYEAR]
+else
+ LASTTEN=$((${LASTYEAR}/10))
+ if [ "$LASTTEN" -gt "1" ]; then
+  LASTTEN="[1-$LASTTEN]"
+ fi
+ LASTONE=$((${LASTYEAR}%10))
+ if [ "$LASTONE" -gt "0" ]; then
+  LASTONE="[0-$LASTONE]"
+ fi
+ PATTERN="0[0-9]\\|$LASTTEN$LASTONE"
+fi
+
+KILLARG='/^#.*as a special exception.*$/,/^#\s*$/d'
+
+PATTERN1="^\\(#\s*\\)CMS - CMS Made Simple"
+REPL1="\1This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>"
+SEDARG1="1,5s~$PATTERN1~$REPL1~"
+
+LONGPATTERN="^\\(\s*#\?\s*\\)[(][Cc][)].*20\\($PATTERN\\)-20\\($PATTERN\\).*Ted Kulp.*"
+SHORTPATTERN="^\\(\s*#\?\s*\\)[(][Cc][)].*20\\($PATTERN\\).*Ted Kulp.*"
+#echo "short = $SHORTPATTERN"
+#echo "long = $LONGPATTERN"
+SEDARG2A="1,5s~$LONGPATTERN~\1Copyright (C) 20\2-20\3 Ted Kulp <ted@cmsmadesimple.org>~"
+SEDARG2B="1,5s~$SHORTPATTERN~\1Copyright (C) 20\2-$THISYEAR Ted Kulp <ted@cmsmadesimple.org>~"
+#echo -e "sed patterns =\n$SEDARG2A\n$SEDARG2B\n"
+
+LONGPATTERN2="^\\(\s*#\?\s*\\)[(][Cc][)].*20\\($PATTERN\\)-20\\($PATTERN\\).*[tT]he\s\+CMSMS\s\+Dev\\(elopment\\)\?\s\+Team.*"
+SHORTPATTERN2="^\\(\s*#\?\s*\\)[(][Cc][)].*20\\($PATTERN\\).*[tT]he\s\+CMSMS\s\+Dev\\(elopment\\)\?\s\+Team.*"
+
+SEDARG3A="1,6s~$LONGPATTERN2~\1Copyright (C) 20\2-$THISYEAR The CMSMS Dev Team <@cmsmadesimple.org>~"
+SEDARG3B="1,6s~$SHORTPATTERN2~\1Copyright (C) 20\2-$THISYEAR The CMSMS Dev Team <@cmsmadesimple.org>~"
+#echo -e "sed patterns =\n$SEDARG3A\n$SEDARG3B\n"
+
+PATTERN5="^#\s*Visit our homepage at: http:\/\/www.cmsmadesimple.org"
+#REPL5="#"
+SEDARG5="1,10{/$PATTERN5/d;}"
+
+PATTERN8="^\\(\s*#\?\s*\\)Foundation,\s\+Inc.,\s\+59\s\+Temple\s\+Place,\s\+Suite 330,\s\+Boston,\s\+MA\s\+02111-1307\s\+USA"
+REPL8="\1Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.\n\1Or read it online, at https://www.gnu.org/licenses/gpl-2.0.html"
+SEDARG8="1,100s~$PATTERN8~$REPL8~"
+
+cd $SHAREDROOT
+SHAREDROOT=$(pwd)
+echo "execute from $SHAREDROOT"
+WORKFILE=./fileyears.txt
+rm -f $WORKFILE
+
+echo "process files in scanned dirs = $TWIGS"
+# update the array usages
+for dir in $TWIGS; do
+  LEAVES=$(find -L $dir -type f -name \*.php -not -wholename \*svn\* -not -wholename \*git\* -exec echo -n "{} " \;)
+  for LEAF in $LEAVES; do
+
+# produce a 5-field description like -rw-rw-r-- 1   9108 2007 ACL-description.txt
+# of which we use only the year
+    LONGTEXT=$(ls -Gg --time-style=+%04Y $LEAF)
+    MODYEAR=$(echo $LONGTEXT | gawk '{ print $4 }')
+#    if [ "$MODYEAR" -lt "$THISYEAR" ]; then
+#      echo "$LEAF is old"
+#    else
+    if [ "$MODYEAR" -ge "$THISYEAR" ]; then
+#      echo "$LEAF needs to be checked"
+      rm -f  $LEAF-newyear >/dev/null
+      sed -e "$SEDARG1" -e "$SEDARG2A" -e "$SEDARG2B" -e "$SEDARG3A" -e "$SEDARG3B" -e "$KILLARG" -e "$SEDARG5" -e "$SEDARG8" $LEAF > $LEAF-newyear
+      OLDCS=$(md5sum $LEAF | gawk '{ print $1 }')
+      NEWCS=$(md5sum $LEAF-newyear | gawk '{ print $1 }')
+      if [ "$OLDCS" = "$NEWCS" ] ; then
+        echo "stet $LEAF"
+        rm -f  $LEAF-newyear >/dev/null
+      else
+#        mv -f $LEAF $LEAF-old >/dev/null
+        mv -f $LEAF-newyear $LEAF >/dev/null
+        echo "$LEAF has been updated"
+      fi
+    fi
+  done
+done
+
+rm -f $WORKFILE
