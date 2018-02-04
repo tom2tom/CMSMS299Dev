3c355f4b12d56e806f590aa5ff12abdc51b93b9a
tom2tom <tpgww@onepost.net>
2018-02-04 22:43:30 +1100

    concept tester - forward compatibility

--- lib/autoloader.php
+++ lib/autoloader.php
@@ -23,6 +23,13 @@
  */
 
 /**
+ * @ignore
+ * @ since 2.3 spaced and/or renamed 'core' CMSMS classes
+ * @ deprecated, remove this in a decade or so ...
+ */
+static $class_replaces = null;
+
+/**
  * A function for auto-loading classes.
  *
  * @since 1.7
@@ -32,6 +39,8 @@
  */
 function cms_autoloader(string $classname)
 {
+	global $class_replaces;
+
 	$root = CMS_ROOT_PATH.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR;
 
 	// standard content types (prioritized)
@@ -41,11 +50,68 @@ function cms_autoloader(string $classname)
 		return;
 	}
 
+	if ($class_replaces === null) {
+		$class_replaces = [
+'User' => 'CMSMS\CmsUser',
+/*
+'cms_admin_tabs' => 'CMSMS\CmsAdminTabs',
+'cms_admin_utils' => 'CMSMS\CmsAdminUtils2',
+'cms_cache_driver' => 'CMSMS\CmsCacheDriver',
+'cms_cache_handler' => 'CMSMS\CmsCacheHandler',
+'cms_config' => 'CMSMS\CmsConfig',
+'cms_content_tree' => 'CMSMS\CmsContentTree',
+'cms_cookies' => 'CMSMS\CmsCookies',
+'cms_filecache_driver' => 'CMSMS\CmsFilecacheDriver',
+'cms_http_request' => 'CMSMS\CmsHttpRequest',
+'cms_mailer' => 'CMSMS\CmsMailer',
+'cms_module_smarty_plugin_manager' => 'CMSMS\CmsModulePluginManager',
+'cms_route_manager' => 'CMSMS\CmsRouteManager',
+'cms_siteprefs' => 'CMSMS\CmsSiteprefs',
+'cms_tree' => 'CMSMS\CmsTree',
+'cms_tree_operations' => 'CMSMS\CmsTreeOperations',
+'cms_url' => 'CMSMS\CmsUrl',
+'cms_userprefs' => 'CMSMS\CmsUserprefs',
+'cms_utils' => 'CMSMS\CmsUtils',
+//'lowercase'
+'Bookmark' => 'CMSMS\CmsBookmark',
+'BookmarkOperations' => 'CMSMS\CmsBookmarkOperations',
+'ContentOperations' => 'CMSMS\CmsContentOperations',
+'Events'=> 'CMSMS\CmsEvents',
+'Group' => 'CMSMS\CmsGroup',
+'GroupOperations' => 'CMSMS\CmsGroupOperations',
+'ModuleOperations' => 'CMSMS\CmsModuleOperations',
+'User' => 'CMSMS\CmsUser',
+'UserOperations' => 'CMSMS\CmsUserOperations',
+'UserTagOperations' => 'CMSMS\CmsUserTagOperations',
+//mebbe not these
+'CmsApp' => 'CMSMS\CmsApp',
+'CMSModule' => 'CMSMS\CmsModule',
+'CMSModuleContentType' => 'CMSMS\CmsModuleContentType',
+//rename only
+'simple_plugin_operations' => 'CmsSimplePluginOperations', //new class, all uses changed
+'FileType' => 'CmsFileType',
+'FileTypeHelper' => 'CmsFileTypeHelper',
+'FilePickerProfile' => 'CmsFilePickerProfile'
+'HookManager' => 'CmsHookManager',
+'AuditManager' => 'CmsAuditManager', //new class, all uses changed
+*/
+		];
+	}
+
 	$o = ($classname[0] != '\\') ? 0 : 1;
 	$p = strpos($classname, '\\', $o + 1);
 	if ($p !== false) {
 		$space = substr($classname, $o, $p - $o);
 		if ($space == 'CMSMS') {
+			$path = substr($classname, $o); //ignore any leading \
+			$old = array_search($path, $class_replaces);
+			if ($old !== false) {
+				$fp = $root.'class.'.$old.'.php';
+				require_once $fp;
+				class_alias($old, $class_replaces[$old], false);
+				unset($class_replaces[$old]); //no repeats
+				return;
+			}
 			$sroot = $root;
 		} else {
 			//CHECKME module supposed to be loaded, if a related class is used !? if lazy ??
@@ -117,6 +183,12 @@ function cms_autoloader(string $classname)
 	// standard classes
 	$fp = $root.'class.'.$classname.'.php';
 	if (is_file($fp)) {
+		if (array_key_exists($classname, $class_replaces)) {
+			require_once $fp;
+			class_alias($classname, $class_replaces[$classname], false);
+			unset($class_replaces[$classname]); //no repeats
+			return;
+		}
 		require_once $fp;
 		return;
 	}
@@ -175,80 +247,5 @@ function cms_autoloader(string $classname)
 	}
 }
 
-/*
-/ *
-@ since 2.3 spaced and/or renamed 'core' CMSMS classes
-@ deprecated, remove this in a decade or so ...
-* /
-function cms_deprecated_autoloader(string $classname)
-{
-	static $class_replaces = null;
-
-	if ($class_replaces == null) {
-		$class_replaces = [
-		'cms_admin_tabs' => '\CMSMS\CmsAdminTabs',
-		'cms_admin_utils' => '\CMSMS\CmsAdminUtils2',
-		'cms_cache_driver' => '\CMSMS\CmsCacheDriver',
-		'cms_cache_handler' => '\CMSMS\CmsCacheHandler',
-		'cms_config' => '\CMSMS\CmsConfig',
-		'cms_content_tree' => '\CMSMS\CmsContentTree',
-		'cms_cookies' => '\CMSMS\CmsCookies',
-		'cms_filecache_driver' => '\CMSMS\CmsFilecacheDriver',
-		'cms_http_request' => '\CMSMS\CmsHttpRequest',
-		'cms_mailer' => '\CMSMS\CmsMailer',
-		'cms_module_smarty_plugin_manager' => '\CMSMS\CmsModulePluginManager',
-		'cms_route_manager' => '\CMSMS\CmsRouteManager',
-		'cms_siteprefs' => '\CMSMS\CmsSiteprefs',
-		'cms_tree' => '\CMSMS\CmsTree',
-		'cms_tree_operations' => '\CMSMS\CmsTreeOperations',
-		'cms_url' => '\CMSMS\CmsUrl',
-		'cms_userprefs' => '\CMSMS\CmsUserprefs',
-		'cms_utils' => '\CMSMS\CmsUtils',
-		'Bookmark' => '\CMSMS\CmsBookmark',
-		'BookmarkOperations' => '\CMSMS\CmsBookmarkOperations',
-		'ContentOperations' => '\CMSMS\CmsContentOperations',
-		'Events'=> '\CMSMS\CmsEvents',
-		'Group' => '\CMSMS\CmsGroup',
-		'GroupOperations' => '\CMSMS\CmsGroupOperations',
-		'ModuleOperations' => '\CMSMS\CmsModuleOperations',
-		'User' => '\CMSMS\CmsUser',
-		'UserOperations' => '\CMSMS\CmsUserOperations',
-		'UserTagOperations' => '\CMSMS\CmsUserTagOperations',
-/ *
-		'CmsApp' => '\CMSMS\CmsApp',
-		'CMSModule' => '\CMSMS\CmsModule',
-		'CMSModuleContentType' => '\CMSMS\CmsModuleContentType',
-		 rename only
-		'simple_plugin_operations' => 'CmsSimplePluginOperations' new class, all uses changed
-		'FileType' => 'CmsFileType'
-		'FileTypeHelper' => 'CmsFileTypeHelper'
-		'FilePickerProfile' => 'CmsFilePickerProfile'
-		'HookManager' => 'CmsHookManager'
-		'AuditManager' => 'CmsAuditManager' new class, all uses changed
-* /
-		];
-	}
-
-	if (array_key_exists($classname, $class_replaces)) {
-		$nowclass = $class_replaces[$classname];
-		$path = substr($nowclass, 7);
-		$class = basename($path);
-		$path = dirname($path);
-
-		$fp = CMS_ROOT_PATH.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR;
-		if ($path != '.') {
-			$fp .= $path.DIRECTORY_SEPARATOR;
-		}
-		$fp .= 'class.'.$class.'.php';
-		if (is_file($fp)) {
-			require_once $fp;
-			class_alias($nowclass, $classname, true);
-			return;
-		}
-	}
-}
-*/
-
 spl_autoload_register('cms_autoloader');
-//spl_autoload_register('cms_deprecated_autoloader');
 
