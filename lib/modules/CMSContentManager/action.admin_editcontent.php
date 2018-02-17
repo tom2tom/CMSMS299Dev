9e524450c45844e051c82f055a17b7348aae9881
tom2tom <tpgww@onepost.net>
2018-02-17 17:24:41 +1100

    top-n-tail license text

--- lib/modules/CMSContentManager/action.admin_editcontent.php
+++ lib/modules/CMSContentManager/action.admin_editcontent.php
@@ -1,30 +1,21 @@
 <?php
-#BEGIN_LICENSE
-#-------------------------------------------------------------------------
-# Module: CMSContentManager (c) 2013 by Robert Campbell
-#         (calguy1000@cmsmadesimple.org)
-#  A module for managing content in CMSMS.
+#module action: edit page content
+#Copyright (C) 2013-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+#This file is a component of CMS Made Simple module CMSContentManager
+#  <http://dev.cmsmadesimple.org>
 #
-#-------------------------------------------------------------------------
-# CMS - CMS Made Simple is (c) 2004 by Ted Kulp (wishy@cmsmadesimple.org)
-# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
 #
-#-------------------------------------------------------------------------
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-#-------------------------------------------------------------------------
-#END_LICENSE
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 if( !isset($gCms) ) exit;
 
 $this->SetCurrentTab('pages');
