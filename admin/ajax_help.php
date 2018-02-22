e4e20559915e281e6d10a11d0d46f96dccbfa17b
tom2tom <tpgww@onepost.net>
2018-02-22 23:18:31 +1100

    cleanups

--- admin/ajax_help.php
+++ admin/ajax_help.php
@@ -1,5 +1,5 @@
 <?php
-#...
+#runtime help-string getter
 #Copyright (C) 2004-2018 Ted Kulp <ted@cmsmadesimple.org>
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
@@ -14,28 +14,20 @@
 #GNU General Public License for more details.
 #You should have received a copy of the GNU General Public License
 #along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-#$Id$
 
 $CMS_ADMIN_PAGE=1;
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'include.php';
-$urlext='?'.CMS_SECURE_PARAM_NAME.'='.$_SESSION[CMS_USER_KEY];
-check_login();
+//$urlext='?'.CMS_SECURE_PARAM_NAME.'='.$_SESSION[CMS_USER_KEY];
+//check_login();
 
-$realm = 'admin';
-$key = 'help';
-$out = 'NO HELP KEY SPECIFIED';
-if( isset($_GET['key']) ) $key = filter_var(trim($_GET['key']),FILTER_SANITIZE_STRING);
+$key = ( isset($_GET['key']) ) ? filter_var(trim($_GET['key']),FILTER_SANITIZE_STRING) : 'help';
 if( strstr($key,'__') !== FALSE ) {
   list($realm,$key) = explode('__',$key,2);
+  if( strtolower($realm) == 'core' ) $realm = 'admin';
+} else {
+  $realm = 'admin';
 }
-if( strtolower($realm) == 'core' ) $realm = 'admin';
 $out = CmsLangOperations::lang_from_realm($realm,$key);
 
 echo $out;
 exit;
-
-#
-# EOF
-#
-?>
