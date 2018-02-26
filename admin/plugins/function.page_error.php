7b7a4680ba0cf7a7a980c1f324f61447101a480b
tom2tom <tpgww@onepost.net>
2018-02-26 16:40:14 +1100

    update license cleanup content

--- admin/plugins/function.page_error.php
+++ admin/plugins/function.page_error.php
@@ -1,6 +1,7 @@
 <?php
-#...
-#Copyright (C) 2004-2018 Ted Kulp <ted@cmsmadesimple.org>
+#function to generate admin-page-content for an error display
+#Copyright (C) 2004-2017 Ted Kulp <ted@cmsmadesimple.org>
+#Copyright (C) 2018 The CMSMS Dev Team <coreteam@cmsmadesimple.org>
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
 #This program is free software; you can redistribute it and/or modify
@@ -17,16 +18,15 @@
 
 function smarty_function_page_error($params,&$template)
 {
-	$smarty = $template->smarty;
-  
 	if( !cmsms()->test_state(CmsApp::STATE_ADMIN_PAGE) ) return;
-	if( !isset($params['msg']) ) return;
+	if( empty($params['msg']) ) return;
 
 	$out = '<div class="pageerror">'.trim($params['msg']).'</div>';
 	if( isset($params['assign']) ) {
-		$smarty->assign(trim($params['assign']),$out);
+		//TODO why global smarty instead of $template?
+		$template->smarty->assign(trim($params['assign']),$out);
 		return;
 	}
 	return $out;
 }
-?>
+
