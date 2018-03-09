f2a0db5912f35a0ea872007d178f8aa42961bb26
tom2tom <tpgww@onepost.net>
2018-03-09 20:21:07 +1100

    check login before session param correct key names export stuff to admin.js or runtime hookfunction

--- admin/cms_js_setup.php
+++ admin/cms_js_setup.php
@@ -1,5 +1,5 @@
 <?php
-#...
+#ajax processor to generate runtime variables for admin js to use
 #Copyright (C) 2004-2018 Ted Kulp <ted@cmsmadesimple.org>
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
@@ -14,27 +14,27 @@
 #GNU General Public License for more details.
 #You should have received a copy of the GNU General Public License
 #along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-#$Id$
 
 $CMS_ADMIN_PAGE=1;
+
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'include.php';
 
-$urlext='?'.CMS_SECURE_PARAM_NAME.'='.$_SESSION[CMS_USER_KEY];
 check_login();
+
+$urlext='?'.CMS_SECURE_PARAM_NAME.'='.$_SESSION[CMS_USER_KEY];
 $config = \cms_config::get_instance();
 
-// get some urls and preset language strings.
+// get some urls and language strings.
 
-$data = array();
-$data['ajax_help_url'] = 'ajax_help.php'.$urlext;
-$data['ajax_alerts_url'] = 'ajax_alerts.php'.$urlext;
-$data['title_help'] = lang('help');
+$data = [];
+$data['lang_title_help'] = lang('help');
 $data['lang_alert'] = lang('alert');
 $data['lang_error'] = lang('error');
 $data['lang_ok'] = lang('ok');
+$data['lang_gotit'] = lang('gotit');
 $data['lang_cancel'] = lang('cancel');
 $data['lang_confirm'] = lang('confirm');
+$data['lang_close'] = lang('close');
 $data['lang_yes'] = lang('yes');
 $data['lang_no'] = lang('no');
 $data['lang_none'] = lang('none');
@@ -48,40 +48,18 @@ $data['max_upload_size'] = $config['max_upload_size'];
 $data['admin_url'] = $config['admin_url'];
 $data['root_url'] = $config['root_url'];
 $data['uploads_url'] = $config['uploads_url'];
+$data['ajax_help_url'] = $config['admin_url'].'/ajax_help.php'.$urlext;
+$data['ajax_alerts_url'] = $config['admin_url'].'/ajax_alerts.php'.$urlext;
 $data['secure_param_name'] = CMS_SECURE_PARAM_NAME;
 $data['user_key'] = $_SESSION[CMS_USER_KEY];
 
-// todo: use  apreference
-$fp = ModuleOperations::get_instance()->GetFilePickerModule();
-if( $fp ) {
-    $data['filepicker_url'] = $fp->get_browser_url();
-    $data['filepicker_url'] = str_replace('&amp;','&',$data['filepicker_url']).'&showtemplate=false';
-}
-
 // output some javascript
-$out = 'cms_data = {};'."\n";
-
+$out = '';
 foreach( $data as $key => $value ) {
     $value = json_encode($value);
     $out .= "cms_data.{$key} = {$value};\n";
 }
 
-$out .= <<<EOT
-function cms_lang(key) {
-    'use strict';
-    key = 'lang_'+key;
-    if( typeof(cms_data[key]) !== 'undefined' ) return cms_data[key];
-    alert('lang key '+key+' notset');
-}
-
-// a silly shiv for IE11. ... remove me ASAP.
-if (!String.prototype.startsWith) {
-    String.prototype.startsWith = function(searchString, position){
-      position = position || 0;
-      return this.substr(position, searchString.length) === searchString;
-  };
-}
-EOT;
 header('Pragma: public');
 header('Expires: 0');
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
@@ -89,4 +67,3 @@ header('Cache-Control: private',false);
 header('Content-type: text/javascript');
 echo $out;
 exit;
-?>
