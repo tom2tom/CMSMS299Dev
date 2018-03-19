cf736a7bed7f290bb53a710de59df56233694e54
tom2tom <tpgww@onepost.net>
2018-03-19 18:07:26 +1100

    add licence text cleanups

--- admin/footer.php
+++ admin/footer.php
@@ -1,44 +1,55 @@
 <?php
-// $USE_THEME inherited from parent scope.
-if (! isset($USE_THEME) || $USE_THEME ) {
-    // if using the theme... echo the footer to stdout
+#Final shared stage of admin-page display
+#Copyright (C) 2004-2014 Ted Kulp <ted@cmsmadesimple.org>
+#Copyright (C) 2015-2018 The CMSMS Dev Team <coreteam@cmsmadesimple.org>
+#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// $USE_THEME inherited from parent scope
+if (!isset($USE_THEME) || $USE_THEME) {
+    // using the theme... output the footer
     $themeObject->do_footer();
 }
 
-$gCms = \CmsApp::get_instance();
-$config = \cms_config::get_instance();
-if ($config["debug"] == true) {
+// $config inherited from parent scope
+if ($config['debug']) {
     // echo debug output to stdout
     echo '<div id="DebugFooter">';
-    $arr = $gCms->get_errors();
+    $arr = \CmsApp::get_instance()->get_errors();
     foreach ($arr as $error) {
         echo $error;
     }
     echo '</div> <!-- end DebugFooter -->';
 }
 
-// Pull the stuff out of the buffer...
-$bodycontent = '';
-if (!(isset($USE_OUTPUT_BUFFERING) && $USE_OUTPUT_BUFFERING == false)) {
-  $bodycontent = @ob_get_contents();
+if (!isset($USE_OUTPUT_BUFFERING) || $USE_OUTPUT_BUFFERING) {
+  // pull everything out of the buffer...
+  $pagecontent = @ob_get_contents();
   @ob_end_clean();
+} else {
+  $pagecontent = '';
 }
-// bodycontent should contain what goes in the main content area of the page.
 
-#Do any header replacements (this is for WYSIWYG stuff)
-$formtext = '';
-$formsubmittext = '';
-$bodytext = '';
-$userid = get_userid();
-
+// do any header replacements (this is for WYSIWYG stuff)
 // initialize the requested wysiwyg modules
-// because this can change based on module actions etc... it's done in the footer.
+// done here (after content generation) because this can change based on module actions etc 
 $list = CmsFormUtils::get_requested_wysiwyg_modules();
-if( is_array($list) && count($list) ) {
-    foreach( $list as $module_name => $info ) {
+if (is_array($list) && count($list)) {
+    foreach ($list as $module_name => $info) {
 
         $obj = cms_utils::get_module($module_name);
-        if( !is_object($obj) ) {
+        if (!is_object($obj)) {
             audit('','Core','WYSIWYG module '.$module_name.' requested, but could not be instantiated');
             continue;
         }
@@ -47,19 +58,20 @@ if( is_array($list) && count($list) ) {
         // preload all of the named stylesheets.  to minimize queries.
         $css = null;
         $cssnames = array();
-        foreach( $info as $rec ) {
-            if( $rec['stylesheet'] == '' || $rec['stylesheet'] == CmsFormUtils::NONE ) continue;
+        foreach ($info as $rec) {
+            if ($rec['stylesheet'] == '' || $rec['stylesheet'] == CmsFormUtils::NONE) continue;
             $cssnames[] = $rec['stylesheet'];
         }
         $cssnames = array_unique($cssnames);
-        if( is_array($cssnames) && count($cssnames) ) $css = CmsLayoutStylesheet::load_bulk($cssnames);
-
+        if (is_array($cssnames) && count($cssnames)) {
+            $css = CmsLayoutStylesheet::load_bulk($cssnames);
+        }
         // adjust the cssnames array to only contain the list of the stylesheets we actually found.
-        if( is_array($css) && count($css) ) {
+        if (is_array($css) && count($css)) {
             $tmpnames = array();
-            foreach( $css as $stylesheet ) {
+            foreach ($css as $stylesheet) {
                 $name = $stylesheet->get_name();
-                if( !in_array($name,$tmpnames) ) $tmpnames[] = $name;
+                if (!in_array($name,$tmpnames)) $tmpnames[] = $name;
             }
             $cssnames = $tmpnames;
         }
@@ -69,12 +81,12 @@ if( is_array($list) && count($list) ) {
 
         // initialize each 'specialized' textarea.
         $need_generic = FALSE;
-        foreach( $info as $rec ) {
+        foreach ($info as $rec) {
             $selector = $rec['id'];
             $cssname = $rec['stylesheet'];
 
-            if( $cssname == CmsFormUtils::NONE ) $cssname = null;
-            if( !$cssname || !is_array($cssnames) || !in_array($cssname,$cssnames) || $selector == CmsFormUtils::NONE ) {
+            if ($cssname == CmsFormUtils::NONE) $cssname = null;
+            if (!$cssname || !is_array($cssnames) || !in_array($cssname,$cssnames) || $selector == CmsFormUtils::NONE) {
                 $need_generic = TRUE;
                 continue;
             }
@@ -84,7 +96,7 @@ if( is_array($list) && count($list) ) {
         }
 
         // now, do we need a generic iniitialization?
-        if( $need_generic ) {
+        if ($need_generic) {
             $themeObject->add_headtext($obj->WYSIWYGGenerateHeader());
         }
     }
@@ -92,38 +104,37 @@ if( is_array($list) && count($list) ) {
 
 // initialize the requested syntax hilighter modules
 $list = CmsFormUtils::get_requested_syntax_modules();
-if( is_array($list) && count($list) ) {
-    foreach( $list as $one ) {
+if (is_array($list) && count($list)) {
+    foreach ($list as $one) {
         $obj = cms_utils::get_module($one);
-        if( is_object($obj) ) $themeObject->add_headtext($obj->SyntaxGenerateHeader());
+        if (is_object($obj)) $themeObject->add_headtext($obj->SyntaxGenerateHeader());
     }
 }
 
 $out = \CMSMS\HookManager::do_hook_accumulate('admin_add_footertext');
-if( $out && count($out) ) {
-    foreach( $out as $one ) {
+if ($out && count($out)) {
+    foreach ($out as $one) {
         $one = trim($one);
-        if( $one ) $themeObject->add_footertext($one);
+        if ($one) $themeObject->add_footertext($one);
     }
 }
 
-$bodycontent = $themeObject->postprocess($bodycontent);
-echo $bodycontent;
-
-if (!isset($USE_THEME) || $USE_THEME != false) {
-    if( strpos($bodycontent,'</body') === FALSE ) echo '</body></html>';
+if ($pagecontent) {
+    $pagecontent = $themeObject->postprocess($pagecontent);
+    // now we have the final form to display
 }
+echo $pagecontent;
 
-if (!isset($USE_THEME) || $USE_THEME != false) {
-    if( isset($config['show_performance_info']) ) {
+if (!isset($USE_THEME) || $USE_THEME) {
+    if ($pagecontent && strpos($pagecontent,'</body') === false ) echo '</body></html>';
+
+    if (isset($config['show_performance_info'])) {
         $db = \Cmsapp::get_instance()->GetDb();
         $endtime = microtime();
         $memory = (function_exists('memory_get_usage')?memory_get_usage():0);
         $memory_net = 'n/a';
-        if( isset($orig_memory) ) $memory_net = $memory - $orig_memory;
+        if (isset($orig_memory)) $memory_net = $memory - $orig_memory;
         $memory_peak = (function_exists('memory_get_peak_usage')?memory_get_peak_usage():0);
         echo '<div style="clear: both;">'.microtime_diff($starttime,$endtime)." / ".($db->query_count??'')." queries / Net Memory: {$memory_net} / End: {$memory} / Peak: {$memory_peak}</div>\n";
     }
 }
-
-?>
--- admin/header.php
+++ admin/header.php
@@ -1,20 +1,39 @@
 <?php
+#Initial shared stage of admin-page display
+#Copyright (C) 2004-2014 Ted Kulp <ted@cmsmadesimple.org>
+#Copyright (C) 2015-2018 The CMSMS Dev Team <coreteam@cmsmadesimple.org>
+#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+$userid = get_userid(); //also checks login status
+debug_buffer('before theme load');
+$themeObject = cms_utils::get_theme_object();
+debug_buffer('after theme load');
+$smarty = CMSMS\internal\Smarty::get_instance();
+$config = \cms_config::get_instance();
 
 cms_admin_sendheaders();
-$starttime = microtime();
-if (!(isset($USE_OUTPUT_BUFFERING) && $USE_OUTPUT_BUFFERING == false)) @ob_start();
 
-$userid = get_userid();
-$smarty = CMSMS\internal\Smarty::get_instance();
-
-if (isset($USE_THEME) && $USE_THEME == false) {
-    //echo '<!-- admin theme disabled -->';
+if (isset($config['show_performance_info'])) {
+    $starttime = microtime();
 }
-else {
-    debug_buffer('before theme load');
-    $themeObject = cms_utils::get_theme_object();
+if (!isset($USE_OUTPUT_BUFFERING) || $USE_OUTPUT_BUFFERING) {
+    @ob_start();
+}
+
+if (!isset($USE_THEME) || $USE_THEME) {
     $smarty->assign('secureparam', CMS_SECURE_PARAM_NAME . '=' . $_SESSION[CMS_USER_KEY]);
-    debug_buffer('after theme load');
 
     // Display notification stuff from modules
     // should be controlled by preferences or something
@@ -27,4 +46,6 @@ else {
     }
 
     $themeObject->do_header();
+//} else {
+    //echo '<!-- admin theme disabled -->';
 }
