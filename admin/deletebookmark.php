74d109898452b436476221d3d1105bff1186fe27
tom2tom <tpgww@onepost.net>
2018-04-17 21:04:14 +1000

    handle errors

--- admin/deletebookmark.php
+++ admin/deletebookmark.php
@@ -1,6 +1,7 @@
 <?php
-#...
-#Copyright (C) 2004-2018 Ted Kulp <ted@cmsmadesimple.org>
+#procedure to delete a user's bookmark
+#Copyright (C) 2004-2016 Ted Kulp <ted@cmsmadesimple.org>
+#Copyright (C) 2017-2018 The CMSMS Dev Team <coreteam@cmsmadesimple.org>
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
 #This program is free software; you can redistribute it and/or modify
@@ -14,35 +15,35 @@
 #GNU General Public License for more details.
 #You should have received a copy of the GNU General Public License
 #along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-#$Id$
 
 $CMS_ADMIN_PAGE=1;
 
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib/include.php';
-//require_once("../lib/classes/class.bookmark.inc.php");
-$urlext='?'.CMS_SECURE_PARAM_NAME.'='.$_SESSION[CMS_USER_KEY];
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'include.php';
 
 check_login();
 
-$bookmark_id = -1;
-if (isset($_GET["bookmark_id"]))
-{
-	$bookmark_id = $_GET["bookmark_id"];
+if (!isset($_GET['bookmark_id'])) {
+	return;
+}
 
-	$result = false;
+$urlext='?'.CMS_SECURE_PARAM_NAME.'='.$_SESSION[CMS_USER_KEY];
 
-	$bookops = cmsms()->GetBookmarkOperations();
-	$markobj = $bookops->LoadBookmarkByID($bookmark_id);
+$bookops = cmsms()->GetBookmarkOperations();
+$bookmark_id = (int)$_GET['bookmark_id'];
+$markobj = $bookops->LoadBookmarkByID($bookmark_id);
 
-	if ($markobj)
-	{
-		$result = $markobj->Delete();
+if ($markobj) {
+	$userid = get_userid();
+	if ($userid != $markobj->user_id && !check_permission($userid, 'Manage My Bookmarks')) { //TODO or 'Manage Bookmarks'
+		cms_utils::get_theme_object()->ParkString('error', lang('needpermissionto', 'Manage My Bookmarks'));
+		redirect("listbookmarks.php".$urlext);
 	}
 
+	if (!$markobj->Delete()) {
+		cms_utils::get_theme_object()->ParkString('error', lang('failure'));
+	}
+} else {
+	cms_utils::get_theme_object()->ParkString('error', lang('invalid'));
 }
 
 redirect("listbookmarks.php".$urlext);
-
-
-?>
