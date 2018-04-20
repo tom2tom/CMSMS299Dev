c4966700d0b3193c08953823c7172e2970b3ef73
tom2tom <tpgww@onepost.net>
2018-04-20 10:13:02 +1000

    export button-definition to template

--- lib/modules/Search/function.admin_statistics_tab.php
+++ lib/modules/Search/function.admin_statistics_tab.php
@@ -4,10 +4,6 @@ $smarty->assign('formstart',$this->CreateFormStart($id,'defaultadmin'));
 $smarty->assign('formend',$this->CreateFormEnd());
 $smarty->assign('wordtext',$this->Lang('word'));
 $smarty->assign('counttext',$this->Lang('count'));
-$smarty->assign('exportcsv',
-        '<button type="submit" name="'.$id.'exportcsv" id="'.$id.'exportcsv" class="adminsubmit icon do">'.$this->Lang('export_to_csv').'</button>');
-$smarty->assign('clearwordcount',
-        '<button type="submit" name="'.$id.'clearwordcount" id="'.$id.'clearwordcount" class="adminsubmit icon undo" onclick="return confirm(\''.$this->Lang('confirm_clearstats').'\');">'.$this->Lang('clear').'</button>');
 
 $results = [];
 $query = 'SELECT * FROM '.CMS_DB_PREFIX.'module_search_words ORDER BY count DESC';
