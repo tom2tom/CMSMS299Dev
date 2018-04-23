422bab8d78b1f5afd9bc316e182645dc47154de1
tom2tom <tpgww@onepost.net>
2018-04-23 12:44:43 +1000

    migrate script from template to page-footer

--- lib/modules/ModuleManager/function.admin_installed.php
+++ lib/modules/ModuleManager/function.admin_installed.php
@@ -17,4 +17,61 @@ if ($devmode) {
     $smarty->assign('iconsurl',$this->GetModuleURLPath().'/images');
 }
 $smarty->assign('allow_modman_uninstall',$this->GetPreference('allowuninstall',0));
+
+$yes = $this->Lang('yes');
+$s1 = json_encode($this->Lang('confirm_upgrade'));
+$s2 = json_encode($this->Lang('confirm_remove'));
+$s3 = json_encode($this->Lang('confirm_chmod'));
+$s4 = json_encode($this->Lang('error_nofileuploaded'));
+$s5 = json_encode($this->Lang('error_invaliduploadtype'));
+
+$js = <<<EOS
+<script type="text/javascript">
+//<![CDATA[
+$(document).ready(function() {
+  $('a.mod_upgrade').on('click', function(ev) {
+    ev.preventDefault();
+    cms_confirm_linkclick(this,$s1,'$yes');
+    return false;
+  });
+  $('a.mod_remove').on('click', function(ev) {
+    ev.preventDefault();
+    cms_confirm_linkclick(this,$s2,'$yes');
+    return false;
+  });
+  $('a.mod_chmod').on('click', function(ev) {
+    ev.preventDefault();
+    cms_confirm_linkclick(this,$s3,'$yes');
+    return false;
+  });
+  $('#importbtn').on('click', function() {
+    cms_dialog($('#importdlg'), {
+      modal: true,
+      buttons: {
+        {$this->Lang('submit')}: function() {
+          var file = $('#xml_upload').val();
+          if(file.length == 0) {
+            cms_alert($s4);
+          } else {
+            var ext = file.split('.').pop().toLowerCase();
+            if($.inArray(ext, ['xml','cmsmod']) == -1) {
+              cms_alert($s5);
+            } else {
+              cms_dialog($(this), 'close');
+              $('#local_import').submit();
+            }
+          }
+        },
+        {$this->Lang('cancel')}: function() {
+          cms_dialog($(this), 'close');
+        }
+      }
+    });
+  });
+});
+//]]>
+</script>
+EOS;
+$this->AdminBottomContent($js);
+
 echo $this->ProcessTemplate('admin_installed.tpl');
