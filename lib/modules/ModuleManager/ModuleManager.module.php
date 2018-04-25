2926bf3b24697459054dd1aed1a9707f2820c5b0
tom2tom <tpgww@onepost.net>
2018-04-25 19:49:41 +1000

    update license smarty corrections

--- lib/modules/ModuleManager/ModuleManager.module.php
+++ lib/modules/ModuleManager/ModuleManager.module.php
@@ -1,16 +1,9 @@
 <?php
-#BEGIN_LICENSE
-#-------------------------------------------------------------------------
-# Module: ModuleManager (c) 2013 by Robert Campbell
-#         (calguy1000@cmsmadesimple.org)
-#  An addon module for CMS Made Simple to allow browsing remotely stored
+# Module: ModuleManager
+# An addon module for CMS Made Simple to allow browsing remotely stored
 #  modules, viewing information about them, and downloading or upgrading
-#
-#-------------------------------------------------------------------------
-# CMS - CMS Made Simple is (c) 2005 by Ted Kulp (wishy@cmsmadesimple.org)
-# Visit our homepage at: http://www.cmsmadesimple.org
-#
-#-------------------------------------------------------------------------
+# Copyright (C) 2013-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,9 +16,6 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-#-------------------------------------------------------------------------
-#END_LICENSE
 
 define('MINIMUM_REPOSITORY_VERSION','1.5');
 
@@ -34,24 +24,24 @@ class ModuleManager extends CMSModule
     const _dflt_request_url = 'https://www.cmsmadesimple.org/ModuleRepository/request/v2/';
     private $_operations;
 
-    function GetName() { return get_class($this); }
-    function GetFriendlyName() { return $this->Lang('friendlyname'); }
-    function GetVersion() { return '2.2'; }
-    function GetHelp() { return $this->Lang('help'); }
-    function GetAuthor() { return 'calguy1000'; }
-    function GetAuthorEmail() { return 'calguy1000@hotmail.com'; }
-    function GetChangeLog() { return file_get_contents(__DIR__.'/changelog.inc'); }
-    function IsPluginModule() { return FALSE; }
-    function HasAdmin() { return TRUE; }
-    function IsAdminOnly() { return TRUE; }
-    function GetAdminSection() { return 'extensions'; }
-    function GetAdminDescription() { return $this->Lang('admindescription'); }
-    function LazyLoadAdmin() { return TRUE; }
-    function MinimumCMSVersion() { return '2.2.3'; }
-    function InstallPostMessage() { return $this->Lang('postinstall'); }
-    function UninstallPostMessage() { return $this->Lang('postuninstall'); }
-    function UninstallPreMessage() { return $this->Lang('really_uninstall'); }
-    function VisibleToAdminUser() { return ($this->CheckPermission('Modify Site Preferences') || $this->CheckPermission('Modify Modules')); }
+    public function GetName() { return get_class($this); }
+    public function GetFriendlyName() { return $this->Lang('friendlyname'); }
+    public function GetVersion() { return '2.2'; }
+    public function GetHelp() { return $this->Lang('help'); }
+    public function GetAuthor() { return 'calguy1000'; }
+    public function GetAuthorEmail() { return 'calguy1000@hotmail.com'; }
+    public function GetChangeLog() { return file_get_contents(__DIR__.'/changelog.inc'); }
+    public function IsPluginModule() { return FALSE; }
+    public function HasAdmin() { return TRUE; }
+    public function IsAdminOnly() { return TRUE; }
+    public function GetAdminSection() { return 'extensions'; }
+    public function GetAdminDescription() { return $this->Lang('admindescription'); }
+    public function LazyLoadAdmin() { return TRUE; }
+    public function MinimumCMSVersion() { return '2.2.3'; }
+    public function InstallPostMessage() { return $this->Lang('postinstall'); }
+    public function UninstallPostMessage() { return $this->Lang('postuninstall'); }
+    public function UninstallPreMessage() { return $this->Lang('really_uninstall'); }
+    public function VisibleToAdminUser() { return ($this->CheckPermission('Modify Site Preferences') || $this->CheckPermission('Modify Modules')); }
 
     /**
      * @internal
@@ -64,29 +54,30 @@ class ModuleManager extends CMSModule
 
     protected function _DisplayErrorPage($id, &$params, $returnid, $message='')
     {
-        $this->smarty->assign('title_error', $this->Lang('error'));
-        $this->smarty->assign('message', $message);
-        $this->smarty->assign('link_back',$this->CreateLink($id,'defaultadmin',$returnid, $this->Lang('back_to_module_manager')));
+        $smarty = CmsApp::get_instance()->GetSmarty();
+        $smarty->assign('title_error', $this->Lang('error'));
+        $smarty->assign('message', $message);
+        $smarty->assign('link_back',$this->CreateLink($id,'defaultadmin',$returnid, $this->Lang('back_to_module_manager')));
 
         // Display the populated template
         echo $this->ProcessTemplate('error.tpl');
     }
 
-    function Install()
+    public function Install()
     {
         $this->SetPreference('module_repository',ModuleManager::_dflt_request_url);
     }
 
-    function Upgrade($oldversion, $newversion)
+    public function Upgrade($oldversion, $newversion)
     {
         $this->SetPreference('module_repository',ModuleManager::_dflt_request_url);
     }
 
-    function DoAction($action, $id, $params, $returnid=-1)
+    public function DoAction($action, $id, $params, $returnid=-1)
     {
         @set_time_limit(9999);
         /*
-          $smarty = \CMSMS\internal\Smarty::get_instance();
+		  $smarty = CmsApp::get_instance()->GetSmarty(); OR $smarty = \CMSMS\internal\Smarty::get_instance();
           $smarty->assign($this->GetName(), $this);
           $smarty->assign('mod', $this);
         */
@@ -117,8 +108,5 @@ class ModuleManager extends CMSModule
         $out[] = new \ModuleManager\ReposGetXMLCommand( $app );
         return $out;
     }
-} // end of class
+} // class
 
-#
-# EOF
-#
