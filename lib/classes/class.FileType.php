492ccb3dfe85d658c0a0a5171a0c3a84bb0e0846
tom2tom <tpgww@onepost.net>
2018-05-04 09:48:00 +1000

    add license

--- lib/classes/class.FileType.php
+++ lib/classes/class.FileType.php
@@ -1,7 +1,22 @@
 <?php
+#Filetype constants class
+#Copyright (C) 2016-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 /**
- * This file defines constants for file types
+ * This class defines constants representing file types
  * @package CMS
  * @license GPL
  */
@@ -26,4 +41,4 @@ abstract class FileType
     const TYPE_DOCUMENT = 'document';
     const TYPE_ARCHIVE = 'archive';
     const TYPE_ANY = 'any';
-} // end of class.
+} // class
