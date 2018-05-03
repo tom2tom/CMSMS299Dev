2326269b29a04214487efa71e71b6b6e70393e0d
tom2tom <tpgww@onepost.net>
2018-05-04 09:57:01 +1000

    add license conform API to method in FilePicker module

--- lib/classes/interface.FilePickerInterface.php
+++ lib/classes/interface.FilePickerInterface.php
@@ -1,4 +1,19 @@
 <?php
+# Interface for file-picking modules
+# Copyright (C) 2016-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * Define an interface for File Picker related modules.
@@ -49,6 +64,7 @@ interface FilePickerInterface
      * @param string $name The name for the input field.
      * @param string $value the current value for the input filed
      * @param FilePickerProfile $profile The profile to use when building the filepicker interface.
+	 * @param bool   $required Optional property ... (see FilePicker::get_html()) default false
      */
-    public function get_html( $name, $value, \CMSMS\FilePickerProfile $profile );
-} // end of class
+    public function get_html( $name, $value, \CMSMS\FilePickerProfile $profile, $required = false );
+} // interface
