e7a06c9428cfc6cc3258b1e5ad7299e31aebc158
tom2tom <tpgww@onepost.net>
2018-05-03 12:23:42 +1000

    path fixes

--- lib/modules/CoreFileManager/function.filemanager.php
+++ lib/modules/CoreFileManager/function.filemanager.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Extract of H3K | Tiny File Manager https://github.com/prasathmani/tinyfilemanager
+ * Derived from H3K | Tiny File Manager https://github.com/prasathmani/tinyfilemanager
  * CCP Programmers http://fb.com/ccpprogrammers
  * Licence GPL3
  */
@@ -21,7 +21,7 @@ function fm_rdelete($path)
         if (is_array($objects)) {
             foreach ($objects as $file) {
                 if ($file != '.' && $file != '..') {
-                    if (!fm_rdelete($path . '/' . $file)) {
+                    if (!fm_rdelete($path . DIRECTORY_SEPARATOR . $file)) {
                         $ok = false;
                     }
                 }
@@ -52,7 +52,7 @@ function fm_rchmod($path, $filemode, $dirmode)
         if (is_array($objects)) {
             foreach ($objects as $file) {
                 if ($file != '.' && $file != '..') {
-                    if (!fm_rchmod($path . '/' . $file, $filemode, $dirmode)) {
+                    if (!fm_rchmod($path . DIRECTORY_SEPARATOR . $file, $filemode, $dirmode)) {
                         return false;
                     }
                 }
@@ -97,7 +97,7 @@ function fm_rcopy($path, $dest, $upd = true, $force = true)
         if (is_array($objects)) {
             foreach ($objects as $file) {
                 if ($file != '.' && $file != '..') {
-                    if (!fm_rcopy($path . '/' . $file, $dest . '/' . $file)) {
+                    if (!fm_rcopy($path . DIRECTORY_SEPARATOR . $file, $dest . DIRECTORY_SEPARATOR . $file)) {
                         $ok = false;
                     }
                 }
@@ -184,11 +184,11 @@ function fm_clean_path($path)
 {
     $path = trim($path);
     $path = trim($path, '\\/');
-    $path = str_replace(array('../', '..\\'), '', $path);
+    $path = str_replace(array('../', '..\\'), array('', ''), $path);
     if ($path == '..') {
         $path = '';
     }
-    return str_replace('\\', '/', $path);
+    return str_replace(array('\\', '/'), array(DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR), $path);
 }
 
 /**
@@ -200,12 +200,7 @@ function fm_get_parent_path($path)
 {
     $path = fm_clean_path($path);
     if ($path != '') {
-        $array = explode('/', $path);
-        if (count($array) > 1) {
-            $array = array_slice($array, 0, -1);
-            return implode('/', $array);
-        }
-        return '';
+        return dirname($path);
     }
     return false;
 }
@@ -217,14 +212,16 @@ function fm_get_parent_path($path)
  */
 function fm_get_filesize($size)
 {
+    global $bytename, $kbname, $mbname, $gbname; //$tbname
+
     if ($size < 1000) {
-        return sprintf('%s B', $size);
+        return sprintf('%s %s', $size, $bytename);
     } elseif (($size / 1024) < 1000) {
-        return sprintf('%s KiB', round(($size / 1024), 2));
+        return sprintf('%s %s', round(($size / 1024), 2), $kbname);
     } elseif (($size / 1024 / 1024) < 1000) {
-        return sprintf('%s MiB', round(($size / 1024 / 1024), 2));
+        return sprintf('%s %s', round(($size / 1024 / 1024), 2), $mbname);
     } elseif (($size / 1024 / 1024 / 1024) < 1000) {
-        return sprintf('%s GiB', round(($size / 1024 / 1024 / 1024), 2));
+        return sprintf('%s %s', round(($size / 1024 / 1024 / 1024), 2), $gbname);
     } else {
         return sprintf('%s TiB', round(($size / 1024 / 1024 / 1024 / 1024), 2));
     }
@@ -243,7 +240,7 @@ function fm_get_zif_info($path)
             $filenames = array();
             while ($zip_entry = zip_read($arch)) {
                 $zip_name = zip_entry_name($zip_entry);
-                $zip_folder = substr($zip_name, -1) == '/';
+                $zip_folder = substr($zip_name, -1) == DIRECTORY_SEPARATOR;
                 $filenames[] = array(
                     'name' => $zip_name,
                     'filesize' => zip_entry_filesize($zip_entry),
@@ -270,87 +267,45 @@ function fm_enc($text)
 }
 
 /**
- * Scans the files folder recursively, and build a large array
- * @param string $dir
- * @return json
- */
-function scan($dir){
-    $files = array();
-    $_dir = $dir;
-    $dir = FM_ROOT_PATH.'/'.$dir;
-    // Is there actually such a folder/file?
-    if(file_exists($dir)){
-        foreach(scandir($dir) as $f) {
-            if(!$f || $f[0] == '.') {
-                continue; // Ignore hidden files
-            }
-
-            if(is_dir($dir . '/' . $f)) {
-                // The path is a folder
-                $files[] = array(
-                    "name" => $f,
-                    "type" => "folder",
-                    "path" => $_dir.'/'.$f,
-                    "items" => scan($dir . '/' . $f), // Recursively get the contents of the folder
-                );
-            } else {
-                // It is a file
-                $files[] = array(
-                    "name" => $f,
-                    "type" => "file",
-                    "path" => $_dir,
-                    "size" => filesize($dir . '/' . $f) // Gets the size of this file
-                );
-            }
-        }
-    }
-    return $files;
-}
-
-/**
-* Scan directory and return tree view
+* Recursive function called by fm_file_tree() to list directories
 * @param string $directory
+* @param string $current
 * @param boolean $first_call
 */
-function php_file_tree_dir($directory, $first_call = true) {
-    // Recursive function called by php_file_tree() to list directories/files
+function fm_file_tree_dir($directory, $current, $first_call = true)
+{
+    global $FM_ROOT_PATH, $FM_EXCLUDE_FOLDERS, $FM_FOLDER_URL, $FM_FOLDER_TITLE;
 
+    if( !is_readable($directory) ) {
+        return "";
+    }
+
+    $len = strlen($FM_ROOT_PATH) + 1; //skip to relative-path
     $php_file_tree = "";
-    // Get and sort directories/files
-    if( function_exists("scandir") ) $file = scandir($directory);
-    natcasesort($file);
-    // Make directories first
-    $files = $dirs = array();
-    foreach($file as $this_file) {
-        if( is_dir("$directory/$this_file" ) ) {
-      if(!in_array($this_file, FM_EXCLUDE_FOLDERS)){
-          $dirs[] = $this_file;
-      }
-    } else {
-      $files[] = $this_file;
-    }
-    }
-    $file = array_merge($dirs, $files);
-
-    if( count($file) > 2 ) { // Use 2 instead of 0 to account for . and .. "directories"
-        $php_file_tree = "<ul";
-        if( $first_call ) { $php_file_tree .= " class=\"php-file-tree\""; $first_call = false; }
-        $php_file_tree .= ">";
-        foreach( $file as $this_file ) {
-            if( $this_file != "." && $this_file != ".." ) {
-                if( is_dir("$directory/$this_file") ) {
-                    // Directory
-                    $php_file_tree .= "<li class=\"pft-directory\"><i class=\"if-folder-empty\"></i><a href=\"#\">" . htmlspecialchars($this_file) . "</a>";
-                    $php_file_tree .= php_file_tree_dir("$directory/$this_file", false);
-                    $php_file_tree .= "</li>";
-                } else {
-                    // File
-                    $ext = fm_get_file_icon_class($this_file);
-                    $path = str_replace($_SERVER['DOCUMENT_ROOT'],"",$directory);
-                    $link = "?p="."$path" ."&view=".urlencode($this_file);
-                    $php_file_tree .= "<li class=\"pft-file\"><a href=\"$link\"> <i class=\"$ext\"></i>" . htmlspecialchars($this_file) . "</a></li>";
-                }
+    // Get and sort directories
+    $file = glob ($directory. DIRECTORY_SEPARATOR . "*", GLOB_NOSORT|GLOB_NOESCAPE|GLOB_ONLYDIR);
+    if( $file ) {
+        natcasesort($file);
+        $dirs = array();
+        foreach($file as $this_file) {
+            $name = basename($this_file);
+            if( !($name == '.' || $name == '..' || in_array($this_file, $FM_EXCLUDE_FOLDERS)) ) {
+                $dirs[$name] = $this_file;
             }
+        }
+
+        $php_file_tree = "<ul";
+        if( $first_call ) { $php_file_tree .= " class=\"fm-tree\""; $first_call = false; }
+        $php_file_tree .= ">";
+        foreach( $dirs as $name => $path ) {
+            $relpath = substr($path, $len); 
+            $php_file_tree .= "<li class=\"fm-directory\"><a href=\"".$FM_FOLDER_URL.rawurlencode($relpath)."\"";
+            if ($FM_FOLDER_TITLE) {
+                $php_file_tree .= " title=\"".$FM_FOLDER_TITLE."\"";
+            }
+            $php_file_tree .= ">" . htmlspecialchars($name) . "</a>";
+            $path = $directory . DIRECTORY_SEPARATOR . $name;
+            $php_file_tree .= fm_file_tree_dir($path, $current, false) . "</li>";
         }
         $php_file_tree .= "</ul>";
     }
@@ -361,14 +316,12 @@ function php_file_tree_dir($directory, $first_call = true) {
  * Scan directory and render tree view
  * @param string $directory
  */
-function php_file_tree($directory) {
-    // Remove trailing slash
-    $code = "";
-    if( substr($directory, -1) == "/" ) $directory = substr($directory, 0, strlen($directory) - 1);
-    if(function_exists('php_file_tree_dir')) {
-        $code .= php_file_tree_dir($directory);
-        return $code;
-    }
+function fm_dir_tree($directory, $current = '')
+{
+    // Remove trailing separator
+    if( substr($directory, -1) == DIRECTORY_SEPARATOR ) $directory = substr($directory, 0, strlen($directory) - 1);
+    if( $current && substr($current, -1) == DIRECTORY_SEPARATOR ) $current = substr($current, 0, strlen($current) - 1);
+    return fm_file_tree_dir($directory, $current);
 }
 
 /**
@@ -376,12 +329,12 @@ function php_file_tree($directory) {
  * @param string $msg
  * @param string $status
  */
-function fm_set_msg($msg, $status = 'ok')
+/*function fm_set_msg($msg, $status = 'ok')
 {
     $_SESSION['message'] = $msg;
     $_SESSION['status'] = $status;
 }
-
+*/
 /**
  * Check if string is in UTF-8
  * @param string $string
@@ -399,8 +352,9 @@ function fm_is_utf8($string)
  */
 function fm_convert_win($filename)
 {
-    if (FM_IS_WIN && function_exists('iconv')) {
-        $filename = iconv(FM_ICONV_INPUT_ENC, 'UTF-8//IGNORE', $filename);
+    global $FM_IS_WIN, $FM_ICONV_INPUT_ENC;
+    if ($FM_IS_WIN && function_exists('iconv')) {
+        $filename = iconv($FM_ICONV_INPUT_ENC, 'UTF-8//IGNORE', $filename);
     }
     return $filename;
 }
@@ -657,12 +611,12 @@ class FM_Zipper
         if (is_array($objects)) {
             foreach ($objects as $file) {
                 if ($file != '.' && $file != '..') {
-                    if (is_dir($path . '/' . $file)) {
-                        if (!$this->addDir($path . '/' . $file)) {
+                    if (is_dir($path . DIRECTORY_SEPARATOR . $file)) {
+                        if (!$this->addDir($path . DIRECTORY_SEPARATOR . $file)) {
                             return false;
                         }
-                    } elseif (is_file($path . '/' . $file)) {
-                        if (!$this->zip->addFile($path . '/' . $file)) {
+                    } elseif (is_file($path . DIRECTORY_SEPARATOR . $file)) {
+                        if (!$this->zip->addFile($path . DIRECTORY_SEPARATOR . $file)) {
                             return false;
                         }
                     }
@@ -673,126 +627,3 @@ class FM_Zipper
         return false;
     }
 }
-
-
-
- /**
- * Get image
- * @param string $img
- */
-/*function fm_get_image($img)
-{
-    $img = trim($img);
-    $images = fm_get_images();
-    if (isset($images[$img])) {
-        $image = $images[$img];
-    } else {
-        $image = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAEElEQVR42mL4//8/A0CAAQAI/AL+26JNFgAAAABJRU5ErkJggg==';
-    }
-    return base64_decode($image);
-}
-*/
-/**
- * Get base64-encoded images
- * @return array
- */
-/*function fm_get_images()
-{
-    return array(
-        'favicon' => 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJ
-bWFnZVJlYWR5ccllPAAAAZVJREFUeNqkk79Lw0AUx1+uidTQim4Waxfpnl1BcHMR6uLkIF0cpYOI
-f4KbOFcRwbGTc0HQSVQQXCqlFIXgFkhIyvWS870LaaPYH9CDy8vdfb+fey930aSUMEvT6VHVzw8x
-rKUX3N3Hj/8M+cZ6GcOtBPl6KY5iAA7KJzfVWrfbhUKhALZtQ6myDf1+X5nsuzjLUmUOnpa+v5r1
-Z4ZDDfsLiwER45xDEATgOI6KntfDd091GidzC8vZ4vH1QQ09+4MSMAMWRREKPMhmsyr6voYmrnb2
-PKEizdEabUaeFCDKCCHAdV0wTVNFznMgpVqGlZ2cipzHGtKSZwCIZJgJwxB38KHT6Sjx21V75Jcn
-LXmGAKTRpGVZUx2dAqQzSEqw9kqwuGqONTufPrw37D8lQFxCvjgPXIixANLEGfwuQacMOC4kZz+q
-GdhJS550BjpRCdCbAJCMJRkMASEIg+4Bxz4JwAwDSEueAYDLIM+QrOk6GHiRxjXSkJY8KUCvdXZ6
-kbuvNx+mOcbN9taGBlpLAWf9nX8EGADoCfqkKWV/cgAAAABJRU5ErkJggg==',
-        'sprites' => 'iVBORw0KGgoAAAANSUhEUgAAAYAAAAAgCAMAAAAscl/XAAAC/VBMVEUAAABUfn4KKipIcXFSeXsx
-VlZSUlNAZ2c4Xl4lSUkRDg7w8O/d3d3LhwAWFhYXODgMLCx8fHw9PT2TtdOOAACMXgE8lt+dmpq+
-fgABS3RUpN+VUycuh9IgeMJUe4C5dUI6meKkAQEKCgoMWp5qtusJmxSUPgKudAAXCghQMieMAgIU
-abNSUlJLe70VAQEsh85oaGjBEhIBOGxfAoyUbUQAkw8gui4LBgbOiFPHx8cZX6PMS1OqFha/MjIK
-VKFGBABSAXovGAkrg86xAgIoS5Y7c6Nf7W1Hz1NmAQB3Hgx8fHyiTAAwp+eTz/JdDAJ0JwAAlxCQ
-UAAvmeRiYp6ysrmIAABJr/ErmiKmcsATpRyfEBAOdQgOXahyAAAecr1JCwHMiABgfK92doQGBgZG
-AGkqKiw0ldYuTHCYsF86gB05UlJmQSlra2tVWED////8/f3t9fX5/Pzi8/Px9vb2+/v0+fnn8vLf
-7OzZ6enV5+eTpKTo6Oj6/v765Z/U5eX4+Pjx+Pjv0ojWBASxw8O8vL52dnfR19CvAADR3PHr6+vi
-4uPDx8v/866nZDO7iNT335jtzIL+7aj86aTIztXDw8X13JOlpKJoaHDJAACltratrq3lAgKfAADb
-4vb76N2au9by2I9gYGVIRkhNTE90wfXq2sh8gL8QMZ3pyn27AADr+uu1traNiIh2olTTshifodQ4
-ZM663PH97+YeRq2GqmRjmkGjnEDnfjLVVg6W4f7s6/p/0fr98+5UVF6wz+SjxNsmVb5RUVWMrc7d
-zrrIpWI8PD3pkwhCltZFYbNZja82wPv05NPRdXzhvna4uFdIiibPegGQXankxyxe0P7PnOhTkDGA
-gBrbhgR9fX9bW1u8nRFamcgvVrACJIvlXV06nvtdgON4mdn3og7AagBTufkucO7snJz4b28XEhIT
-sflynsLEvIk55kr866aewo2YuYDrnFffOTk6Li6hgAn3y8XkusCHZQbt0NP571lqRDZyMw96lZXE
-s6qcrMmJaTmVdRW2AAAAbnRSTlMAZodsJHZocHN7hP77gnaCZWdx/ki+RfqOd/7+zc9N/szMZlf8
-z8yeQybOzlv+tP5q/qKRbk78i/vZmf798s3MojiYjTj+/vqKbFc2/vvMzJiPXPzbs4z9++bj1XbN
-uJxhyMBWwJbp28C9tJ6L1xTnMfMAAA79SURBVGje7Jn5b8thHMcfzLDWULXq2upqHT2kbrVSrJYx
-NzHmviWOrCudqxhbNdZqHauKJTZHm0j0ByYkVBCTiC1+EH6YRBY/EJnjD3D84PMc3++39Z1rjp+8
-Kn189rT5Pt/363k+3YHEDOrCSKP16t48q8U1IysLAUKZk1obLBYDKjAUoB8ziLv4vyQLQD+Lcf4Q
-jvno90kfDaQTRhcioIv7QPk2oJqF0PsIT29RzQdOEhfKG6QW8lcoLIYxjWPQD2GXr/63BhYsWrQA
-fYc0JSaNxa8dH4zUEYag32f009DTkNTnC4WkpcRAl4ryHTt37d5/ugxCIIEfZ0Dg4poFThIXygSp
-hfybmhSWLS0dCpDrdFMRZubUkmJ2+d344qIU8sayN8iFQaBgMDy+FWA/wjelOmbrHUKVtQgxFqFc
-JeE2RpmLEIlfFazzer3hcOAPCQiFasNheAo9HQ1f6FZRTgzs2bOnFwn8+AnG8d6impClTkSjCXWW
-kH80GmUGWP6A4kKkQwG616/tOhin6kii3dzl5YHqT58+bf5KQdq8IjCAg3+tk3NDCoPZC2fQuGcI
-7+8nKQMk/b41r048UKOk48zln4MgesydOw0NDbeVCA2B+FVaEIDz/0MCSkOlAa+3tDRQSgW4t1MD
-+7d1Q8DA9/sY7weKapZ/Qp+tzwYDtLyRiOrBANQ0/3hTMBIJNsXPb0GM5ANfrLO3telmTrWXGBG7
-fHVHbWjetKKiPCJsAkQv17VNaANv6zJTWAcvmCEtI0hnII4RLsIIBIjmHStXaqKzNCtXOvj+STxl
-OXKwgDuEBuAOEQDxgwDIv85bCwKMw6B5DzOyoVMCHpc+Dnu9gUD4MSeAGWACTnCBnxgorgGHRqPR
-Z8OTg5ZqtRoEwLODy79JdfiwqgkMGBAlJ4caYK3HNGGCHedPBLgqtld30IbmLZk2jTsB9jadboJ9
-Aj4BMqlAXCqV4e3udGH8zn6CgMrtQCUIoPMEbj5Xk3jS3N78UpPL7R81kJOTHdU7QACff/9kAbD/
-IxHvEGTcmi/1+/NlMjJsNXZKAAcIoAkwA0zAvqOMfQNFNcOsf2BGAppotl6D+P0fi6nOnFHFYk1x
-CzOgvqEGA4ICk91uQpQee90V1W58fdYDx0Ls+JnmTwy02e32iRNJB5L5X7y4/Pzq1buXX/lb/X4Z
-SRtTo4C8uf6/Nez11dRI0pkNCswzA+Yn7e3NZi5/aKcYaKPqLBDw5iHPKGUutCAQoKqri0QizsgW
-lJ6/1mqNK4C41bo2P72TnwEMEEASYAa29SCBHz1J2fdo4ExRTbHl5NiSBWQ/yGYCLBnFLbFY8PPn
-YCzWUpxhYS9IJDSIx1iydKJpKTPQ0+lyV9MuCEcQJw+tH57Hjcubhyhy00TAJEdAuocX4Gn1eNJJ
-wHG/xB+PQ8BC/6/0ejw1nAAJAeZ5A83tNH+kuaHHZD8A1MsRUvZ/c0WgPwhQBbGAiAQz2CjzZSJr
-GOxKw1aU6ZOhX2ZK6GYZ42ZoChbgdDED5UzAWcLRR4+cA0U1ZfmiRcuRgJkIYIwBARThuyDzE7hf
-nulLR5qKS5aWMAFOV7WrghjAAvKKpoEByH8J5C8WMELCC5AckkhGYCeS1lZfa6uf2/AuoM51yePB
-DYrM18AD/sE8Z2DSJLaeLHNCr385C9iowbekfHOvQWBN4dzxXhUIuIRPgD+yCskWrs3MOETIyFy7
-sFMC9roYe0EA2YLMwIGeCBh68iDh5P2TFUOhzhs3LammFC5YUIgEVmY/mKVJ4wTUx2JvP358G4vV
-8wLo/TKKl45cWgwaTNNx1b3M6TwNh5DuANJ7xk37Kv+RBDCAtzMvoPJUZSUVID116pTUw3ecyPZI
-vHIzfEQXMAEeAszzpKUhoR81m4GVNnJHyocN/Xnu2NLmaj/CEVBdqvX5FArvXGTYoAhIaxUb2GDo
-jAD3doabCeAMVFABZ6mAs/fP7sCBLykal1KjYemMYYhh2zgrWUBLi2r8eFVLiyDAlpS/ccXIkSXk
-IJTIiYAy52l8COkOoAZE+ZtMzEA/p8ApJ/lcldX4fc98fn8Nt+Fhd/Lbnc4DdF68fjgNzZMQhQkQ
-UKK52mAQC/D5fHVe6VyEDBlWqzXDwAbUGQEHdjAOgACcAGegojsRcPAY4eD9g7uGonl5S4oWL77G
-17D+fF/AewmzkDNQaG5v1+SmCtASAWKgAVWtKKD/w0egD/TC005igO2AsctAQB6/RU1VVVUmuZwM
-CM3oJ2CB7+1xwPkeQj4TUOM5x/o/IJoXrR8MJAkY9ab/PZ41uZwAr88nBUDA7wICyncyypkAzoCb
-CbhIgMCbh6K8d5jFfA3346qUePywmtrDfAdcrmmfZeMENNbXq7Taj/X1Hf8qYk7VxOlcMwIRfbt2
-7bq5jBqAHUANLFlmRBzyFVUr5NyQgoUdqcGZhMFGmrfUA5D+L57vcP25thQBArZCIkCl/eCF/IE5
-6PdZHzqwjXEgtB6+0KuMM+DuRQQcowKO3T/WjE/A4ndwAmhNBXjq4q1wyluLamWIN2Aebl4uCAhq
-x2u/JUA+Z46Ri4aeBLYHYAEggBooSHmDXBgE1lnggcQU0LgLUMekrl+EclQSSgQCVFrVnFWTKav+
-xAlY35Vn/RTSA4gB517X3j4IGMC1oOsHB8yEetm7xSl15kL4TVIAfjDxKjIRT6Ft0iQb3da3GhuD
-QGPjrWL0E7AlsAX8ZUTr/xFzIP7pRvQ36SsI6Yvr+QN45uN607JlKbUhg8eAOgB2S4bFarVk/PyG
-6Sss4O/y4/WL7+avxS/+e8D/+ku31tKbRBSFXSg+6iOpMRiiLrQ7JUQ3vhIXKks36h/QhY+FIFJ8
-pEkx7QwdxYUJjRC1mAEF0aK2WEActVVpUbE2mBYp1VofaGyibW19LDSeOxdm7jCDNI0rv0lIvp7v
-nnPnHKaQ+zHV/sxcPlPZT5Hrp69SEVg1vdgP+C/58cOT00+5P2pKreynyPWr1s+Ff4EOOzpctTt2
-rir2A/bdxPhSghfrt9TxcCVlcWU+r5NH+ukk9fu6MYZL1NtwA9De3n6/dD4GA/N1EYwRxXzl+7NL
-i/FJUo9y0Mp+inw/Kgp9BwZz5wxArV5e7AfcNGDcLMGL9XXnEOpcAVlcmXe+QYAJTFLfbcDoLlGv
-/QaeQKiwfusuH8BB5EMnfYcKPGLAiCjmK98frQFDK9kvNZdW9lPk96cySKAq9gOCxmBw7hd4LcGl
-enQDBsOoAW5AFlfkMICnhqdvDJ3pSerDRje8/93GMM9xwwznhHowAINhCA0gz5f5MOxiviYG8K4F
-XoBHjO6RkdNuY4TI9wFuoZBPFfd6vR6EOAIaQHV9vaO+sJ8Ek7gAF5OQ7JeqoJX9FPn9qYwSqIr9
-gGB10BYMfqkOluBIr6Y7AHQz4q4667k6q8sVIOI4n5zjARjfGDtH0j1E/FoepP4dg+Nha/fwk+Fu
-axj0uN650e+vxHqhG6YbptcmbSjPd13H8In5TRaU7+Ix4GgAI5Fx7qkxIuY7N54T86m89mba6WTZ
-Do/H2+HhB3Cstra2sP9EdSIGV3VCcn+Umlb2U+T9UJmsBEyqYj+gzWJrg8vSVoIjPW3vWLjQY6fx
-DXDcKOcKNBBxyFdTQ3KmSqOpauF5upPjuE4u3UPEhQGI66FhR4/iAYQfwGUNgx7Xq3v1anxUqBdq
-j8WG7mlD/jzfcf0jf+0Q8s9saoJnYFBzkWHgrC9qjUS58RFrVMw3ynE5IZ/Km2lsZtmMF9p/544X
-DcAEDwDAXo/iA5bEXd9dn2VAcr/qWlrZT5H7LSqrmYBVxfsBc5trTjbbeD+g7crNNuj4lTZYocSR
-nqa99+97aBrxgKvV5WoNNDTgeMFfSCYJzmi2ATQtiKfTrZ2t6daeHiLeD81PpVLXiPVmaBgfD1eE
-hy8Nwyvocb1X7tx4a7JQz98eg/8/sYQ/z3cXngDJfizm94feHzqMBsBFotFohIsK+Vw5t0vcv8pD
-0SzVjPvPdixH648eO1YLmIviUMp33Xc9FpLkp2i1sp8i91sqzRUEzJUgMNbQdrPZTtceBEHvlc+f
-P/f2XumFFUoc6Z2Nnvu/4o1OxBsC7kAgl2s4T8RN1RPJ5ITIP22rulXVsi2LeE/aja6et4T+Zxja
-/yOVEtfzDePjfRW2cF/YVtGH9LhebuPqBqGeP9QUCjVd97/M82U7fAg77EL+WU0Igy2DDDMLDeBS
-JBq5xEWFfDl3MiDmq/R0wNvfy7efdd5BAzDWow8Bh6OerxdLDDgGHDE/eb9oAsp+itxvqaw4QaCi
-Eh1HXz2DFGfOHp+FGo7RCyuUONI7nZ7MWNzpRLwhj/NE3GRKfp9Iilyv0XVpuqr0iPfk8ZbQj/2E
-/v/4kQIu+BODhwYhjgaAN9oHeqV6L/0YLwv5tu7dAXCYJfthtg22tPA8yrUicFHlfDCATKYD+o/a
-74QBoPVHjuJnAOIwAAy/JD9Fk37K/auif0L6LRc38IfjNQRO8AOoYRthhuxJCyTY/wwjaKZpCS/4
-BaBnG+NDQ/FGFvEt5zGSRNz4fSPgu8D1XTqdblCnR3zxW4yHhP7j2M/fT09dTgnr8w1DfFEfRhj0
-SvXWvMTwYa7gb8yA97/unQ59F5oBJnsUI6KcDz0B0H/+7S8MwG6DR8Bhd6D4Jj9GQlqPogk/JZs9
-K/gn5H40e7aL7oToUYAfYMvUnMw40Gkw4Q80O6XcLMRZFgYwxrKl4saJjabqjRMCf6QDdOkeldJ/
-BfSnrvWLcWgYxGX6KfPswEKLZVL6yrgXvv6g9uMBoDic3B/9e36KLvDNS7TZ7K3sGdE/wfoqDQD9
-NGG+9AmYL/MDRM5iLo9nqDEYAJWRx5U5o+3SaHRaplS8H+Faf78Yh4bJ8k2Vz24qgJldXj8/DkCf
-wDy8fH/sdpujTD2KxhxM/ueA249E/wTru/Dfl05bPkeC5TI/QOAvbJjL47TnI8BDy+KlOJPV6bJM
-yfg3wNf+r99KxafOibNu5IQvKKsv2x9lTtEFvmGlXq9/rFeL/gnWD2kB6KcwcpB+wP/IyeP2svqp
-9oeiCT9Fr1cL/gmp125aUc4P+B85iX+qJ/la0k/Ze0D0T0j93jXTpv0BYUGhQhdSooYAAAAASUVO
-RK5CYII=',
-    );
-}
-*/
