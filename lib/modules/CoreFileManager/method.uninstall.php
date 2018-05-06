61345ba53b6774d49d1b77d64dbc1ca0f7938fd4
tom2tom <tpgww@onepost.net>
2018-05-06 19:52:41 +1000

    omit permissions

--- lib/modules/CoreFileManager/method.uninstall.php
+++ lib/modules/CoreFileManager/method.uninstall.php
@@ -1,11 +1,23 @@
 <?php
+# CoreFileManager module method: uninstall
+# Copyright (C) 2018 The CMSMS Dev Team <coreteam@cmsmadesimple.org>
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-if (!function_exists("cmsms")) exit;
+if (!function_exists('cmsms')) exit;
 
-// remove the permissions
-$this->RemovePermission('Use Filemanager'); //Used in some old versions
-$this->RemovePermission('Use Filemanager Advanced');
 $this->RemoveEvent('OnFileUploaded');
 $this->RemoveEvent('OnFileDeleted');
+
 $this->RemovePreference();
-$this->RemoveSmartyPlugin();
