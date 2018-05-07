7dca6b7b6b759ab293da1b5892754771a518ab60
tom2tom <tpgww@onepost.net>
2018-05-07 12:39:08 +1000

    remove preferences

--- lib/modules/CmsJobManager/method.uninstall.php
+++ lib/modules/CmsJobManager/method.uninstall.php
@@ -10,6 +10,7 @@ $dict = NewDataDictionary( $db );
 $sqlarray = $dict->DropTableSQL( CmsJobManager::table_name() );
 $dict->ExecuteSQLArray($sqlarray);
 
+$this->RemovePreference();
 $this->RemovePermission(\CmsJobManager::MANAGE_JOBS);
 $this->RemoveEvent(\CmsJobManager::EVT_ONFAILEDJOB);
 $this->RemoveEventHandler('Core','ModuleUninstalled');
