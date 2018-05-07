1b100962d03edd23e61ccb67e8b93c849d5a4a50
tom2tom <tpgww@onepost.net>
2018-05-07 12:39:54 +1000

    extra strings for settings

--- lib/modules/CmsJobManager/lang/en_US.php
+++ lib/modules/CmsJobManager/lang/en_US.php
@@ -1,4 +1,7 @@
 <?php
+
+$lang['apply'] = 'Apply';
+$lang['cancel'] = 'Cancel';
 $lang['created'] = 'Created';
 $lang['errors'] = 'Errors';
 $lang['evtdesc_CmsJobManager::OnJobFailed'] = 'Sent after a job is removed from the job queue after failing too many times';
@@ -15,10 +18,10 @@ $lang['moddescription'] = 'A module for managing asynchronous processing jobs.';
 $lang['module'] = 'Module';
 $lang['name'] = 'Name';
 $lang['processing_freq'] = 'Maximum processing frequency (seconds)';
-$lang['recur_120m'] = 'Every 2 Hours';
-$lang['recur_15m'] = 'Every 15 Minutes';
-$lang['recur_180m'] = 'Every 3 Hours';
-$lang['recur_30m'] = 'Every 30 Minutes';
+$lang['recur_120m'] = 'Every 2 hours';
+$lang['recur_15m'] = 'Every 15 minutes';
+$lang['recur_180m'] = 'Every 3 hours';
+$lang['recur_30m'] = 'Every 30 minutes';
 $lang['recur_daily'] = 'Daily';
 $lang['recur_hourly'] = 'Hourly';
 $lang['recur_monthly'] = 'Monthly';
@@ -27,18 +30,25 @@ $lang['settings'] = 'Settings';
 $lang['start'] = 'Start';
 $lang['until'] = 'Until';
 
+$lang['prompt_enabled'] = 'Enable job processing by this module';
+$lang['help_enabled'] = 'Background job-processing is an essential element of this website. Only disable this if another compatible job processor is present, and <strong>you understand what you\'re doing</strong>.';
+$lang['prompt_frequency'] = 'Minimum interval between jobs processing (<em>minutes</em>)';
+$lang['help_frequency'] = 'Enter a value from 1 to 10. Lower is better, but not so low that performance of the website is noticeably degraded.';
+$lang['prompt_timelimit'] = 'Timeout for each batch of jobs (<em>seconds</em>)';
+$lang['help_timelimit'] = 'Enter a value from 30 to 1800. This is akin to PHP’s maximum execution time setting.';
+$lang['prompt_joburl'] = 'Custom URL for job processing';
+$lang['help_joburl'] = 'Enter a suitable URL to replace the default internal URL, if that cannot be used. Leave blank to use the default.';
+
 $lang['help'] = <<<EOT
 <h3>What does this do?</h3>
 <p>This is a CMSMS core module that provides functionality for processing jobs asynchronously (in the background) as the website is handling requests.</p>
 <p>Any module which is suitably constructed can create jobs to perform tasks that do not need direct user intervention or that can take some time to process.  This module provides the processing capability for those jobs.</p>
 <h3>How do I use it?</h3>
-<p>This module has no interaction of its own.  It does provide a simple job report that lists jobs that the manager currently has in its queue.  Jobs might regularly pop on to, and off of this queue so refreshing the page from time to time might give you an indication as to what is happening in the background of your site.</p>
-<p>This module will only process jobs at most every minute, and at least every ten minutes.  Though the default is 3 minutes.  This infrequent processing is to ensure reasonable performance on most websites.</p>
-<p>You can adjust the frequency by adding a cmsjobmgr_asyncfreq variable into the config.php file for your site containing an integer value between 0 and 10.</p>
-<pre>i.e: <code>\$config["cmsjobmgr_asyncfreq"] = 5;</code>.</pre>
-<p><strong>Note:</strong> It is not possible to disable asynchronous processing completely.  This is because some functioning of the CMSMS core relies on this functionality.</p>
-
-<h3>What about problem jobs.</h3>
+<p>This module has no interaction of its own.  It does provide a simple job report that lists jobs that the manager currently has in its queue.  Jobs might regularly pop on and off this queue, so refreshing the page from time to time might give you an indication as to what is happening in the background of your site.</p>
+<p>This module will process jobs at most every minute, and at least every ten minutes. The default interval is 3 minutes. This infrequent processing is to ensure reasonable performance on most websites.</p>
+<p>You can adjust the frequency in the module settings.</p>
+<p><strong>Note:</strong> It is an error to disable asynchronous processing completely, because some functioning of the CMSMS core relies on this functionality. This module's operation may be disabled, but only do so if another suitable alternative is present.</p>
+<h3>What about problem jobs?</h3>
 <p>From time to time some applications might create jobs that fail, exiting with some sort of error.  CmsJobManager will remove the job after the job has failed a number of times.  At which time the originating code can re-create the job.  If you encounter a problematic job that continues to fail this is a bug that should be diagnosed, and reported in detail to the appropriate developers.</p>
 EOT;
 
