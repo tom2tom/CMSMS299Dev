b1b54ad4740f842e0f8c65a58eeb9aa3d72345e1
tom2tom <tpgww@onepost.net>
2018-05-07 12:43:25 +1000

    support preferences

--- lib/modules/CmsJobManager/action.defaultadmin.php
+++ lib/modules/CmsJobManager/action.defaultadmin.php
@@ -2,6 +2,25 @@
 if( !isset($gCms) ) exit;
 if( !$this->VisibleToAdminUser() ) exit;
 
+if( isset($params['apply']) && $this->CheckPermission('Modify Site Preferences') ) {
+    $this->SetPreference('enabled', !empty($params['enabled']));
+    $t = max(30, min(1800, (int)$params['jobtimeout']));
+    $this->SetPreference('jobtimeout', $t);
+    $t = max(1, min(10, (int)$params['jobinterval']));
+	$this->SetPreference('jobinterval', $t);
+	$t = trim($params['joburl']);
+	if( $t ) {
+		$t2 = filter_var($t, FILTER_SANITIZE_URL);
+		if( filter_var($t2, FILTER_VALIDATE_URL) ) {
+            $this->SetPreference('joburl', $t2);
+		} else {
+			$this->ShowErrors($this-Lang('err_url'));
+		}
+    } else {
+		$this->SetPreference('joburl', '');
+	}
+}
+
 $jobs = [];
 $job_objs = \CmsJobManager\JobQueue::get_all_jobs();
 if( $job_objs ) {
@@ -18,8 +37,7 @@ if( $job_objs ) {
     }
 }
 
-$list = array();
-$list[''] = '';
+$list = ['' => ''];
 $list[\CMSMS\Async\CronJob::RECUR_NONE] = '';
 $list[\CMSMS\Async\CronJob::RECUR_15M] = $this->Lang('recur_15m');
 $list[\CMSMS\Async\CronJob::RECUR_30M] = $this->Lang('recur_30m');
@@ -32,8 +50,35 @@ $list[\CMSMS\Async\CronJob::RECUR_MONTHLY] = $this->Lang('recur_monthly');
 
 $tpl = $this->create_new_template('defaultadmin.tpl');
 $tpl->assign('jobs',$jobs);
-$tpl->assign('async_freq',\CmsJobManager\utils::get_async_freq());
-$tpl->assign('last_processing',(int) $this->GetPreference('last_processing'));
 $tpl->assign('recur_list',$list);
-$tpl->assign('async_freq',\CmsJobManager\utils::get_async_freq());
+$tpl->assign('jobinterval',(int)$this->GetPreference('jobinterval'));
+$tpl->assign('last_processing',(int)$this->GetPreference('last_processing'));
+
+if( $this->CheckPermission('Modify Site Preferences') ) {
+	$tpl->assign('tabbed',1);
+	$tpl->assign('enabled',(int)$this->GetPreference('enabled'));
+	$tpl->assign('jobtimeout',(int)$this->GetPreference('jobtimeout'));
+	$tpl->assign('joburl',$this->GetPreference('joburl'));
+}
+
+/* TESTING
+extra template content
+<a id="simple1" href="{cms_action_url action=test1}" class="link_button icon do">Simple Derived Class Test</a>
+<a href="{cms_action_url action=test2}" class="link_button icon check">Simple Derived Cron Test</a>
+$js = <<<EOS
+<script type="text/javascript">
+//<![CDATA[
+$(document).ready(function() {
+  $('#simple1').on('click', function(ev) {
+    ev.preventDefault();
+    cms_confirm('woot it works');
+    return false;
+  });
+});
+//]]>
+</script>
+EOS;
+$this->AdminFooterContent($js);
+*/
+
 $tpl->display();
