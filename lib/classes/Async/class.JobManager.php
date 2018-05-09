6d96d8ede2e6d076bf279c61edf49019a4c64b05
tom2tom <tpgww@onepost.net>
2018-05-09 18:14:03 +1000

    update license

--- lib/classes/Async/class.JobManager.php
+++ lib/classes/Async/class.JobManager.php
@@ -1,13 +1,7 @@
 <?php
-#BEGIN_LICENSE
-#-------------------------------------------------------------------------
-# \CMSMS\Async\JobManager (c) 2016 by Robert Campbell (calguy1000@cmsmadesimple.org)
-#
-#-------------------------------------------------------------------------
-# CMS - CMS Made Simple is (c) 2005 by Ted Kulp (wishy@cmsmadesimple.org)
+# asynchronous jobs class
+# Copyright (C) 2017-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
 # This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
-#
-#-------------------------------------------------------------------------
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,19 +14,16 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-#-------------------------------------------------------------------------
-#END_LICENSE
 
 /**
- * This file defines the job manager for asyncrhonous jobs.
+ * This file defines the job manager for asynchronous jobs.
  *
  * @package CMS
  */
 namespace CMSMS\Async;
 
 /**
- * A singleton class defining a manager for asyncrhonous jobs.
+ * A singleton class defining a manager for asynchronous jobs.
  *
  * In reality, this is a simple proxy for methods in the CmsJobManager module.
  *
@@ -149,4 +140,4 @@ final class JobManager
         $mod = $this->get_mod();
         if( $mod ) return $mod->delete_job($module_name);
     }
-} // end of class
+} // class
