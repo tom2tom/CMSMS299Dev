95ea455f9f77ad9b9b3cf1981ce3f692256782d5
tom2tom <tpgww@onepost.net>
2018-05-14 10:47:27 +1000

    short array syntax

--- phar_installer/app/install/createseq.php
+++ phar_installer/app/install/createseq.php
@@ -1,29 +1,27 @@
 <?php
 
 if (isset($CMS_INSTALL_CREATE_TABLES)) {
-    $table_ids = array(
-        'additional_users'          => array('id' => 'additional_users_id'),
-        'admin_bookmarks'           => array('id' => 'bookmark_id'),
-        'content'                   => array('id' => 'content_id'),
-        'content_props'             => array('id' => 'content_id'),
-        'events'                    => array('id' => 'event_id'),
-        'event_handlers'            => array('id' => 'handler_id', 'seq' => 'event_handler_seq'),
-        'group_perms'               => array('id' => 'group_perm_id'),
-        'groups'                    => array('id' => 'group_id'),
-        'users'                     => array('id' => 'user_id'),
-        'userplugins'               => array('id' => 'userplugin_id'),
-        'permissions'               => array('id' => 'permission_id')
-    );
+    $table_ids = [
+        'additional_users'          => ['id' => 'additional_users_id'],
+        'admin_bookmarks'           => ['id' => 'bookmark_id'],
+        'content'                   => ['id' => 'content_id'],
+        'content_props'             => ['id' => 'content_id'],
+        'events'                    => ['id' => 'event_id'],
+        'event_handlers'            => ['id' => 'handler_id', 'seq' => 'event_handler_seq'],
+        'group_perms'               => ['id' => 'group_perm_id'],
+        'groups'                    => ['id' => 'group_id'],
+        'users'                     => ['id' => 'user_id'],
+        'userplugins'               => ['id' => 'userplugin_id'],
+        'permissions'               => ['id' => 'permission_id']
+    ];
 
     status_msg(ilang('install_update_sequences'));
-    foreach ($table_ids as $tablename => $tableinfo)
-    {
+    foreach ($table_ids as $tablename => $tableinfo) {
         $sql = 'SELECT COALESCE(MAX(?),0) AS maxid FROM '.CMS_DB_PREFIX.$tablename;
-        $max = $db->GetOne($sql,array($tableinfo['id']));
+        $max = $db->GetOne($sql,[$tableinfo['id']]);
         $tableinfo['seq'] = $tableinfo['seq'] ?? $tablename . '_seq';
         verbose_msg(ilang('install_updateseq',$tableinfo['seq']));
         $db->CreateSequence(CMS_DB_PREFIX.$tableinfo['seq'], $max);
     }
 }
 
-?>
