21132d0f57ba9897b1e92107a7502422ad1f31da
tom2tom <tpgww@onepost.net>
2018-05-21 09:10:10 +1000

    add license

--- lib/classes/class.CmsCoreCapabilities.php
+++ lib/classes/class.CmsCoreCapabilities.php
@@ -1,14 +1,25 @@
 <?php
+# Class that identifies various standard core capabilities
+# Copyright (C) 2018 The CMSMS Dev Team <coreteam@cmsmadesimple.org>
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# BUT withOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//namespace CMSMS;
+//TODO why is this a class?
 
 /**
- * A class that identifies numerous core capabilities
- *
- * @package CMS
- * @license GPL
- */
-
-/**
- * A class that identifies numerous standard core capabilities
+ * A class that identifies various standard core capabilities
  *
  * @package CMS
  * @license GPL
@@ -16,56 +27,54 @@
  */
 final class CmsCoreCapabilities
 {
-  /**
-   * @ignore
-   */
-  private function __construct() {}
+	/**
+	 * @ignore
+	 */
+	private function __construct() {}
 
-  /**
-   * A constant for the admin search capability
-   */
-  const ADMINSEARCH = 'AdminSearch'; // module supports admin search.
+	/**
+	 * A constant for the admin search capability
+	 */
+	const ADMINSEARCH = 'AdminSearch'; // module supports admin search.
 
-  /**
-   * A constant for a capability indicating the module provides content block types
-   */
-  const CONTENT_BLOCKS = 'contentblocks'; 
+	/**
+	 * A constant for a capability indicating the module provides content block types
+	 */
+	const CONTENT_BLOCKS = 'contentblocks'; 
 
-  /**
-   * A constant for a capability indicating that the module provides custom content types
-   */
-  const CONTENT_TYPES = 'content_types';
+	/**
+	 * A constant for a capability indicating that the module provides custom content types
+	 */
+	const CONTENT_TYPES = 'content_types';
 
-  /**
-   * A constant indicating that the module handles events
-   */
-  const EVENTS = 'handles_events';
+	/**
+	 * A constant indicating that the module handles events
+	 */
+	const EVENTS = 'handles_events';
 
-  /**
-   * A constant indicating that the module is a plugin module
-   */
-  const PLUGIN_MODULE = 'plugin';
+	/**
+	 * A constant indicating that the module is a plugin module
+	 */
+	const PLUGIN_MODULE = 'plugin';
 
-  /**
-   * A capability indicating that the module provides frontend search functionality.
-   */
-  const SEARCH_MODULE = 'search';
+	/**
+	 * A capability indicating that the module provides frontend search functionality.
+	 */
+	const SEARCH_MODULE = 'search';
 
-  /**
-   * A capability indicating that the module is a syntax editor module.
-   */
-  const SYNTAX_MODULE = 'syntaxhighlighting'; // string used pre-2.0
+	/**
+	 * A capability indicating that the module is a syntax editor module.
+	 */
+	const SYNTAX_MODULE = 'syntaxhighlighting'; // string used pre-2.0
 
-  /**
-   * A capability indicating that the module provides pseudocron tasks
-   */
-  const TASKS = 'tasks';  // string used pre-2.0
+	/**
+	 * A capability indicating that the module provides pseudocron tasks
+	 */
+	const TASKS = 'tasks';  // string used pre-2.0
 
-  /**
-   * A capability indicating that the module is a WYSIWYG module
-   */
-  const WYSIWYG_MODULE = 'wysiwyg'; // string used pre-2.0
+	/**
+	 * A capability indicating that the module is a WYSIWYG module
+	 */
+	const WYSIWYG_MODULE = 'wysiwyg'; // string used pre-2.0
 
-} // end of class
-
-?>
+} // class
