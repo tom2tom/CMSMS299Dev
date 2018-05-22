1dbcbf4e98dd657ff4bc5b86e88eb72ab3d9a730
tom2tom <tpgww@onepost.net>
2018-05-22 16:13:30 +1000

    support prior creation of cms_data object

--- admin/cms_js_setup.php
+++ admin/cms_js_setup.php
@@ -1,6 +1,7 @@
 <?php
 #ajax processor to generate runtime variables for admin js to use
-#Copyright (C) 2004-2018 Ted Kulp <ted@cmsmadesimple.org>
+#Copyright (C) 2004-2017 Ted Kulp <ted@cmsmadesimple.org>
+#Copyright (C) 2018 The CMSMS Dev Team <coreteam@cmsmadesimple.org>
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
 #This program is free software; you can redistribute it and/or modify
@@ -24,8 +25,8 @@ check_login();
 $urlext='?'.CMS_SECURE_PARAM_NAME.'='.$_SESSION[CMS_USER_KEY];
 $config = cms_config::get_instance();
 
-// get some urls and language strings.
-
+// get some urls and language strings
+// TODO some of these should be action-spacific
 $data = [];
 $data['lang_title_help'] = lang('help');
 $data['lang_alert'] = lang('alert');
@@ -39,10 +40,10 @@ $data['lang_yes'] = lang('yes');
 $data['lang_no'] = lang('no');
 $data['lang_none'] = lang('none');
 $data['lang_disabled'] = lang('disabled');
-$data['lang_hierselect_title'] = lang('title_hierselect_select');
-$data['lang_select_file'] = lang('select_file');
+$data['lang_hierselect_title'] = lang('title_hierselect_select'); // jquery.cmsms_hierselector.js
+$data['lang_select_file'] = lang('select_file'); // jquery.cmsms_filepicker.js
 $data['lang_choose'] = lang('choose');
-$data['lang_filetobig'] = lang('upload_filetobig');
+//$data['lang_filetobig'] = lang('upload_filetobig'); //unused
 $data['lang_largeupload'] = lang('upload_largeupload');
 $data['max_upload_size'] = $config['max_upload_size'];
 $data['admin_url'] = $config['admin_url'];
@@ -52,13 +53,29 @@ $data['ajax_help_url'] = $config['admin_url'].'/ajax_help.php'.$urlext;
 $data['ajax_alerts_url'] = $config['admin_url'].'/ajax_alerts.php'.$urlext;
 $data['secure_param_name'] = CMS_SECURE_PARAM_NAME;
 $data['user_key'] = $_SESSION[CMS_USER_KEY];
+$c = count($data) - 1; // special-case the last member
 
 // output some javascript
-$out = '';
-foreach( $data as $key => $value ) {
-    $value = json_encode($value);
-    $out .= "cms_data.{$key} = {$value};\n";
+$out = <<<EOS
+var ex = {
+
+EOS;
+for ($i=0; $i<$c; ++$i) {
+    $key = key($data);
+    $value = json_encode(current($data));
+    $out .= " $key: $value,\n";
+	next($data);
 }
+$key = key($data);
+$value = json_encode(current($data));
+$out .= <<<EOS
+ $key: $value
+};
+if(typeof cms_data === 'undefined') {
+ var cms_data = {};
+}
+Object.keys(ex).forEach(function(key) { cms_data[key] = ex[key]; });
+EOS;
 
 header('Pragma: public');
 header('Expires: 0');
