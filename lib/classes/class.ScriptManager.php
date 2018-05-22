686d96a02fbc57117ee9a495ba52100d7c9be91a
tom2tom <tpgww@onepost.net>
2018-05-22 11:15:24 +1000

    add API text apply PHP7 features optional path for files

--- lib/classes/class.ScriptManager.php
+++ lib/classes/class.ScriptManager.php
@@ -1,5 +1,5 @@
 <?php
-#Class for consolidating various javascript's into a single file
+#Class for consolidating specified javascript's into a single file
 #Copyright (C) 2018 Robert Campbell <calguy1000@cmsmadesimple.org>
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
@@ -16,32 +16,60 @@
 #along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 namespace CMSMS;
-use CMSMS\HookManager
-use const CMS_SCRIPTS_PATH;
-use function file_put_contents;
+use CMSMS\HookManager;
+use const CMS_SCRIPTS_PATH, TMP_CACHE_LOCATION;
 
+/**
+ * A class for consolidating specified javascript's into a single file.
+ *
+ * @since 2.3
+ * @package CMS
+ */
 class ScriptManager
 {
     private $_scripts = [];
     private $_script_priority = 2;
 
+    /**
+     * Get default priority for scripts to be merged
+     *
+     * @return int 1..3 The current default priority
+     */
     public function get_script_priority() : int
     {
         return $this->_script_priority;
     }
 
+    /**
+     * Set default priority for scripts to be merged
+     *
+     * @param int $val The new default priority value (constrained to 1..3)
+     */
     public function set_script_priority( int $val )
     {
         $this->_script_priority = max(1,min(3,$val));
     }
 
+    /**
+     * Record a script to be merged if necessary
+     *
+     * @param string $filename Filesystem path of script file
+     * @param int    $priority Optional priority 1..3 for the script. Default 0 (use current default)
+     */
     public function queue_script( string $filename, int $priority = 0 )
     {
         if( !is_file($filename) ) return;
 
         $sig = md5( $filename );
         if( isset( $this->_scripts[$sig]) ) return;
-        if( is_null( $priority ) ) $priority = $this->_script_priority;
+
+        if( $priority < 1 ) {
+            $priority = $this->_script_priority;
+        } elseif( $priority > 3 ) {
+            $priority = 3;
+        } else {
+            $priority = (int)$priority;
+		}
 
         $this->_scripts[$sig] = [
             'file' => $filename,
@@ -51,10 +79,22 @@ class ScriptManager
             ];
     }
 
-    public function render_scripts( string $output_path, bool $force = false, bool $allow_defer = true )
+    /**
+     * Construct a merged file from previously-queued scripts, if such file
+     * doesn't exist or is out-of-date.
+     * Hooks 'Core::PreProcessScripts' and 'Core::PostProcessScripts' are
+     * run respectively before and after the content merge.
+     *
+     * @param string $output_path Optional Filesystem path of folder to hold the script file. Default '' (use TMP_CACHE_LOCATION)
+     * @param bool   $force       Optional flag whether to force recreation of the merged file. Default false
+     * @param bool   $allow_defer Optional flag whether to force-include jquery.cmsms_defer.js. Default true
+     * @return string basename of the merged-scripts file
+     */
+    public function render_scripts( string $output_path = '', bool $force = false, bool $allow_defer = true )
     {
         if( $this->_scripts && !count($this->_scripts) ) return; // nothing to do
-        if( !is_dir( $output_path ) ) return; // nowhere to put it
+        $base_path = ($output_path) ? rtrim($output_path, ' /\\') : TMP_CACHE_LOCATION;
+        if( !is_dir( $base_path ) ) return; // nowhere to put it
 
         // auto append the defer script
         if( $allow_defer ) {
@@ -62,37 +102,34 @@ class ScriptManager
             $this->queue_script( $defer_script, 3 );
         }
 
-        $t_scripts = HookManager::do_hook( 'Core::PreProcessScripts', $this->_scripts );
-        if( $t_scripts ) $scripts = $t_scripts;
+        $tmp = HookManager::do_hook( 'Core::PreProcessScripts', $this->_scripts );
+        $scripts = ( $tmp ) ? $tmp : $this->_scripts;
 
-        // sort the scripts by their priority, then their index (to preserve order)
-        // because module actions can be processed first... we 'lower' their priority
-        $scripts = $this->_scripts;
+        // sort the scripts by priority, then index (to preserve order)
         usort( $scripts, function( $a, $b ) {
-                if( $a['priority'] < $b['priority'] ) return -1;
-                if( $a['priority'] > $b['priority'] ) return 1;
-                if( $a['index'] < $b['index'] ) return -1;
-                if( $a['index'] > $b['index'] ) return 1;
-                return 0;
-            });
+            if( $a['priority'] != $b['priority'] ) return $a['priority'] <=> $b['priority'];
+            return $a['index'] <=> $b['index'];
+        });
 
-        $t_sig = $t_mtime = null;
+        $t_sig = '';
+        $t_mtime = -1;
         foreach( $scripts as $sig => $rec ) {
             $t_sig .= $sig;
             $t_mtime = max( $rec['mtime'], $t_mtime );
         }
         $sig = md5( __FILE__.$t_sig.$t_mtime );
         $js_filename = "cms_$sig.js";
-        $output_file = "$output_path/$js_filename";
+        $output_file = $base_path.DIRECTORY_SEPARATOR.$js_filename;
+
         if( $force || !is_file($output_file) || filemtime($output_file) < $t_mtime ) {
             $output = '';
             foreach( $scripts as $sig => $rec ) {
-                $content = file_get_contents( $rec['file'] );
-                $output .= $content."\n\n";
+                $content = @file_get_contents( $rec['file'] );
+                if( $content ) $output .= $content."\n\n";
             }
             $tmp = HookManager::do_hook( 'Core::PostProcessScripts', $output );
             if( $tmp ) $output = $tmp;
-            file_put_contents( $output_file, $output );
+            file_put_contents( $output_file, $output, LOCK_EX );
         }
         return $js_filename;
     }
