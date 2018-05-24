2f87594ffab9662fb7fae45c7310127b7ab5d25e
tom2tom <tpgww@onepost.net>
2018-05-24 10:36:13 +1000

    support sort-types

--- lib/classes/class.FilePickerProfile.php
+++ lib/classes/class.FilePickerProfile.php
@@ -1,5 +1,5 @@
 <?php
-# Class defining folder-specific properties and roles
+# Class defining folder-specific properties and roles, and/or permitted users/groups
 # Copyright (C) 2016-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
 # This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
@@ -15,16 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-/**
- * A class that defines folder-specific properties, permitted operations, and/or permitted users/groups
- * @package CMS
- * @license GPL
- */
-
 namespace CMSMS;
 
+use CmsInvalidDataException;
+
 /**
- * A simple class that defines a profile of information used by the filepicker to indicate how it should
+ * A simple class that defines a profile of information used by a filepicker to indicate how it should
  * behave and what functionality should be provided.
  *
  * This is an immutable class.
@@ -40,24 +36,26 @@ namespace CMSMS;
  * @license GPL
  * @author Robert Campbell <calguy1000@cmsmadesimple.org>
  * @since  2.2
- * @property-read string $top The top directory for the filepicker (relative to the CMSMS uploads directory)
- * @property-read FileType $type A FileType object representing files which may be selected.
+ * @property-read string $top The top directory for the filepicker (relative to the CMSMS uploads directory, or ... TODO)
+ * @property-read FileType $type A FileType enumerator representing files which may be used.
  * @property-read string $match_prefix List only files/items that have the specified prefix.
  * @property-read string exclude_prefix  Exclude any files/items that have the specified prefix.
- * @property-read bool $can_mkdir  Users of the filepicker can create new directories.
- * @property-read bool $can_upload  Users of the filepicker can upload new files (of the specified type)
- * @property-read bool $can_delete  Users of the filepicker can remove files.
- * @property-read bool $show_thumbs Whether thumbnail images should be shown in place of normal icons for images.
- * @property-read bool $show_hidden Indicates that hidden files should be shown in the filepicker.
- * @property-read bool $sort Indicates whether files should be sorted before listing them in the filepicker.
- * @property-read array $allow_groups Since 2.3 group-id's which are permitted to perform the suitably-flagged operations defined in the profile default ['*']
- * @property-read array $block_groups Since 2.3 group-id's which may not perform the suitably-flagged operations defined in the profile default []
- * @property-read array $allow_users  Since 2.3 user-id's which are permitted to perform the suitably-flagged operations defined in the profile default ['*']
- * @property-read array $block_groups Since 2.3 user-id's which may not perform the suitably-flagged operations defined in the profile default []
+ * @property-read int $can_mkdir  Users of the filepicker can create new directories.
+ * @property-read int $can_upload  Users of the filepicker can upload new files (of the specified type)
+ * @property-read int $can_delete  Users of the filepicker can remove files.
+ * @property-read int $show_thumbs Whether thumbnail images should be shown in place of normal icons for images.
+ * @property-read int $show_hidden Indicates that hidden files should be shown in the filepicker.
+ * @property-read mixed $sort Indicates whether and how files should be sorted before listing them in the filepicker.
+ *   FilePickerProfile::FLAG_NO, or one of 'name','size','date' & optionally appended ',a[sc]' or ',d[esc]'
+ * @property-read array $match_groups Since 2.3 group-id's which are permitted to perform the suitably-flagged operations defined in the profile default ['*']
+ * @property-read array $exclude_groups Since 2.3 group-id's which may not perform the suitably-flagged operations defined in the profile default []
+ * @property-read array $match_users  Since 2.3 user-id's which are permitted to perform the suitably-flagged operations defined in the profile default ['*']
+ * @property-read array $exclude_groups Since 2.3 user-id's which may not perform the suitably-flagged operations defined in the profile default []
  */
 class FilePickerProfile
 {
     const FLAG_NONE = 0;
+    const FLAG_NO = 0;
     const FLAG_YES = 1;
     const FLAG_BYGROUP = 2;
     const FLAG_BYUSER = 3;
@@ -67,21 +65,57 @@ class FilePickerProfile
      * @ignore
      */
     private $_data = [
-      'allow_groups'=>['*'],
-      'allow_users'=>['*'],
-      'block_groups'=>[],
-      'block_users'=>[],
       'can_delete'=>self::FLAG_YES,
-      'can_mkdir'=>TRUE,
+      'can_mkdir'=>self::FLAG_YES,
       'can_upload'=>self::FLAG_YES,
+      'exclude_groups'=>[],
+      'exclude_users'=>[],
       'exclude_prefix'=>null,
+      'match_groups'=>['*'],
+      'match_users'=>['*'],
       'match_prefix'=>null,
-      'show_hidden'=>FALSE,
-      'show_thumbs'=>1,
-      'sort'=>TRUE,
+      'show_hidden'=>self::FLAG_NO,
+      'show_thumbs'=>self::FLAG_YES,
+      'sort'=>'name',
       'top'=>null,
       'type'=>FileType::TYPE_ANY,
-   ];
+    ];
+
+    /**
+     * Constructor
+     *
+     * @param array $params An associative array of params suitable for the setValue method.
+     */
+    public function __construct( array $params = [] )
+    {
+        foreach( $params as $key => $val ) {
+            $this->setValue($key,$val);
+        }
+    }
+
+    /**
+     * @ignore
+     */
+    public function __get( string $key )
+    {
+        switch( $key ) {
+        case 'top':
+        case 'type':
+        case 'match_prefix':
+        case 'exclude_prefix':
+            return trim($this->_data[$key]);
+
+        case 'can_mkdir':
+        case 'can_upload':
+        case 'can_delete':
+        case 'show_thumbs':
+        case 'show_hidden':
+            return (int) $this->_data[$key];
+
+        default:
+            return $this->_data[$key] ?? null;
+        }
+    }
 
     /**
      * Set a value into this profile
@@ -89,14 +123,11 @@ class FilePickerProfile
      * @param string $key The key to set
      * @param mixed $val The value to set.
      */
-    protected function setValue( $key, $val )
+    protected function setValue( string $key, $val )
     {
         switch( $key ) {
         case 'top':
-            $val = trim($val);
-            $this->_data[$key] = $val;
-            break;
-
+           //TODO relevant checks
         case 'match_prefix':
         case 'exclude_prefix':
             $this->_data[$key] = trim($val);
@@ -119,7 +150,7 @@ class FilePickerProfile
                 $this->_data[$key] = FileType::TYPE_ANY;
                 break;
             default:
-                throw new \CmsInvalidDataException("$val is an invalid value for type in ".__CLASS__);
+                throw new CmsInvalidDataException("$val is an invalid value for type in ".__CLASS__);
                 break;
             }
             break;
@@ -127,61 +158,33 @@ class FilePickerProfile
         case 'can_mkdir':
         case 'can_delete':
         case 'can_upload':
+        case 'show_thumbs':
+        case 'show_hidden':
             $val = (int) $val;
             switch( $val ) {
-            case self::FLAG_NONE:
+            case self::FLAG_NO:
             case self::FLAG_YES:
             case self::FLAG_BYGROUP:
                 $this->_data[$key] = $val;
                 break;
             default:
                 die('val is '.$val);
-                throw new \CmsInvalidDataException("$val is an invalid value for $key in ".__CLASS__);
+                throw new CmsInvalidDataException("$val is an invalid value for $key in ".__CLASS__);
             }
             break;
 
-        case 'show_thumbs':
-        case 'show_hidden':
         case 'sort':
-            $this->_data[$key] = (bool) $val;
+           //TODO relevant checks
+            $this->_data[$key] = $val;
             break;
-        }
-    }
 
-    /**
-     * Constructor
-     *
-     * @param array $params An associative array of params suitable for hte setValue method.
-     */
-    public function __construct( array $params = null )
-    {
-        if( !is_array($params) || !count($params) ) return;
-        foreach( $params as $key => $val ) {
-            $this->setValue($key,$val);
-        }
-    }
-
-    /**
-     * @ignore
-     */
-    public function __get($key)
-    {
-        switch( $key ) {
-        case 'top':
-        case 'type':
-        case 'match_prefix':
-        case 'exclude_prefix':
-            return trim($this->_data[$key]);
-
-        case 'can_mkdir':
-        case 'can_upload':
-        case 'can_delete':
-            return (int) $this->_data[$key];
-
-        case 'show_thumbs':
-        case 'show_hidden':
-        case 'sort':
-            return (bool) $this->_data[$key];
+        case 'exclude_groups':
+        case 'exclude_users':
+        case 'match_groups':
+        case 'match_users':
+           //TODO relevant checks
+            $this->_data[$key] = $val;
+            break;
         }
     }
 
