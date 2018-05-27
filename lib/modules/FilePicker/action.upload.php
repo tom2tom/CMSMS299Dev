817624e45641e695701e497da2b097366049e2e0
tom2tom <tpgww@onepost.net>
2018-05-27 15:39:53 +1000

    cleanup license specify default upload path

--- lib/modules/FilePicker/action.upload.php
+++ lib/modules/FilePicker/action.upload.php
@@ -1,50 +1,37 @@
 <?php
-#-------------------------------------------------------------------------
-# Module: FilePicker - A CMSMS addon module to provide file picking capabilities.
-# (c) 2016 by Fernando Morgado <jomorg@cmsmadesimple.org>
-# (c) 2016 by Robert Campbell <calguy1000@cmsmadesimple.org>
-#-------------------------------------------------------------------------
-# CMS - CMS Made Simple is (c) 2006 by Ted Kulp (wishy@cmsmadesimple.org)
-# This project's homepage is: http://www.cmsmadesimple.org
-#-------------------------------------------------------------------------
-#-------------------------------------------------------------------------
-# BEGIN_LICENSE
-#-------------------------------------------------------------------------
-# This file is part of FilePicker
-# FilePicker is free software; you can redistribute it and/or modify
+# FilePicker module action: upload
+# Copyright (C) 2016 Fernando Morgado <jomorg@cmsmadesimple.org>
+# Copyright (C) 2016-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# FilePicker is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-#-------------------------------------------------------------------------
-# END_LICENSE
-#-------------------------------------------------------------------------
 
 if( !defined('CMS_VERSION') ) exit;
 
-# just for tests purposes
-#this will be handled differently 
-$fn = cms_join_path($this->GetModulePath(), 'lib',  'ext', 'UploadHandler.php');
-require_once($fn);
+$devmode = $this->CheckPermission('Modify Site Code') || !empty($config['developer_mode']);
+$base = ( $devmode ) ? CMS_ROOT_PATH : $config['uploads_path'];
+//TOOO some $param for relative path
 
-$UploadHandler = new UploadHandler();
+$UploadHandler = new FilePicker\UploadHandler(['module'=>$this, 'upload_dir' => $base]);
 
 header('Pragma: no-cache');
 header('Cache-Control: private, no-cache');
 header('Content-Disposition: inline; filename="files.json"');
 header('X-Content-Type-Options: nosniff');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: OPTIONS, HEAD, GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Methods: OPTIONS, HEAD, GET, POST, DELETE');
 header('Access-Control-Allow-Headers: X-File-Name, X-File-Type, X-File-Size');
 
-switch ($_SERVER['REQUEST_METHOD']) 
-{
+switch( $_SERVER['REQUEST_METHOD'] ) {
 	case 'OPTIONS':
         break;
 	case 'HEAD':
@@ -62,8 +49,3 @@ switch ($_SERVER['REQUEST_METHOD'])
 }
 
 exit;
-
-#
-# EOF
-#
-?>
