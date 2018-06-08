70d14f007172a1b90cd41fa591e8b2a261e9ab3b
tom2tom <tpgww@onepost.net>
2018-06-08 12:02:49 +1000

    upgrade 901

---
+++ phar_installer/app/upgrade/2.2.910/upgrade.php
@@ -0,0 +1,31 @@
+<?php
+
+$dbdict = GetDataDictionary($db);
+$taboptarray = array('mysqli' => 'ENGINE=MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci');
+
+$flds = '
+category_id I NOTNULL,
+tpl_id I NOTNULL,
+tpl_order I(4) DEFAULT 0
+';
+$sqlarray = $dbdict->CreateTableSQL(
+ CMS_DB_PREFIX.CmsLayoutTemplateCategory::TPLTABLE,
+ $flds,
+ $taboptarray
+);
+$return = $dbdict->ExecuteSQLArray($sqlarray);
+verbose_msg(ilang('install_created_table', CmsLayoutTemplateCategory::TPLTABLE, $msg_ret));
+
+$sqlarray = $dbdict->CreateIndexSQL(
+ CMS_DB_PREFIX.'idx_layout_cat_tplasoc_1',
+ CMS_DB_PREFIX.CmsLayoutTemplateCategory::TPLTABLE,
+ 'tpl_id'
+);
+$return = $dbdict->ExecuteSQLArray($sqlarray);
+$msg_ret = ($return == 2) ? ilang('done') : ilang('failed');
+verbose_msg(ilang('install_creating_index', 'idx_layout_cat_tplasoc_1', $msg_ret));
+
+if ($return == 2) {
+    $query = 'INSERT INTO '.CMS_DB_PREFIX.'version VALUES (205)';
+    $db->Execute($query);
+}
