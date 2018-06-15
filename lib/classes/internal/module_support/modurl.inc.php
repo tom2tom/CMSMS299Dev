07530de82d7c056645c04fbe96b4937cef40262f
tom2tom <tpgww@onepost.net>
2018-06-15 19:21:45 +1000

    update API text omit redundant methods

--- lib/classes/internal/module_support/modurl.inc.php
+++ lib/classes/internal/module_support/modurl.inc.php
@@ -35,17 +35,18 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
  *   default content block of the destination frontend-page is to be targeted)
  * @param string $action	The module action name
  * Optional parameters
- * @param mixed $returnid The integer page-id to return to after the action
- *   is done, or ''/null for admin
- * @param array $params	Parameters to include in the URL.
+ * @param mixed $returnid Optional integer page-id to return to after the action
+ *   is done, or ''|null for admin. Default null.
+ * @param array $params	Optional parameters to include in the URL. Default []
  *   These will be ignored if the prettyurl parameter is present
  * @param string $prettyurl URL segment(s) relative to the root-URL of the
  *   action, to generate a pretty-formatted URL
- * @param bool	$inline	Whether the target of the output link is the same
- *   tag on the same page
- * @param bool	$targetcontentonly Whether the target of the output link
- *   targets the content area of the destination page
- * @param int    $mode since 2.3 Indicates how to format the url
+ * @param bool	$inline	Optional flag whether the target of the output link
+ *   is the same tag on the same page Default false
+ * @param bool	$targetcontentonly Optional flag whether the target of the
+ *   output link targets the content area of the destination page Default false
+ * @param string $prettyurl Optional url part(s) Default ''
+ * @param int    $mode since 2.3 Optional indicator for how to format the url
  *  0 = (default) rawurlencoded parameter keys and values, '&amp;' for parameter separators
  *  1 = raw: no rawurlencoding, '&' for parameter separators - e.g. for use in js
  *  2 = page-displayable: all html_entitized, probably not usable as-is
@@ -56,7 +57,7 @@ function cms_module_create_actionurl(
 	&$modinstance,
 	string $id,
 	string $action,
-	$returnid = '',
+	$returnid = null,
 	array $params = [],
 	bool $inline = false,
 	bool $targetcontentonly = false,
@@ -87,7 +88,8 @@ function cms_module_create_actionurl(
 	} elseif ($prettyurl && $config['url_rewriting'] == 'internal') {
 		$text = $base_url.'/index.php/'.$prettyurl.$config['page_extension'];
 	} else {
-		if ($targetcontentonly || ($returnid != '' && !$inline)) {
+        $frontend = is_int($returnid);
+		if ($targetcontentonly || ($frontend && !$inline)) {
 			$id = 'cntnt01';
 		}
 
@@ -95,7 +97,7 @@ function cms_module_create_actionurl(
 		'mact' => $modinstance->GetName().','.$id.','.$action.','.($inline ? 1 : 0)
 		];
 
-		if ($returnid == '') {
+		if (!$frontend) {
 /*TODO			if (!isset($_SESSION[CMS_USER_KEY])) {
 				return '<!-- '.__METHOD__.' error : "session has expired -->';
 			}
@@ -116,7 +118,7 @@ function cms_module_create_actionurl(
 				$parms[$id.$key] = $value;
 			}
 		}
-		if ($returnid != '') {
+		if ($frontend) {
 			$parms[$id.'returnid'] = $returnid;
 			if ($inline) {
 				$parms[$id.$config['query_var']] = $returnid;
@@ -160,18 +162,18 @@ function cms_module_create_actionurl(
  *
  * Get an URL representing a site page
  *
- * @param string $id		  The module action-id (e.g. 'cntnt01'indicates that the
- *   default content block of the destination frontend-page is to be targeted)
+ * @param string $id		  The module action-id (e.g. 'cntnt01' indicates that
+ *   the default content block of the destination frontend-page is to be targeted)
  * @param mixed $returnid The integer page-id to return to after the action
- *   is done, or ''/null for admin
+ *   is done, or ''|null for admin
  * @param array $params	  Optional array of parameters to include in the URL.
- * @param int   $mode since 2.3 Indicates how to format the url
+ * @param int   $mode since 2.3 Optional indicator for how to format the url
  *  0 = (default) rawurlencoded parameter keys and values, '&amp;' for parameter separators
  *  1 = raw: as for 0, except '&' for parameter separators - e.g. for use in js
  *  2 = page-displayable: all html_entitized, probably not usable as-is
  * @return string
  */
-function cms_module_create_pageurl($id, $returnid, array $params = [], int $mode = 0) : string
+function cms_module_create_pageurl(string $id, $returnid, array $params = [], int $mode = 0) : string
 {
 	$text = '';
 	$gCms = \CmsApp::get_instance();
@@ -182,7 +184,6 @@ function cms_module_create_pageurl($id, $returnid, array $params = [], int $mode
 		if ($content) { //CHECKME
 			$pageurl = $content->GetURL();
 			if ($pageurl) {
-
 				switch ($mode) {
 					case 1:
 						$sep = '&';
@@ -227,81 +228,4 @@ function cms_module_create_pageurl($id, $returnid, array $params = [], int $mode
 		}
 	}
 	return $text;
-}
-
-/**
- * @internal
- */
-function cms_module_create_joburl(&$modinstance, string $action, array $params = [], bool $secure = true) : string
-{
-	$modname = $modinstance->GetName();
-	$id = 'aj_';
-
-	$text = CMS_ROOT_URL.'/jobinterface.php?mact='.$modname.','.$id.','.$action.',0';
-
-	if ($secure) {
-		$text .= cms_module_create_secure_params($modinstance);
-	}
-
-	if ($params) {
-		$ignores = ['assign', 'id', 'returnid', 'action', 'module', CMS_SECURE_PARAM_NAME];
-		foreach ($params as $key => $value) {
-			if (!in_array($key, $ignores)) {
-				$text .= '&'.$id.rawurlencode($key).'='.rawurlencode($value);
-			}
-		}
-	}
-
-	return $text;
-}
-
-/**
- * @access private
- */
-function cms_module_create_secure_params(&$modinstance)
-{
-	$modname = $modinstance->GetName();
-	$id = 'aj_';
-
-	$key = uniqid($modname, true);
-	// djb2a hash
-	$bytes = array_values(unpack('C*', $key)); //actual byte-length (i.e. no mb interference);
-	$bc = count($bytes);
-	$h = 5381;
-	for ($i = 0; $i < $bc; ++$i) {
-		$h = ($h + ($h << 5)) ^ $bytes[$i];
-	}
-	$token = dechex($h);
-	$def = '/V^^V\\'.microtime();
-	while (1) {
-		$key = str_shuffle($key);
-		$subkey = substr($key, 0, 6);
-		if ($modinstance->GetPreference($subkey, $def) == $def) {
-			$modinstance->SetPreference($subkey, $token);
-			break;
-		}
-	}
-	$text = '&'.$id.CMS_SECURE_PARAM_NAME.'='.$subkey.'&'.$id.$subkey.'='.$token;
-	return $text;
-}
-
-/**
- * @access private
- */
-function cms_module_check_secure_params(&$modinstance, $params)
-{
-	if (empty($params[CMS_SECURE_PARAM_NAME])) {
-		return false;
-	}
-	$key = $params[CMS_SECURE_PARAM_NAME];
-	$def = '/V^^V\\'.microtime();
-	$log = $modinstance->GetPreference($key, $def);
-	if ($log == $def) {
-		return false;
-	}
-	$modinstance->RemovePreference($key);
-	if (empty($params[$key])) {
-		return false;
-	}
-	return $log == $params[$key];
 }
