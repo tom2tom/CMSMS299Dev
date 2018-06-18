9c043da3d8b2a741aae0cf38d6de49e733e267b3
tom2tom <tpgww@onepost.net>
2018-06-18 20:09:08 +1000

    omit redundant check

--- lib/modules/CMSContentManager/method.install.php
+++ lib/modules/CMSContentManager/method.install.php
@@ -1,6 +1,5 @@
 <?php
-#-------------------------------------------------------------------------
-# CMSContentManager - A CMSMS module to provide page-content management.
+# CMSContentManager module installation process
 # Copyright (C) 2013-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
 # This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
@@ -15,7 +14,7 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-#-------------------------------------------------------------------------
+
 if (!isset($gCms)) {
     exit;
 }
@@ -29,20 +28,16 @@ $this->CreatePermission('Modify Any Page', $this->Lang('perm_modify'));
 $this->CreatePermission('Remove Pages', $this->Lang('perm_remove'));
 $this->CreatePermission('Reorder Content', $this->Lang('perm_reorder'));
 
-//CHECKME DesignManager race when 1st installing?
-$group_id = $db->GetOne('SELECT group_id FROM '.CMS_DB_PREFIX."groups WHERE group_name = 'Editor'");
-if (!$group_id) {
-	$editor_group = new Group();
-	$editor_group->name = 'Editor';
-	$editor_group->description = $this->Lang('group_desc');
-	$editor_group->active = 1;
-	CMSMS\HookManager::do_hook('Core::AddGroupPre', ['group'=>&$editor_group]);
-	$editor_group->Save();
-	CMSMS\HookManager::do_hook('Core::AddGroupPost', ['group'=>&$editor_group]);
+$editor_group = new Group();
+$editor_group->name = 'Editor';
+$editor_group->description = $this->Lang('group_desc');
+$editor_group->active = 1;
+CMSMS\HookManager::do_hook('Core::AddGroupPre', ['group'=>&$editor_group]);
+$editor_group->Save();
+CMSMS\HookManager::do_hook('Core::AddGroupPost', ['group'=>&$editor_group]);
 
-	$editor_group->GrantPermission('Manage All Content');
-	$editor_group->GrantPermission('Manage My Account');
-	$editor_group->GrantPermission('Manage My Bookmarks');
-	$editor_group->GrantPermission('Manage My Settings');
-	$editor_group->GrantPermission('View Tag Help');
-}
+$editor_group->GrantPermission('Manage All Content');
+$editor_group->GrantPermission('Manage My Account');
+$editor_group->GrantPermission('Manage My Bookmarks');
+$editor_group->GrantPermission('Manage My Settings');
+$editor_group->GrantPermission('View Tag Help');
