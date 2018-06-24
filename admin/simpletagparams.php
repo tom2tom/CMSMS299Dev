a90acbcbba21eb2831ed2032aad4d604bac7ea87
tom2tom <tpgww@onepost.net>
2018-06-24 21:56:22 +1000

    add operation for ajax processing

---
+++ admin/simpletagparams.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Procedure to process ajax calls for retrieving parameters-information
+for a named User Defined Tag (aka simple plugin)
+Copyright (C) 2018 The CMSMS Dev Team <coreteam@cmsmadesimple.org>
+This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use CMSMS\SimplePluginOperations;
+
+$CMS_ADMIN_PAGE=1;
+
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'include.php';
+
+$userid = get_userid();
+if (check_permission($userid, 'View Tag Help')) {
+	$ops = SimplePluginOperations::get_instance();
+	$name = cleanValue($_GET['name']);
+	$meta = $ops->get_meta_data($name, 'parameters');
+	if (!empty($meta)) {
+		echo (nl2br(cms_htmlentities(trim($meta, " \t\n\r"))));
+	}
+}
+exit;
