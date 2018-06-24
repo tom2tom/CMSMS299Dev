f95fc56f4a8a3f2bd1e7057c0ff3e68a33583d02
tom2tom <tpgww@onepost.net>
2018-06-24 14:05:20 +1000

    add UDT-delete operation

---
+++ admin/deletesimpletag.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Procedure to delete a named User Defined Tag (aka simple plugin) file
+Copyright (C) 2018 The CMSMS Dev Team <coreteam@cmsmadesimple.org>
+This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use CMSMS\SimpleTagOperations;
+
+$CMS_ADMIN_PAGE=1;
+
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'include.php';
+
+check_login();
+
+$userid = get_userid();
+$pmod = check_permission($userid, 'Modify Simple Plugins');
+if (!$pmod) exit;
+
+$themeObject = cms_utils::get_theme_object();
+
+$tagname = cleanValue($_GET('name'));
+$ops = SimpleTagOperations::get_instance();
+$fp = $ops->file_path($tagname);
+if (is_file($fp)) {
+//? send event :: deleteuserdefinedtagpre
+    if ($ops->delete($tagname)) {
+        $themeObject->RecordNotice('success', lang('deleted_udt'));
+//? send event :: deleteuserdefinedtagpost
+    } else {
+        $themeObject->RecordNotice('error', lang('errordeletag'));
+    }
+} else {
+    $themeObject->RecordNotice('error', lang('error_internal'));
+}
+
+$urlext = '?'.CMS_SECURE_PARAM_NAME.'='.$_SESSION[CMS_USER_KEY];
+redirect('listsimpletags.php'.$urlext);
