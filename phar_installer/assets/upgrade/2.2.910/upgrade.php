9784f41373283ffd37c8a7ccb63056b77a6b8306
tom2tom <tpgww@onepost.net>
2018-06-25 12:35:13 +1000

    handle sequences, permissions

--- phar_installer/assets/upgrade/2.2.910/upgrade.php
+++ phar_installer/assets/upgrade/2.2.910/upgrade.php
@@ -1,7 +1,10 @@
 <?php
+// redundant sequence-tables
+$db->DropSequence(CMS_DB_PREFIX.'content_props_seq');
+$db->DropSequence(CMS_DB_PREFIX.'userplugins_seq');
 
 $dbdict = GetDataDictionary($db);
-$taboptarray = array('mysqli' => 'ENGINE=MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci');
+$taboptarray = ['mysqli' => 'ENGINE=MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci'];
 
 $sqlarray = $dbdict->AddColumnSQL(CMS_DB_PREFIX.CmsLayoutCollection::TPLTABLE,'tpl_order I(4) DEFAULT 0');
 $dbdict->ExecuteSQLArray($sqlarray);
@@ -31,4 +34,16 @@ verbose_msg(ilang('install_creating_index', 'idx_layout_cat_tplasoc_1', $msg_ret
 if ($return == 2) {
     $query = 'INSERT INTO '.CMS_DB_PREFIX.'version VALUES (205)';
     $db->Execute($query);
+}
+
+//extra permissions
+foreach( [
+ 'Modify Simple Plugins',
+ 'Modify Site Code',
+] as $one_perm ) {
+  $permission = new CmsPermission();
+  $permission->source = 'Core';
+  $permission->name = $one_perm;
+  $permission->text = $one_perm;
+  $permission->save();
 }
