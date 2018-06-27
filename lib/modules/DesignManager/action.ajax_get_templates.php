d474b3e8d2159a7bfd9b747df904ff27417b38ee
tom2tom <tpgww@onepost.net>
2018-06-27 17:19:45 +1000

    add license short array syntax

--- lib/modules/DesignManager/action.ajax_get_templates.php
+++ lib/modules/DesignManager/action.ajax_get_templates.php
@@ -1,7 +1,22 @@
 <?php
+# DesignManager module action: process ajax call to populate templates
+# Copyright (C) 2012-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 $handlers = ob_list_handlers();
-for ($cnt = 0; $cnt < sizeof($handlers); $cnt++) { ob_end_clean(); }
+for ($cnt = 0, $n = sizeof($handlers); $cnt < $n; $cnt++) { ob_end_clean(); }
 
 try {
     $tmp = get_parameter_value($_REQUEST,'filter');
@@ -13,7 +28,7 @@ try {
     $templates = $tpl_query->GetMatches();
     if( count($templates) ) {
         $smarty->assign('templates',$templates);
-        $tpl_nav = array();
+        $tpl_nav = [];
         $tpl_nav['pagelimit'] = $tpl_query->limit;
         $tpl_nav['numpages'] = $tpl_query->numpages;
         $tpl_nav['numrows'] = $tpl_query->totalrows;
@@ -24,7 +39,7 @@ try {
     $designs = CmsLayoutCollection::get_all();
     if( count($designs) ) {
         $smarty->assign('list_designs',$designs);
-        $tmp = array();
+        $tmp = [];
         for( $i = 0; $i < count($designs); $i++ ) {
             $tmp['d:'.$designs[$i]->get_id()] = $designs[$i]->get_name();
             $tmp2[$designs[$i]->get_id()] = $designs[$i]->get_name();
@@ -33,11 +48,11 @@ try {
     }
 
     $types = CmsLayoutTemplateType::get_all();
-    $originators = array();
+    $originators = [];
     if( count($types) ) {
-        $tmp = array();
-        $tmp2 = array();
-        $tmp3 = array();
+        $tmp = [];
+        $tmp2 = [];
+        $tmp3 = [];
         for( $i = 0; $i < count($types); $i++ ) {
             $tmp['t:'.$types[$i]->get_id()] = $types[$i]->get_langified_display_value();
             $tmp2[$types[$i]->get_id()] = $types[$i]->get_langified_display_value();
@@ -50,7 +65,7 @@ try {
         $smarty->assign('list_types',$tmp2);
     }
 
-    $locks = \CmsLockOperations::get_locks('template');
+    $locks = CmsLockOperations::get_locks('template');
     $smarty->assign('have_locks',$locks ? count($locks) : 0);
     $smarty->assign('lock_timeout', $this->GetPreference('lock_timeout'));
     $smarty->assign('coretypename',CmsLayoutTemplateType::CORE);
@@ -67,5 +82,3 @@ catch( Exception $e ) {
     // nothing here
 }
 exit;
-
-?>
