c87a345f770a2a07806a8404a141f22424790a46
tom2tom <tpgww@onepost.net>
2018-07-12 20:41:22 +1000

    use translated permission-title

--- lib/modules/AdminSearch/method.install.php
+++ lib/modules/AdminSearch/method.install.php
@@ -1,6 +1,5 @@
 <?php
-#-------------------------------------------------------------------------
-# Module: AdminSearch - A CMSMS addon module to provide admin side search capbilities.
+# AdminSearch module intallation process
 # Copyright (C) 2012-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
 # This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
@@ -15,10 +14,8 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-#
-#-------------------------------------------------------------------------
 
-$this->CreatePermission('Use Admin Search','Use Admin Search');
+$this->CreatePermission('Use Admin Search',$this->Lang('perm_Use_Admin_Search'));
 
 $groupops = GroupOperations::get_instance();
 $groups = $groupops->LoadGroups();
@@ -28,7 +25,4 @@ if( is_array($groups) && count($groups) ) {
     $one_group->GrantPermission('Use Admin Search');
   }
 }
-#
-# EOF
-#
-?>
+
