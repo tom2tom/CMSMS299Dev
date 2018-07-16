c54f025aa1aaa296c6f1a75dd26e87b0b3166245
tom2tom <tpgww@onepost.net>
2018-07-17 00:05:45 +1000

    migrate to service-provision interface

--- lib/modules/CoreAdminLogin/CoreAdminLogin.module.php
+++ lib/modules/CoreAdminLogin/CoreAdminLogin.module.php
@@ -1,36 +1,38 @@
 <?php
-#Module: admin login/out
-#Copyright (C) 2018 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
-#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
-#
-#This program is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#(at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#BUT WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License
-#along with this program. If not, see <https://www.gnu.org/licenses/>.
+/*
+Module: CoreAdminLogin - supports themes' login/out processes
+Copyright (C) 2018 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+BUT WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use CMSMS\internal\Smarty;
 
 /**
- * Module: admin login/out
+ * Module: admin login/out processor
  * @since 2.3
  * @package CMS
  * @license GPL
  */
 
-class CoreAdminLogin extends CMSModule
+class CoreAdminLogin extends CMSModule //uses CMSMS\AdminLogin
 {
-    public $loginaction = 'admin_login';
-    public $logoutaction = 'admin_login';
-    // mimimal methods used by metafile processor
-    public function GetName() { return 'CoreAdminLogin'; }
-    public function GetVersion() { return '0.0.2'; }
+    // minimum methods used by metafile processor
     public function GetAuthor() { return 'Robert Campbell'; }
     public function GetAuthorEmail() { return 'calguy1000@cmsmadesimple.org'; }
+    public function GetName() { return 'CoreAdminLogin'; }
+    public function GetVersion() { return '0.2'; }
     public function IsAdminOnly() { return true; }
 
     public function GetChangeLog()
@@ -43,29 +45,55 @@ class CoreAdminLogin extends CMSModule
         return ''.@file_get_contents(cms_join_path(__DIR__,'lib','doc','modhelp.htm'));
     }
 
-    public function DoAction($name, $id, $params, $returnid = '')
+    public function HasCapability($capability, $parts = [])
     {
-        switch( $name ) {
-            case 'default':
-            case 'login':
-                $name = $this->loginaction;
-                break;
-            case 'logout':
-                $name = $this->logoutaction;
-                break;
-        }
-        return parent::DoAction($name, $id, $params, $returnid);
+        if ($capability == 'adminlogin') return true;
     }
 
-    public function HasCapability($capability, $params = [])
+    // interface methods
+
+    /**
+     * @return stuff usable upstream (but only if further processing is needed)
+     */
+    public function ProcessRequest() : array
     {
-        if( $capability == 'adminlogin' ) return true;
+        //parameters for included code
+        $usecsrf = true;
+        $config = cms_config::get_instance();
+
+        $fp = cms_join_path(__DIR__, 'function.login.php');
+        require_once $fp;
+
+        $smarty = Smarty::get_instance();
+
+        $smarty->assign('mod', $this);
+        $csrf = bin2hex(random_bytes(16));
+        $smarty->assign('csrf', $csrf);
+        $smarty->assign('changepwhash', $changepwhash ?? '');
+
+        $parts = [];
+        //some results from the included function
+        $parts['infomessage'] = $infomessage ?? '';
+        $parts['warnmessage'] = $warnmessage ?? '';
+        $parts['errmessage'] = $errmessage ?? '';
+        $smarty->assign($parts);
+
+        $saved = $smarty->template_dir;
+        $smarty->template_dir = __DIR__ . DIRECTORY_SEPARATOR . 'templates';
+        $form = $smarty->fetch('core.tpl');
+        $smarty->template_dir = $saved;
+
+        $_SESSION[$csrf_key] = $csrf;
+        //$themeobj = cms_utils::get_theme_object();
+        //TODO setup any specific themeobject header / footer inclusions
+
+        $parts['changepwtoken'] = $changepwtoken ?? '';
+        $parts['form'] = $form;
+
+        return $parts;
     }
 
-    protected function getLoginUtils()
-    {
-        static $_obj = null;
-        if( !$_obj ) $_obj = new \CoreAdminLogin\LoginUtils( $this );
-        return $_obj;
-    }
+    public function DoLogin() {}
+
+    public function GrantAccess() {}
 } // class
