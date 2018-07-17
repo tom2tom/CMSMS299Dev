a3eb343d9ded178416b1b832cbca1b99e48f8fc3
tom2tom <tpgww@onepost.net>
2018-07-17 10:17:35 +1000

    conform to revised login/out api

--- admin/login.php
+++ admin/login.php
@@ -1,1 +1,28 @@
-login-NEW.php
+<?php
+#CMSMS admin-login processing
+#Copyright (C) 2018 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+$CMS_ADMIN_PAGE=1;
+$CMS_LOGIN_PAGE=1;
+
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'include.php';
+
+$ob = cms_utils::get_theme_object();
+if ($ob) {
+    $ob->do_login();
+} else {
+    die('System error');
+}
