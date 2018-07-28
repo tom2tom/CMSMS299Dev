73a8e1ca97e4e7e352f1c26dbab52c4e849a1f0a
tom2tom <tpgww@onepost.net>
2018-07-28 10:39:26 +1000

    cleanup license

--- lib/modules/FilePicker/method.uninstall.php
+++ lib/modules/FilePicker/method.uninstall.php
@@ -1,31 +1,22 @@
 <?php
-#-------------------------------------------------------------------------
-# Module: FilePicker - A CMSMS addon module to provide file picking capabilities.
-# (c) 2016 by Fernando Morgado <jomorg@cmsmadesimple.org>
-# (c) 2016 by Robert Campbell <calguy1000@cmsmadesimple.org>
-#-------------------------------------------------------------------------
-# CMS - CMS Made Simple is (c) 2006 by Ted Kulp (wishy@cmsmadesimple.org)
-# This project's homepage is: http://www.cmsmadesimple.org
-#-------------------------------------------------------------------------
-#-------------------------------------------------------------------------
-# BEGIN_LICENSE
-#-------------------------------------------------------------------------
-# This file is part of FilePicker
-# FilePicker is free software; you can redistribute it and/or modify
+# Module: FilePicker - A CMSMS addon module to provide various support services.
+# Copyright (C) 2016 Fernando Morgado <jomorg@cmsmadesimple.org>
+# Copyright (C) 2016-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# FilePicker is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-#-------------------------------------------------------------------------
-# END_LICENSE
-#-------------------------------------------------------------------------
-use \FilePicker\ProfileDAO;
+
+use FilePicker\ProfileDAO;
 
 $dict = NewDataDictionary( $db );
 $sqlarray = $dict->DropTableSQL(ProfileDAO::table_name());
