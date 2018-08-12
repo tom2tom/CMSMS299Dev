4a108ecc5bfdbbb09745ab23ea55ab1c3c374777
tom2tom <tpgww@onepost.net>
2018-08-12 14:23:06 +1000

    re-factored individual class files

---
+++ lib/modules/CMSContentManager/lib/class.ContentListFilter.php
@@ -0,0 +1,90 @@
+<?php
+# Class defining a content filter
+# Copyright (C) 2013-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+namespace CMSContentManager;
+
+use InvalidArgumentException;
+
+/**
+ * A simple class for defining a content filter
+ * @package CMS
+ * @internal
+ * @ignore
+ * @final
+ * @author Robert Campbell
+ * @copyright Copyright (c) 2013, Robert Campbell <calguy1000@cmsmadesimple.org>
+ */
+final class ContentListFilter
+{
+	const EXPR_OWNER = 'OWNER_UID';
+	const EXPR_EDITOR = 'EDITOR_UID';
+	const EXPR_TEMPLATE = 'TEMPLATE_ID';
+	const EXPR_DESIGN = 'DESIGN_ID';
+
+	private $_type;
+	private $_expr; // string
+
+	/**
+	 *
+	 * @param mixed $key
+	 * @return mixed
+	 * @throws InvalidArgumentException upon unrecognized $key
+	 */
+	public function __get($key)
+	{
+		switch( $key ) {
+		case 'type':
+		case 'expr':
+			$key = '_'.$key;
+			return $this->$key;
+
+		default:
+			throw new InvalidArgumentException("$key is not a gettable member of ".__CLASS__);
+		}
+	}
+
+	/**
+	 *
+	 * @param mixed $key, normally a string
+	 * @param mixed $val, normally a string
+	 * @throws InvalidArgumentException upon unrecognized $key or $val
+	 */
+	public function __set($key,$val)
+	{
+		switch( $key ) {
+		case 'type':
+			switch( $val ) {
+			case self::EXPR_OWNER:
+			case self::EXPR_EDITOR:
+			case self::EXPR_TEMPLATE:
+			case self::EXPR_DESIGN:
+				$this->_type = $val;
+				break;
+			default:
+				throw new InvalidArgumentException("$val is an invalid type for ".__CLASS__);
+			}
+			break;
+
+		case 'expr':
+			$this->_expr = trim($val);
+			break;
+
+		default:
+			throw new InvalidArgumentException("$key is not a settable member of ".__CLASS__);
+		}
+	}
+} // class
---
+++ lib/modules/CMSContentManager/lib/class.ContentListQuery.php
@@ -0,0 +1,120 @@
+<?php
+# Class for building content lists
+# Copyright (C) 2013-2018 Robert Campbell <calguy1000@cmsmadesimple.org>
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+namespace CMSContentManager;
+
+use cms_utils;
+use CmsDbQueryBase;
+use CmsLogicException;
+use CmsSQLErrorException;
+use const CMS_DB_PREFIX;
+
+/**
+ * A class for building content lists
+ * @package CMS
+ * @internal
+ * @ignore
+ * @final
+ * @author Robert Campbell
+ * @copyright Copyright (c) 2013, Robert Campbell <calguy1000@cmsmadesimple.org>
+ */
+final class ContentListQuery extends CmsDbQueryBase
+{
+	protected $_filter;
+
+	/**
+	 *
+	 * @param ContentListFilter $filter
+	 */
+	public function __construct(ContentListFilter $filter)
+	{
+		$this->_filter = $filter;
+		$this->_limit = 1000;
+		$this->_offset = 0;
+	}
+
+	/**
+	 * @param int $limit
+	 */
+	public function set_limit(int $limit)
+	{
+		$this->_limit = max(1,$offset);
+	}
+
+	/**
+	 *
+	 * @param int $offset
+	 */
+	public function set_offset(int $offset)
+	{
+		$this->_offset = max(0,$offset);
+	}
+
+	/**
+	 *
+	 * @throws CmsSQLErrorException
+	 */
+	public function execute()
+	{
+		if( $this->_rs ) return;
+
+		$sql = 'SELECT SQL_CALC_FOUND_ROWS C.content_id FROM '.CMS_DB_PREFIX.'content C';
+		$where = $parms = [];
+		switch( $this->_filter->type ) {
+		case ContentListFilter::EXPR_OWNER:
+			$where[] = 'C.owner_id = ?';
+			$parms[] = (int) $this->_filter->expr;
+			break;
+		case ContentListFilter::EXPR_EDITOR:
+			$sql .= ' INNER JOIN '.CMS_DB_PREFIX.'additional_users A ON C.content_id = A.content_id AND A.user_id = ?';
+			$parms[] = (int) $this->_filter->expr;
+			break;
+		case ContentListFilter::EXPR_TEMPLATE:
+			$where[] = 'C.template_id = ?';
+			$parms[] = (int) $this->_filter->expr;
+			break;
+		case ContentListFilter::EXPR_DESIGN:
+			$sql .= ' INNER JOIN '.CMS_DB_PREFIX.'content_props P ON C.content_id = P.content_id AND P.prop_name = ?';
+			$parms[] = 'design_id';
+			$where[] = 'P.content = ?';
+			$parms[] = (int) $this->_filter->expr;
+			break;
+		}
+
+		if( count($where) ) $sql .= ' WHERE '.implode(' AND ',$where);
+		$sql .= ' ORDER BY C.id_hierarchy';
+
+		$db = cms_utils::get_db();
+		$this->_rs = $db->SelectLimit($sql,$this->_limit,$this->_offset,$parms);
+		if( $db->ErrorMsg() != '' ) throw new CmsSQLErrorException($db->sql.' -- '.$db->ErrorMsg());
+		$this->_totalmatchingrows = $db->GetOne('SELECT FOUND_ROWS()');
+	}
+
+	/**
+	 *
+	 * @return type
+	 * @throws CmsLogicException
+	 */
+	public function &GetObject()
+	{
+		$this->execute();
+		if( !$this->_rs ) throw new CmsLogicException('Cannot get stylesheet from invalid stylesheet query object');
+
+		$out = (int) $this->_rs->fields['content_id'];
+		return $out;
+	}
+} // class
