87e6f68299aecb4a1f039beadd69ba81b463906f
tom2tom <tpgww@onepost.net>
2018-08-29 15:03:57 +1000

    namespace cleanup

--- lib/modules/FilePicker/lib/class.PathAssistant.php
+++ lib/modules/FilePicker/lib/class.PathAssistant.php
@@ -1,48 +1,58 @@
 <?php
+
 namespace FilePicker;
+
+use cms_config;
+use LogicException;
+use const CMS_ROOT_PATH;
+use const CMS_ROOT_URL;
+use const CMS_UPLOADS_URL;
+use function cms_join_path;
+use function endswith;
+use function startswith;
 
 class PathAssistant
 {
     private $_topdir;
     private $_topurl;
 
-    public function __construct(\cms_config $config, $topdir)
+    public function __construct(cms_config $config, $topdir)
     {
-        if( !$topdir || !is_dir($topdir) ) throw new \LogicException("Invalid topdir $topdir passed to ".__METHOD__);
-        if( !$this->is_relative_to( $topdir, CMS_ROOT_PATH ) ) throw new \LogicException('Invalid topdir passed to '.__METHOD__);
+        if (!$topdir || !is_dir($topdir)) throw new LogicException("Invalid topdir $topdir passed to ".__METHOD__);
+        if (!$this->is_relative_to($topdir, CMS_ROOT_PATH)) throw new LogicException('Invalid topdir passed to '.__METHOD__);
 
-        if( endswith( $topdir, '/') ) $topdir = substr($topdir,0,-1);
+        if (endswith($topdir, DIRECTORY_SEPARATOR)) $topdir = substr($topdir,0,-1);
         $this->_topdir = trim($topdir);
 
         // now, look at the image uplaods path, the image path, the admin path, and the root path
-        if( $this->is_relative_to( $this->_topdir, $config['image_uploads_path']) ) {
-            $rel_url = $this->to_relative_sub( $this->_topdir, $config['image_uploads_path'] );
+        if ($this->is_relative_to($this->_topdir, $config['image_uploads_path'])) {
+            $rel_url = $this->to_relative_sub($this->_topdir, $config['image_uploads_path']);
             $this->_topurl = $config['image_uploads_url'].'/'.$rel_url;
         }
-        else if( $this->is_relative_to( $this->_topdir, $config['uploads_path']) ) {
-            $rel_url = $this->to_relative_sub( $this->_topdir, $config['uploads_path'] );
-            $this->_topurl = $config['uploads_url'].'/'.$rel_url;
+        elseif ($this->is_relative_to($this->_topdir, $config['uploads_path'])) {
+            $rel_url = $this->to_relative_sub($this->_topdir, $config['uploads_path']);
+            $this->_topurl = CMS_UPLOADS_URL.'/'.$rel_url;
         }
-        else if( $this->is_relative_to( $this->_topdir, $config['admin']) ) {
-            $rel_url = $this->to_relative_sub( $this->_topdir, $config['admin'] );
+        elseif ($this->is_relative_to($this->_topdir, $config['admin'])) {
+            $rel_url = $this->to_relative_sub($this->_topdir, $config['admin']);
             $this->_topurl = $config['admin_url'].'/'.$rel_url;
         }
-        else if( $this->is_relative_to( $this->_topdir, CMS_ROOT_PATH) ) {
-            $rel_url = $this->to_relative_sub( $this->_topdir, CMS_ROOT_PATH );
+        elseif ($this->is_relative_to($this->_topdir, CMS_ROOT_PATH)) {
+            $rel_url = $this->to_relative_sub($this->_topdir, CMS_ROOT_PATH);
             $this->_topurl = CMS_ROOT_URL.'/'.$rel_url;
         }
     }
 
-    protected function to_relative_sub( $path_a, $path_b )
+    protected function to_relative_sub($path_a, $path_b)
     {
-        $path_a = realpath( $path_a );
-        $path_b = realpath( $path_b );
-        if( !is_dir($path_a) && !is_file($path_a) ) throw new \LogicException('Invalid path_a passed to '.__METHOD__.': '.$path_a);
-        if( !is_dir($path_b) ) throw new \LogicException('Invalid path_b passed to '.__METHOD__.': '.$path_b);
+        $path_a = realpath($path_a);
+        $path_b = realpath($path_b);
+        if (!is_dir($path_a) && !is_file($path_a)) throw new LogicException('Invalid path_a passed to '.__METHOD__.': '.$path_a);
+        if (!is_dir($path_b)) throw new LogicException('Invalid path_b passed to '.__METHOD__.': '.$path_b);
 
-        if( !$this->is_relative_to( $path_a, $path_b ) ) throw new \LogicException("$path_a is not relative to $path_b");
+        if (!$this->is_relative_to($path_a, $path_b)) throw new LogicException("$path_a is not relative to $path_b");
         $out = substr($path_a,strlen($path_b));
-        if( startswith($out,'/') ) $out = substr($out,1);
+        if (startswith($out, DIRECTORY_SEPARATOR)) $out = substr($out,1);
         return $out;
 
     }
@@ -52,43 +62,42 @@ class PathAssistant
         return $this->_topurl;
     }
 
-    public function is_relative_to( $path_a, $path_b )
+    public function is_relative_to($path_a, $path_b)
     {
-        $path_a = realpath( $path_a );
-        $path_b = realpath( $path_b );
-        if( !$path_a || ! $path_b ) return FALSE;
+        $path_a = realpath($path_a);
+        $path_b = realpath($path_b);
+        if (!$path_a || ! $path_b) return false;
 
-        return startswith( $path_a, $path_b);
+        return startswith($path_a, $path_b);
     }
 
-    public function is_relative( $path )
+    public function is_relative($path)
     {
-        return $this->is_relative_to( $path, $this->_topdir );
+        return $this->is_relative_to($path, $this->_topdir);
     }
 
-    public function to_relative( $path )
+    public function to_relative($path)
     {
-        return $this->to_relative_sub( $path, $this->_topdir );
+        return $this->to_relative_sub($path, $this->_topdir);
     }
 
-    public function to_absolute( $relative )
+    public function to_absolute($relative)
     {
-        return $this->_topdir.'/'.$relative;
+        return cms_join_path($this->_topdir, $relative);
     }
 
-    public function relative_path_to_url( $relative )
+    public function relative_path_to_url($relative)
     {
-        $relative = trim($relative);
-        if( startswith($relative,'/') ) $relative = substr($relative,1);
         $prefix = $this->get_top_url();
-        if( endswith( $prefix, '/') ) $prefix = substr($prefix,0,-1);
-        return $prefix.'/'.$relative;
+        if (endswith($prefix, '/')) $prefix = substr($prefix,0,-1);
+        $relative = trim($relative, ' /\\');
+        return $prefix . '/' . strtr($relative,'\\','/');
     }
 
-    public function is_valid_relative_path( $str )
+    public function is_valid_relative_path($path)
     {
-        $str = trim($str);
-        $absolute = $this->to_absolute($str);
-        return $this->is_relative( $absolute );
+        $absolute = $this->to_absolute(trim($path));
+        return $this->is_relative($absolute);
     }
 } // class
+
