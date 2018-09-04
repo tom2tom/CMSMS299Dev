29c6325f29c819ac84491fe9836844b6008395de
tom2tom <tpgww@onepost.net>
2018-09-04 10:05:56 +1000

    cleanup

--- phar_installer/lib/msg_functions.php
+++ phar_installer/lib/msg_functions.php
@@ -8,17 +8,20 @@ function ilang(...$args)
   return langtools::get_instance()->translate($args);
 }
 
-function verbose_msg($str) {
+function verbose_msg($str)
+{
   $obj = wizard::get_instance()->get_step();
   if( method_exists($obj,'verbose') ) return $obj->verbose($str);
 }
 
-function status_msg($str) {
+function status_msg($str)
+{
   $obj = wizard::get_instance()->get_step();
   if( method_exists($obj,'message') ) return $obj->message($str);
 }
 
-function error_msg($str) {
+function error_msg($str)
+{
   $obj = wizard::get_instance()->get_step();
   if( method_exists($obj,'error') ) return $obj->error($str);
 }
