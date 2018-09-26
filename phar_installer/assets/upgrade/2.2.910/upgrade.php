3a6b2f701cb6c928c78ab68a218cb0f25b2e6d36
tom2tom <tpgww@onepost.net>
2018-09-26 12:42:07 +1000

    import upgrades from former 2.2.900

--- phar_installer/assets/upgrade/2.2.910/upgrade.php
+++ phar_installer/assets/upgrade/2.2.910/upgrade.php
@@ -1,10 +1,168 @@
 <?php
 
+use __installer\utils;
 use CMSMS\Group;
+use CMSMS\LogicException;
+use CMSMS\SimplePluginOperations;
 use function __installer\CMSMS\endswith;
 use function __installer\CMSMS\joinpath;
+use function __installer\get_app;
 
-//extra permissions
+$app = get_app();
+$destdir = $app->get_destdir();
+if( !$destdir || !is_dir($destdir) ) {
+    throw new LogicException('Destination directory does not exist');
+}
+$config = $app->get_config();
+$s = ( !empty( $config['admin_dir'] ) ) ? $config['admin_dir'] : 'admin';
+$admindir = $destdir . DIRECTORY_SEPARATOR . $s;
+$assetsdir = ( !empty( $config['assets_path'] ) ) ? $config['assets_path'] : $destdir . DIRECTORY_SEPARATOR . 'assets';
+
+// 1. Move core modules to /lib/modules
+foreach([
+'AdminLog',
+'AdminSearch',
+'CMSContentManager',
+'CmsJobManager',
+'CoreAdminLogin',
+'CoreTextEditing',
+'DesignManager',
+'FileManager',
+'FilePicker',
+'MicroTiny',
+'ModuleManager',
+'Navigator',
+'Search',
+] as $modname ) {
+    $fp = $destdir . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . $modname;
+    if( is_dir( $fp ) ) {
+        $to = $destdir . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . $modname;
+        if( !is_dir( $to ) ) {
+            rename( $fp, $to );
+        } else {
+            utils::rrmdir( $fp );
+        }
+    }
+}
+
+// 2. Move ex-core modules to /assets/modules
+foreach( ['MenuManager', 'CMSMailer', 'News'] as $modname ) {
+    $fp = $destdir . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . $modname;
+    if( is_dir( $fp ) ) {
+        $to = $assetsdir . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . $modname;
+        if( !is_dir( $to ) ) {
+            rename( $fp, $to );
+        } else {
+            utils::rrmdir( $fp );
+        }
+    }
+}
+
+// 3. Create new folders, if necessary
+foreach ([
+ ['admin','configs'],
+ ['assets','admin_custom'],
+ ['assets','configs'],
+ ['assets','css'],
+ ['assets','images'],
+ ['assets','module_custom'],
+ ['assets','modules'],
+ ['assets','plugins'],
+ ['assets','simple_plugins'],
+ ['assets','templates'],
+] as $segs) {
+    switch($segs[0]) {
+        case 'admin':
+            $to = $admindir . DIRECTORY_SEPARATOR . $segs[1];
+            break;
+        case 'assets':
+            $to = $assetsdir . DIRECTORY_SEPARATOR . $segs[1];
+            break;
+        default:
+            break 2;
+    }
+    if( !is_dir( $to ) ) @mkdir( $to, 0771, true );
+    if( !is_dir( $to ) ) throw new LogicException("Could not create $to directory");
+    touch($to . DIRECTORY_SEPARATOR . 'index.html');
+}
+touch($assetsdir . DIRECTORY_SEPARATOR . 'index.html');
+
+// 4. Convert UDT's to simple plugins, widen users-table columns
+$udt_list = $db->GetArray('SELECT * FROM '.CMS_DB_PREFIX.'userplugins');
+if( $udt_list ) {
+
+    $create_simple_plugin = function( array $row, SimplePluginOperations $ops, $smarty ) {
+        $fp = $ops->file_path($row['userplugin_name']);
+        if( is_file( $fp ) ) {
+            verbose_msg('simple plugin named '.$row['userplugin_name'].' already exists');
+            return;
+        }
+
+        $code = preg_replace(
+                ['/^[\s\r\n]*<\\?php\s*[\r\n]*/i', '/[\s\r\n]*\\?>[\s\r\n]*$/'],
+                ['', ''], $row['code']);
+        if ( !$code ) {
+            verbose_msg('UDT named '.$row['userplugin_name'].' is empty, and will be discarded');
+            return;
+        }
+
+        $meta = ['name'=>$row['userplugin_name']];
+        if( $row['description'] ) {
+            $desc = trim($row['description'], " \t\n\r");
+            if( $desc ) {
+                $meta['description'] = $desc;
+            }
+        }
+
+        if( $ops->save($row['userplugin_name'], $meta, $code, $smarty) ) {
+            verbose_msg('Converted UDT '.$row['userplugin_name'].' to a plugin file');
+        } else {
+            verbose_msg('Error saving UDT named '.$row['userplugin_name']);
+        }
+    };
+
+    $ops = SimplePluginOperations::get_instance();
+	//$smarty defined upstream, used downstream
+    foreach( $udt_list as $udt ) {
+        $create_simple_plugin( $udt, $ops, $smarty );
+    }
+
+    $dict = GetDataDictionary($db);
+    $sqlarr = $dict->DropTableSQL(CMS_DB_PREFIX.'userplugins_seq');
+    $dict->ExecuteSQLArray($sqlarr);
+    $sqlarr = $dict->DropTableSQL(CMS_DB_PREFIX.'userplugins');
+    $dict->ExecuteSQLArray($sqlarr);
+    status_msg('Converted User Defined Tags to simple-plugin files');
+
+    $db->Execute( 'ALTER TABLE '.CMS_DB_PREFIX.'users MODIFY username VARCHAR(80)' );
+    $db->Execute( 'ALTER TABLE '.CMS_DB_PREFIX.'users MODIFY password VARCHAR(128)' );
+
+    verbose_msg(ilang('upgrading_schema',204));
+    $query = 'UPDATE '.CMS_DB_PREFIX.'version SET version = 204';
+    $db->Execute($query);
+}
+
+// 5. Tweak callbacks for page and generic layout template types
+$page_type = CmsLayoutTemplateType::load('__CORE__::page');
+if( $page_type ) {
+    $page_type->set_lang_callback('\\CMSMS\\internal\\std_layout_template_callbacks::page_type_lang_callback');
+    $page_type->set_content_callback('\\CMSMS\\internal\\std_layout_template_callbacks::reset_page_type_defaults');
+    $page_type->set_help_callback('\\CMSMS\\internal\\std_layout_template_callbacks::template_help_callback');
+    $page_type->save();
+} else {
+    error_msg('__CORE__::page template update '.ilang('failed'));
+}
+
+$generic_type = CmsLayoutTemplateType::load('__CORE__::generic');
+if( $generic_type ) {
+    $generic_type->set_lang_callback('\\CMSMS\\internal\\std_layout_template_callbacks::generic_type_lang_callback');
+    $generic_type->set_help_callback('\\CMSMS\\internal\\std_layout_template_callbacks::template_help_callback');
+    $generic_type->save();
+} else {
+    error_msg('__CORE__::generic template update '.ilang('failed'));
+}
+
+// 6. Extra permissions
 foreach( [
  'Modify Simple Plugins',
  'Modify Site Code',
@@ -35,7 +193,7 @@ $group->Save();
 $group->GrantPermission('Modify Site Assets');
 */
 
-// remove reference from plugins-arugument where necessary
+// 6. Remove reference from plugins-argument where necessary
 foreach ([
  ['lib', 'plugins'],
  ['admin', 'plugins'],
@@ -64,7 +222,7 @@ foreach ([
     }
 }
 
-// redundant sequence-tables
+// 7. Drop redundant sequence-tables
 $db->DropSequence(CMS_DB_PREFIX.'content_props_seq');
 $db->DropSequence(CMS_DB_PREFIX.'userplugins_seq');
 
@@ -122,7 +280,7 @@ $sqlarray = $dbdict->CreateIndexSQL('idx_layout_tpl_3',
     CMS_DB_PREFIX.CmsLayoutTemplate::TABLENAME, 'originator,name', ['UNIQUE']);
 $dbdict->ExecuteSQLArray($sqlarray);
 
-// migrate module templates to layout-templates table
+// 9. Migrate module templates to layout-templates table
 $sql = 'SELECT * FROM '.CMS_DB_PREFIX.'module_templates ORDER BY module_name,template_name';
 $data = $db->GetArray($sql);
 if ($data) {
@@ -167,18 +325,24 @@ $sqlarray = $dbdict->DropTableSQL(CMS_DB_PREFIX.'module_templates');
 $dbdict->ExecuteSQLArray($sqlarray);
 verbose_msg(ilang('upgrade_deletetable', 'module_templates'));
 
-// migrate everyone to new default theme
+// 10. Update preferences
+$now = $db->DbTimeStamp(time());
+// migrate to new default theme
 $files = glob(joinpath(CMS_ADMIN_PATH,'themes','*','*Theme.php'),GLOB_NOESCAPE);
 foreach ($files as $one) {
     if (is_readable($one)) {
         $name = basename($one, 'Theme.php');
         $query = 'UPDATE '.CMS_DB_PREFIX.'userprefs SET value=? WHERE preference=\'admintheme\'';
         $db->Execute($query,[$name]);
+        $query = 'UPDATE '.CMS_DB_PREFIX.'siteprefs SET sitepref_value=?,modified_date=? WHERE sitepref_name=\'logintheme\'';
+        $db->Execute($query,[$name,$now]);
         break;
     }
 }
+$query = 'INSERT INTO '.CMS_DB_PREFIX.'siteprefs (sitepref_name,create_date,modified_date) VALUES (\'loginmodule\',?,?);';
+$db->Execute($query,[$now,$now]);
 
 //if ($return == 2) {
-    $query = 'INSERT INTO '.CMS_DB_PREFIX.'version VALUES (205)';
+    $query = 'UPDATE '.CMS_DB_PREFIX.'version SET version = 205';
     $db->Execute($query);
 //}
