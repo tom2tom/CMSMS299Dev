90247ac10f8f0de3cf2f231ae2885c55f5fea807
tom2tom <tpgww@onepost.net>
2018-09-28 20:11:29 +1000

    init referenced variable

--- lib/classes/internal/class.template_wrapper.php
+++ lib/classes/internal/class.template_wrapper.php
@@ -27,20 +27,26 @@ use Smarty_Internal_Template;
  */
 class template_wrapper extends Smarty_Internal_Template
 {
-	/**
+    /**
      * fetch a rendered Smarty template
      *
      * @param  string $template   optional resource handle of the template file or template object
      * @param  mixed  $cache_id   optional cache id to be used with this template
      * @param  mixed  $compile_id optional compile id to be used with this template
      * @param  object $parent     optional next-higher level of Smarty variables
-	 * @return string rendered template output
-	 */
+     * @return string rendered template output
+     */
     public function fetch($template = null, $cache_id = null, $compile_id = null, $parent = null)
     {
         // send an event before fetching...this allows us to change template stuff.
         if( CmsApp::get_instance()->is_frontend_request() ) {
-            $parms = ['template'=>&$template,'cache_id'=>&$cache_id,'compile_id'=>&$compile_id,'display'=>&$display];
+            $display = false;
+            $parms = [
+             'template'=>&$template,
+             'cache_id'=>&$cache_id,
+             'compile_id'=>&$compile_id,
+             'display'=>&$display,
+            ];
             Events::SendEvent( 'Core', 'TemplatePrefetch', $parms );
         }
         return parent::fetch($template,$cache_id,$compile_id,$parent);
