8c12ecbfd66436c698674449540e7c50ddeb6fd8
tom2tom <tpgww@onepost.net>
2018-10-06 21:33:34 +1000

    typo

--- assets/modules/News/action.approvearticle.php
+++ assets/modules/News/action.approvearticle.php
@@ -33,7 +33,7 @@ if( is_object($search) ) {
     $search->DeleteWords($this->GetName(),$articleid,'article');
   }
   else if( $status == 'published' ) {
-    $query = 'SELECT * FROM '.CMS_DB_PREFIX.'module_news WHERE news_id = ?';;
+    $query = 'SELECT * FROM '.CMS_DB_PREFIX.'module_news WHERE news_id = ?';
     $article = $db->GetRow($query,[$articleid]);
     if( !$article ) return;
 
