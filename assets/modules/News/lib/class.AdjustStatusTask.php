453e76378aceefe3737e1c63e93732a63401f68e
tom2tom <tpgww@onepost.net>
2018-10-07 13:37:58 +1100

    add task to update article status

---
+++ assets/modules/News/lib/class.AdjustStatusTask.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Task: update articles' status in accord with their start/end times
+Copyright (C) 2018 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace News;
+
+use cms_utils;
+use CmsApp;
+use CmsRegularTask;
+use const CMS_DB_PREFIX;
+
+final class AdjustStatusTask implements CmsRegularTask
+{
+    const PREFNAME = 'StatusUpdateAt';
+
+    public function get_name()
+    {
+       $c = __CLASS__;
+       $p = strrpos($c, '\\');
+       return substr($c, $p+1);
+    }
+
+    public function get_description() {
+        return '';
+    }
+
+    public function test($time = '')
+    {
+        if( !$time ) $time = time();
+        $mod = cms_utils::get_module('News');
+        $lastrun = (int) $mod->GetPreference(self::PREFNAME);
+        return $lastrun <= ($time - 3600); // hardcoded to hourly
+    }
+
+    public function on_success($time = '')
+    {
+        if( !$time ) $time = time();
+        $mod = cms_utils::get_module('News');
+        $mod->SetPreference(self::PREFNAME,$time);
+    }
+
+    public function on_failure($time = '') {}
+
+    public function execute($time = '')
+    {
+        $db = CmsApp::get_instance()->GetDb();
+        if( !$time ) $time = time();
+        $query = 'UPDATE '.CMS_DB_PREFIX.'module_news SET status=\'archived\' WHERE (status=\'final\' OR status=\'published\') AND end_time IS NOT NULL AND end_time>0 AND end_time<?';
+        $db ->Execute($query,[$time]);
+        $query = 'UPDATE '.CMS_DB_PREFIX.'module_news SET status=\'published\' WHERE status=\'final\' AND start_time IS NOT NULL AND start_time>0 AND start_time<?';
+        $db ->Execute($query,[$time]);
+        return true;
+    }
+} // class
+
