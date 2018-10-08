cc31d71b86550acf5f03742511e9e1a4c2a83ee0
tom2tom <tpgww@onepost.net>
2018-10-08 22:59:01 +1100

    PHP7 declarations extra static function for hook

--- lib/classes/Async/class.JobManagerInterface.php
+++ lib/classes/Async/class.JobManagerInterface.php
@@ -4,19 +4,21 @@ namespace CMSMS\Async;
 
 interface JobManagerInterface
 {
-	public function load_jobs_by_module($module_name);
+	public function load_jobs_by_module($module);
 
-	public function load_job_by_id($job_id);
+	public function load_job_by_id(int $job_id);
 
-	public function load_job(Job &$job);
+	public function load_job(Job &$job):int;
 
-	public function unload_jobs_by_module($module_name);
+	public function unload_jobs_by_module(string $module_name);
 
-	public function unload_job_by_id($job_id);
+	public function unload_job_by_id(int $job_id);
 
-	public function unload_job_by_name($module_name, $job_name);
+	public function unload_job_by_name(string $module_name, string $job_name);
 
 	public function unload_job(Job &$job);
 
 	public function trigger_async_processing();
+
+	public static function trigger_async_hook();
 }
