5c2d51162df5b16a1e90e89101afbe7927b9fc11
tom2tom <tpgww@onepost.net>
2018-10-20 12:31:48 +1100

    correct lang keys

--- lib/classes/internal/class.extended_module_info.php
+++ lib/classes/internal/class.extended_module_info.php
@@ -6,22 +6,22 @@ use ModuleOperations;
 
 class extended_module_info extends module_info
 {
-    const KEYS = [
-     'active',
+    const EPROPS = [
+     'active', //c.f. parent::notavailable
      'admin_only',
-     'allow_admin_lazyload',
-     'allow_fe_lazyload',
+     'allow_admin_lazyload', //c.f. parent::lazyloadadmin
+     'allow_fe_lazyload', //c.f. parent::lazyloadfrontend
      'can_deactivate',
      'can_uninstall',
-     'dependants',
+     'dependants', //c.f. parent::depends
      'installed',
      'installed_version',
      'missingdeps',
      'status',
-     'system_module',
+     'system_module', //c.f. parent::is_system_module
     ];
 
-    private $_data = [];
+    private $_edata = [];
 
     public function __construct($module_name,$can_load = false)
     {
@@ -40,8 +40,8 @@ class extended_module_info extends module_info
             $this['allow_fe_lazyload'] = $minfo[$module_name]['allow_fe_lazyload'];
             $this['allow_admin_lazyload'] = $minfo[$module_name]['allow_admin_lazyload'];
 
-            $this->_data['can_deactivate'] = $this['name'] != 'ModuleManager';
-            $this->_data['can_uninstall'] = $this['name'] != 'ModuleManager';
+            $this->_edata['can_deactivate'] = $this['name'] != 'ModuleManager';
+            $this->_edata['can_uninstall'] = $this['name'] != 'ModuleManager';
 
             // dependants is the list of modules that use this module (e.g. CGBlog uses CGExtensions)
             if( isset($minfo[$module_name]['dependants']) ) $this['dependants'] = $minfo[$module_name]['dependants'];
@@ -50,8 +50,8 @@ class extended_module_info extends module_info
 
     public function OffsetGet($key)
     {
-        if( !in_array($key,self::KEYS) ) return parent::OffsetGet($key);
-        if( isset($this->_data[$key]) ) return $this->_data[$key];
+        if( !in_array($key,self::EPROPS) ) return parent::OffsetGet($key);
+        if( isset($this->_edata[$key]) ) return $this->_edata[$key];
         if( $key == 'missingdeps' ) {
             $out = null;
             $deps = $this['depends'];
@@ -61,22 +61,22 @@ class extended_module_info extends module_info
                     if( !$depinfo['installed'] || version_compare($depinfo['version'],$onedepversion) < 0 ) $out[$onedepname] = $onedepversion;
                 }
             }
-        return $out;
+            return $out;
         }
     }
 
     public function OffsetSet($key,$value)
     {
-        if( !in_array($key,self::KEYS) ) parent::OffsetSet($key,$value);
-        if( $key == 'can_deactivate' ) throw new CmsLogicException('CMSEX_INVALIDMEMBER',null,$key);
-        if( $key == 'can_uninstall' ) throw new CmsLogicException('CMSEX_INVALIDMEMBER',null,$key);
-        if( $key == 'missingdeps' ) throw new CmsLogicException('CMSEX_INVALIDMEMBER',null,$key);
-        $this->_data[$key] = $value;
+        if( !in_array($key,self::EPROPS) ) parent::OffsetSet($key,$value);
+        if( $key == 'can_deactivate' ) throw new CmsLogicException('CMSEX_INVALIDMEMBERSET',null,$key);
+        if( $key == 'can_uninstall' ) throw new CmsLogicException('CMSEX_INVALIDMEMBERSET',null,$key);
+        if( $key == 'missingdeps' ) throw new CmsLogicException('CMSEX_INVALIDMEMBERSET',null,$key);
+        $this->_edata[$key] = $value;
     }
 
     public function OffsetExists($key)
     {
-        if( !in_array($key,self::KEYS) ) return parent::OffsetExists($key);
-        return isset($this->_data[$key]);
+        if( !in_array($key,self::EPROPS) ) return parent::OffsetExists($key);
+        return isset($this->_edata[$key]);
     }
 } // class
