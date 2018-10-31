f3dd7c02b02d207df1d06e7dcc2c37a09547b153
tom2tom <tpgww@onepost.net>
2018-10-31 20:44:37 +1100

    correct query per svn

--- lib/modules/AdminLog/lib/class.storage.php
+++ lib/modules/AdminLog/lib/class.storage.php
@@ -8,6 +8,8 @@ use const CMS_DB_PREFIX;
 
 class storage
 {
+    const TABLENAME = 'mod_adminlog';
+
     private $_mod;
 
     public function __construct( AdminLog $mod )
@@ -17,28 +19,22 @@ class storage
 
     public function clear()
     {
-        $sql = 'TRUNCATE TABLE '.self::table_name();
         $db = $this->_mod->GetDb();
+        $sql = 'TRUNCATE TABLE '.CMS_DB_PREFIX.self::TABLENAME;
         $db->Execute( $sql );
     }
 
     public function save( event $ev )
     {
         $db = $this->_mod->GetDb();
-        $sql = 'INSERT INTO '.self::table_name().' ( timestamp, severity, uid, ip_addr, username, subject, msg, item_id ) VALUES (?,?,?,?,?,?,?,?)';
+        $sql = 'INSERT INTO '.CMS_DB_PREFIX.self::TABLENAME.' (timestamp, severity, uid, ip_addr, username, subject, msg, item_id) VALUES (?,?,?,?,?,?,?,?)';
         $db->Execute( $sql, [ $ev->timestamp, $ev->severity, $ev->uid, $ev->ip_addr, $ev->username, $ev->subject, $ev->msg, $ev->item_id ] );
-    }
-
-    public static function table_name()
-    {
-        return CMS_DB_PREFIX.'mod_adminlog';
     }
 
     public function clear_older_than( $time )
     {
-        $time = (int) $time;
         $db = $this->_mod->GetDb();
-        $sql = 'DELETE FROM '.self::table_name().' WHERE timestamp > ?';
-        $db->Execute( $sql, [ $time ] );
+        $sql = 'DELETE FROM '.CMS_DB_PREFIX.self::TABLENAME.' WHERE timestamp < ?';
+        $db->Execute( $sql, [ (int)$time ] );
     }
 }
