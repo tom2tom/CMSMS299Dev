ba3f389bfd67453bb90ef38f64c30b1801b78681
tom2tom <tpgww@onepost.net>
2018-12-12 14:34:18 +1100

    rewrite

--- lib/modules/AdminSearch/action.defaultadmin.php
+++ lib/modules/AdminSearch/action.defaultadmin.php
@@ -21,54 +21,121 @@ use AdminSearch\tools;
 if( !isset($gCms) ) exit;
 if( !$this->VisibleToAdminUser() ) return;
 
-$url = $this->create_url($id,'admin_search');
-$ajax_url = str_replace('&amp;','&',$url).'&cmsjobtype=1';
-$js_url = $this->GetModuleURLPath().'/lib/js/admin_search_tab.js';
-
-$s1 = json_encode($this->Lang('error_select_slave'));
-
-$template = get_parameter_value( $params, 'template', 'defaultadmin.tpl' );
-$tpl = $smarty->createTemplate($this->GetTemplateResource($template),null,null,$smarty);
-
-$userid = get_userid(false);
-$tmp = cms_userprefs::get_for_user($userid,$this->GetName().'saved_search');
-if( $tmp ) $tpl->assign('saved_search',unserialize($tmp));
-
-$slaves = tools::get_slave_classes();
-$tpl->assign('slaves',$slaves);
-
 $out = <<<EOS
-<style type="text/css" scoped>
+<style type="text/css">
 #status_area,#searchresults_cont,#workarea {
- display: none
+ display:none
 }
 #searchresults {
- max-height: 25em;
- overflow: auto;
- cursor: pointer
+ max-height:25em;
+ overflow:auto;
+ cursor:pointer
 }
 .search_oneresult {
- color: red
+ color:red
 }
 </style>
-
-<script type="text/javascript">
-//<![CDATA[
- var ajax_url = '$ajax_url';
- $('#searchbtn').on('click', function() {
-   var l = $('#filter_box :checkbox.filter_toggle:checked').length;
-   if(l === 0) {
-     cms_alert($s1);
-   } else {
-     $('#searchresults').html('');
-   }
- });
-//]]>
-</script>
-<script type="text/javascript" src="$js_url"></script>
 
 EOS;
 $this->AdminHeaderContent($out);
 
-$tpl->display();
+$s1 = json_encode($this->Lang('warn_clickthru'));
+$s2 = json_encode($this->Lang('error_search_text'));
+$s3 = json_encode($this->Lang('error_select_slave'));
+$url = $this->create_url($id,'admin_search');
+$ajax_url = str_replace('&amp;','&',$url) . '&cmsjobtype=1';
 
+/*function _update_status(html) {
+  $('#status_area').html(html);
+  $('#status_area').show();
+}*/
+$out = <<<EOS
+<script type="text/javascript">
+//<![CDATA[
+function process_results(c) {
+ c.find('.section_children').hide();
+ c.find('a').each(function() {
+  var \$el = $(this),
+      d = \$el.data('events');
+  if(d === undefined || d.length === 0) {
+   \$el.on('click', function(e) {
+     e.preventDefault();
+     cms_confirm_linkclick(this,$s1);
+     return false;
+   });
+  }
+ });
+ c.find('li.section').on('click',function() {
+  $('.section_children').hide();
+  $(this).children('.section_children').show();
+ });
+}
+
+$(document).ready(function() {
+ $('#filter_all').on('change',function() {
+  $('#filter_box .filter_toggle').prop('checked',this.checked);
+ });
+ $('#searchbtn').on('click', function() {
+   var t = $('#searchtext').val();
+   if(t.length < 2) {
+     cms_alert($s2);
+     return false;
+   }
+   var cb = $('#filter_box :checkbox.filter_toggle:checked');
+   if(cb.length === 0) {
+     cms_alert($s3);
+     return false;
+   } else {
+     $('#searchresults').html('');
+     var s = [];
+     cb.each(function() {
+       s.push(this.value);
+     });
+     var d = $('#filter_box #search_desc:checked').length;
+     var u = '$ajax_url' + '&{$id}search_text=' + encodeURIComponent(t) + '&{$id}slaves=' + s.join() + '&{$id}search_descriptions=' + d;
+     $.ajax({
+      url: u,
+      method: 'POST',
+      dataType: 'html',
+      success: function (data, textStatus, jqXHR) {
+       var \$el = $('#searchresults_cont');
+       \$el.hide();
+	   var \$c = \$el.find('#searchresults');
+       \$c.html(data);
+       process_results(\$c);
+       \$el.show();
+      },
+      error: function(jqXHR, textStatus) {
+       cms_alert(jqXHR.responseText);
+      }
+     });
+   }
+ });
+});
+//]]>
+</script>
+
+EOS;
+$this->AdminBottomContent($out);
+
+$template = get_parameter_value($params,'template','defaultadmin.tpl');
+$tpl = $smarty->createTemplate($this->GetTemplateResource($template),null,null,$smarty);
+
+$userid = get_userid(false);
+$tmp = cms_userprefs::get_for_user($userid,$this->GetName().'saved_search');
+if( $tmp ) {
+    $init = unserialize($tmp,[]);
+}
+else {
+    $init = [
+     'search_text' => '',
+     'slaves' => [],
+     'search_descriptions' => FALSE,
+    ];
+}
+$tpl->assign('saved_search',$init);
+
+$slaves = tools::get_slave_classes();
+$tpl->assign('slaves',$slaves);
+
+$tpl->display();
