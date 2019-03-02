d2f580d38cc66917de8a53f6b5abfa5cf533a09a
tom2tom <tpgww@onepost.net>
2019-03-02 19:08:14 +1100

    correct installer namespace

--- admin/systemmaintenance.php
+++ admin/systemmaintenance.php
@@ -16,7 +16,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use __installer\installer_base;
+use cms_installer\installer_base;
 use CMSMS\AdminUtils;
 use CMSMS\internal\Smarty;
 
