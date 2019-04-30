e647b585954a8afb0e7439ee4c96bc6e8cfeed9f
tom2tom <tpgww@onepost.net>
2019-04-30 19:26:54 +1000

    replace deprecate method call

--- lib/modules/Search/action.default.php
+++ lib/modules/Search/action.default.php
@@ -1,4 +1,19 @@
 <?php
+# Search module action: default
+# Copyright (C) 2004-2019 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use CMSMS\TemplateOperations;
 
@@ -26,7 +41,7 @@ if( isset( $params['inline'] ) ) {
 $origreturnid = $returnid;
 if( isset( $params['resultpage'] ) ) {
     $hm = $gCms->GetHierarchyManager();
-    $cid = $hm->find_by_tag_anon($params['resultpage']);
+    $cid = $hm->find_by_identifier($params['resultpage'],false);
     if( $cid ) $returnid = $cid;
 }
 //Pretty URL compatibility
@@ -62,4 +77,3 @@ foreach( $params as $key => $value ) {
 if( $hidden != '' ) $tpl->assign('hidden',$hidden);
 $tpl->assign('endform', $this->CreateFormEnd());
 $tpl->display();
-
