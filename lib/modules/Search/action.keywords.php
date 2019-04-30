32b795aa9803b9487fffa2100620398806543de3
tom2tom <tpgww@onepost.net>
2019-04-30 19:24:15 +1000

    add license

--- lib/modules/Search/action.keywords.php
+++ lib/modules/Search/action.keywords.php
@@ -1,5 +1,19 @@
 <?php
-if( !isset($gCms) ) exit;
+# Search module action: keywords
+# Copyright (C) 2004-2019 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 $pageid = $params['pageid'] ?? $returnid;
 
