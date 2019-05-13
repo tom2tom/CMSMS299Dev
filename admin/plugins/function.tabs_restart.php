33bc7b28eaed659efcf963bd3a688bfb9cd56fac
tom2tom <tpgww@onepost.net>
2019-05-13 14:26:33 +1000

    add plugin to reset tabs-class properties

---
+++ admin/plugins/function.tabs_restart.php
@@ -0,0 +1,23 @@
+<?php
+#function to revert tabs-class data back to vanilla
+#Copyright (C) 2019 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+#Thanks to Ted Kulp and all other contributors from the CMSMS Development Team.
+#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+function smarty_function_tabs_restart($params, $template)
+{
+	CMSMS\AdminTabs::reset();
+	return '';
+}
