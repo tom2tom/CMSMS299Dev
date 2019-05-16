a97939f0c2e84dd814223bd351ef7792cce088f6
tom2tom <tpgww@onepost.net>
2019-05-16 14:08:33 +1000

    default to site dateformat preference

--- lib/plugins/function.created_date.php
+++ lib/plugins/function.created_date.php
@@ -1,5 +1,5 @@
 <?php
-#Plugin to...
+#Plugin to get the creation date of a site page
 #Copyright (C) 2004-2019 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
 #Thanks to Ted Kulp and all other contributors from the CMSMS Development Team.
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
@@ -20,12 +20,12 @@ function smarty_function_created_date($params, $template)
 {
 	$content_obj = CmsApp::get_instance()->get_content_object();
 
-	$format = '%x %X';
-	if(!empty($params['format'])) $format = $params['format'];
-
 	if (is_object($content_obj) && $content_obj->GetCreationDate() > -1) {
+		if(!empty($params['format'])) { $format = trim($params['format']); }
+		else { $format = cms_siteprefs::get('defaultdateformat','%x %X'); }
+
 		$time = $content_obj->GetCreationDate();
-		$str = cms_htmlentities(strftime($format, $time));;
+		$str = cms_htmlentities(strftime($format, $time));
 
 		if( isset($params['assign']) ) {
 			$template->assign(trim($params['assign']),$str);
