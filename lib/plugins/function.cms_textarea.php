1a2b818c53784173b9f5156bba3cdf99f5ce1752
tom2tom <tpgww@onepost.net>
2019-05-19 12:49:51 +1000

    populate help and about methods

--- lib/plugins/function.cms_textarea.php
+++ lib/plugins/function.cms_textarea.php
@@ -1,5 +1,5 @@
 <?php
-#Plugin to generate page content for a textarea element
+#Plugin to generate html for a textarea element
 #Copyright (C) 2004-2019 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
 #Thanks to Ted Kulp and all other contributors from the CMSMS Development Team.
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
@@ -16,14 +16,58 @@
 #You should have received a copy of the GNU General Public License
 #along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+use CMSMS\FormUtils;
+
 function smarty_function_cms_textarea($params, $template)
 {
-	if( !isset($params['name']) ) throw new CmsInvalidDataException('syntax_area plugin missing parameter: name');
+	if( !isset($params['name']) ) throw new CmsInvalidDataException('cms_textarea plugin missing parameter: name');
 
-	$out = CMSMS\FormUtils::create_textarea($params);
+	$out = FormUtils::create_textarea($params);
 	if( isset($params['assign']) ) {
 		$template->assign(trim($params['assign']),$out);
 		return;
 	}
 	return $out;
+}
+
+function smarty_cms_help_function_cms_textarea()
+{
+	echo <<<'EOS'
+<h3>What does it do?</h3>
+Generates html for a textarea element.
+<h4>Parameters:</h4>
+As for <code>FormUtils::create_textarea()</code><br />
+<ul>
+<li>name: element name (mandatory, but only relevant for form submission)</li>
+<li>modid: submitted-parameter prefix ('m1_' etc)</li>
+<li>prefix: alias for modid</li>
+<li>id: id for the created element id="whatever"</li>
+<li>htmlid: alias for id</li>
+<li>class: class name(s) to apply to the element</li>
+<li>classname: alias for class</li>
+<li>cols: number initial size</li>
+<li>width: alias for cols</li>
+<li>rows: number initial size</li>
+<li>height: alias for rows</li>
+<li>maxlength: content length-limit</li>
+<li>value: initial content</li>
+<li>text: alias for value</li>
+<li>addtext: additional attribute(s) for the element e.g. style="whatever" cms-data-X="whatever" readonly</li>
+</ul>
+<br />
+As always<br />
+assign
+EOS;
+}
+
+function smarty_cms_about_function_cms_textarea()
+{
+	echo <<<'EOS'
+<p>Initial release 2004</p>
+<p>Change History:<br />
+<ul>
+<li>Adapted to work with CMSMS 2.3 FormUtils::create_textarea() May 2019</li>
+</ul>
+</p>
+EOS;
 }
