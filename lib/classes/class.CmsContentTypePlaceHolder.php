dde8c7a2bdff48b4cb01d3ab37adcfb65d816854
tom2tom <tpgww@onepost.net>
2019-05-24 11:05:56 +1000

    typo

--- lib/classes/class.CmsContentTypePlaceHolder.php
+++ lib/classes/class.CmsContentTypePlaceHolder.php
@@ -1,4 +1,4 @@
 <?php
 assert(empty(CMS_DEBUG), new DeprecationNotice('class','CMSMS\\ContentTypePlaceHolder'));
-require_once  __DIR__.DIRECTORY_SEPARATOR.'class ContentTypePlaceHolder.php';
+require_once  __DIR__.DIRECTORY_SEPARATOR.'class.ContentTypePlaceHolder.php';
 \class_alias('CMSMS\\ContentTypePlaceHolder', 'CmsContentTypePlaceHolder', false);
