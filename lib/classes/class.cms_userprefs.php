99320f624213173eacce6c09e54404601a45c1f5
tom2tom <tpgww@onepost.net>
2019-06-07 18:32:45 +1000

    PHP7 typehints support non-scalar values

--- lib/classes/class.cms_userprefs.php
+++ lib/classes/class.cms_userprefs.php
@@ -1,5 +1,5 @@
 <?php
-#Classes and utilities for working with user preferences.
+#Class and utilities for working with user preferences.
 #Copyright (C) 2016-2019 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
 #Thanks to Robert Campbell and all other contributors from the CMSMS Development Team.
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
@@ -17,7 +17,7 @@
 #along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 /**
- * A static class for preferences stored with admin user accounts
+ * A class for working with preferences associated with admin users
  *
  * @package CMS
  * @license GPL
@@ -28,9 +28,15 @@ final class cms_userprefs
 {
 	/**
 	 * @ignore
+	 * Constant indicating a serialized value
+	 */
+	const SERIAL = '_S8D_'; // shortened '_SERIALIZED_'
+
+	/**
+	 * @ignore
 	 * Intra-request cache
 	 */
-	private static $_prefs;
+	private static $_prefs = null;
 
 	/**
 	 * @ignore
@@ -41,20 +47,18 @@ final class cms_userprefs
 	/**
 	 * @ignore
 	 */
-	private static function _read($userid)
+	private static function _read(int $userid)
 	{
 		if( is_array(self::$_prefs) && isset(self::$_prefs[$userid]) && is_array(self::$_prefs[$userid]) ) return;
 
 		$db = CmsApp::get_instance()->GetDb();
 		$query = 'SELECT preference,value FROM '.CMS_DB_PREFIX.'userprefs WHERE user_id = ?';
-		$dbr = $db->GetArray($query,[$userid]);
-		if( is_array($dbr) ) {
-			if( !is_array(self::$_prefs) ) self::$_prefs = [];
+		$dbr = $db->GetAssoc($query,[$userid]);
+		if( $dbr ) {
+			self::$_prefs[$userid] = $dbr;
+		}
+		else {
 			self::$_prefs[$userid] = [];
-			for( $i = 0, $n = count($dbr); $i < $n; $i++ ) {
-				$row = $dbr[$i];
-				self::$_prefs[$userid][$row['preference']] = $row['value'];
-			}
 		}
 	}
 
@@ -75,63 +79,78 @@ final class cms_userprefs
 	}
 
 	/**
-	 * A method to get a preference for a specific user
+	 * Get a preference value (if such exists and is not '') for the specified user
+	 * or otherwise return a default value.
 	 *
-	 * @param int $userid The specified user id.
+	 * @param int    $userid The user id
 	 * @param string $key The preference name
-	 * @param mixed $dflt The default value.
-	 * @return string
+	 * @param mixed  $dflt Optional default value
+	 * @return mixed
 	 */
-	public static function get_for_user($userid,$key,$dflt = '')
+	public static function get_for_user(int $userid,string $key,$dflt = '')
 	{
 		self::_read($userid);
-		if( isset(self::$_prefs[$userid][$key]) && self::$_prefs[$userid][$key] != '' ) return self::$_prefs[$userid][$key];
+		if( isset(self::$_prefs[$userid][$key]) && self::$_prefs[$userid][$key] != '' ) {
+			$value = self::$_prefs[$userid][$key];
+			$l = strlen(self::SERIAL);
+			if( strncmp($value,self::SERIAL,$l) == 0 ) {
+				$value = unserialize(substr($val,$l),['allowed_classes' => FALSE]);
+			}
+			return $value;
+		}
 		return $dflt;
 	}
 
-
 	/**
-	 * Get a preference for the current user
+	 * Get a preference value (if such exists and is not '') for the current user
+	 * or otherwise return a default value.
 	 *
 	 * @param string $key The preference name
-	 * @param string $dflt A default value if the preference could not be found
-	 * @return strung
+	 * @param mixed  $dflt Optional default value
+	 * @return mixed
 	 */
-	public static function get($key,$dflt = '')
+	public static function get(string $key,$dflt = '')
 	{
 		return self::get_for_user(self::_userid(),$key,$dflt);
 	}
 
 	/**
-	 * Return an array of all user preferences
+	 * Return all preferences for the specified user.
 	 *
 	 * @param int $userid
-	 * @return array Associative array of preferences and values.
+	 * @return mixed array | null Assoc. array of preferences and values.
 	 */
-	public static function get_all_for_user($userid)
+	public static function get_all_for_user(int $userid)
 	{
-		$userid = (int)$userid;
 		self::_read($userid);
-		if( isset(self::$_prefs[$userid]) ) return self::$_prefs[$userid];
+		if( isset(self::$_prefs[$userid]) ) {
+			$ret = [];
+			$l = strlen(self::SERIAL);
+			foreach(self::$_prefs[$userid] as $key => $value ) {
+				if( strncmp($value,self::SERIAL,$l) == 0 ) {
+					$value = unserialize(substr($value,$l),['allowed_classes' => FALSE]);
+				}
+				$ret[$key] = $value;
+			}
+			return $ret;
+		}
 	}
 
 	/**
-	 * A method to test if a preference exists for a user
+	 * Test if a preference exists (set and value is not '') for a user
 	 *
-	 * @param int $userid the user id
+	 * @param int $userid The user id
 	 * @param string $key The preference name
 	 * @return bool
 	 */
-	public static function exists_for_user($userid,$key)
+	public static function exists_for_user(int $userid,string $key)
 	{
-		$userid = (int)$userid;
 		self::_read($userid);
 		return ( isset(self::$_prefs[$userid][$key]) && self::$_prefs[$userid][$key] !== '' ) ;
 	}
 
-
 	/**
-	 * A method to test if a preference exists for the current user
+	 * Test if a preference exists (set and value is not '') for the current user
 	 *
 	 * @param string $key The preference name
 	 * @return bool
@@ -141,17 +160,18 @@ final class cms_userprefs
 		return self::exists_for_user(self::_userid(),$key);
 	}
 
-
 	/**
-	 * A method to set a preference for a specific user
+	 * Set a preference for the specified user
 	 *
-	 * @param int $userid The user id
+	 * @param int    $userid The user id
 	 * @param string $key The preference name
-	 * @param string $value The preference value
+	 * @param mixed  $value The preference value
 	 */
-	public static function set_for_user($userid,$key,$value)
+	public static function set_for_user(int $userid,string $key,$value)
 	{
-		$userid = (int)$userid;
+		if( !(is_scalar($value) || is_null($value)) ) {
+			$value = self::SERIAL.serialize($value);
+		}
 		self::_read($userid);
 		$db = CmsApp::get_instance()->GetDb();
 		if( !self::exists_for_user($userid,$key) ) {
@@ -167,27 +187,25 @@ final class cms_userprefs
 		self::$_prefs[$userid][$key] = $value;
 	}
 
-
 	/**
-	 * A method to set a preference for the current logged in user.
+	 * Set a preference for the current logged in user.
 	 *
 	 * @param string $key The preference name
-	 * @param string $value The preference value
+	 * @param mixed  $value The preference value
 	 */
-	public static function set($key,$value)
+	public static function set(string $key,$value)
 	{
-		return self::set_for_user(self::_userid(),$key,$value);
+		self::set_for_user(self::_userid(),$key,$value);
 	}
 
-
 	/**
-	 * A method to remove a method for the user
+	 * Remove preference(s) for the specified user
 	 *
-	 * @param int $userid The user id
-	 * @param string $key (optional) The preference name.  If not specified, all preferences for this user will be removed.
-	 * @param bool $like (optional) whether or not to use approximation in the preference name
+	 * @param int    $userid The user id
+	 * @param string $key  Optional preference name. If not specified, all preferences for this user will be removed.
+	 * @param bool   $like Optional flag whether to interpret $key as a preference-name prefix. Default false.
 	 */
-	public static function remove_for_user($userid,$key = '',$like = FALSE)
+	public static function remove_for_user(int $userid,string $key = '',bool $like = FALSE)
 	{
 		$userid = (int)$userid;
 		self::_read($userid);
@@ -208,36 +226,36 @@ final class cms_userprefs
 		self::_reset();
 	}
 
-
 	/**
-	 * A method to remove a preference for the current user
+	 * Remove preference(s) for the current user
 	 *
-	 * @param string $key The preference name.
-	 * @param bool $like (optional) whether or not to use approximation in the preference name
+	 * @param string $key Optional preference name. If not specified, all preferences will be removed.
+	 * @param bool   $like Optional flag whether to interpret $key as a preference-name prefix. Default false.
 	 */
-	public static function remove($key,$like = FALSE)
+	public static function remove(string $key = '',bool $like = FALSE)
 	{
-		return self::remove_for_user(self::_userid(),$key,$like);
+		self::remove_for_user(self::_userid(),$key,$like);
 	}
 } // class
 
 /**
- * Retrieve the value of the named preference for the given userid.
+ * Retrieve the value of the named preference for the given user.
  *
  * @since 0.3
  * @deprecated since 1.10 Use cms_userprefs::get_for_user()
- * @param int $userid The user id
- * @param string  $prefname The preference name
- * @param mixed   $default The default value if the preference is not set for the given user id.
+ * @param int    $userid The user id
+ * @param string $prefname The preference name
+ * @param mixed  $default Optional default value.
  * @return mixed
  */
 function get_preference($userid, $prefname, $default='')
 {
+	assert(empty(CMS_DEPREC), new DeprecationNotice('method','cms_userprefs::get_for_user'));
 	return cms_userprefs::get_for_user($userid,$prefname,$default);
 }
 
 /**
- * Sets the given preference for the given userid with the given value.
+ * Set the given preference for the given userid with the given value.
  *
  * @since 0.3
  * @deprecated since 1.10 Use cms_userprefs::set_for_user()
@@ -247,5 +265,6 @@ function get_preference($userid, $prefname, $default='')
  */
 function set_preference($userid, $prefname, $value)
 {
-	return cms_userprefs::set_for_user($userid,$prefname,$value);
+	assert(empty(CMS_DEPREC), new DeprecationNotice('method','cms_userprefs::set_for_user'));
+	cms_userprefs::set_for_user($userid,$prefname,$value);
 }
