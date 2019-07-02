8cabe975b0d5dc718d0d0c9bc59ddbe7ca11c513
tom2tom <tpgww@onepost.net>
2019-07-02 23:36:35 +1000

    reinstate login-processing by module directly

--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,6 @@
 <?php
 #CMSMS admin-login processing
-#Copyright (C) 2018-2019 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+#Copyright (C) 2019 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
 #This program is free software; you can redistribute it and/or modify
@@ -16,15 +16,45 @@
 #along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use CMSMS\AppState;
+use CMSMS\internal\GetParameters;
+use CMSMS\ModuleOperations;
 
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'class.AppState.php';
 $CMS_APP_STATE = AppState::STATE_ADMIN_PAGE; // in scope for inclusion, to set initial state
 AppState::add_state(AppState::STATE_LOGIN_PAGE);
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'include.php';
 
-$ob = cms_utils::get_theme_object();
-if ($ob) {
-    $ob->do_login();
+$modname = cms_siteprefs::get('loginmodule');
+if ($modname) {
+	$params = (new GetParameters())->get_action_values(['module','id','action']);
+	$module = $params['module'];
+	if ($module && $module != $modname) throw new RuntimeException('Invalid login-module parameter');
+	$id = $params['id'];
+	if (!$id) $id = '__';
+	$action = $params['action'];
+	if (!$action) $action = 'admin_login';
+
+	$modops = ModuleOperations::get_instance();
+	$modinst = $modops->get_module_instance($modname, '', true);
+	if (!$modinst) {
+		throw new RuntimeException('Invalid login module');
+	}
+	$params = array_diff_key($params, ['module'=>1,'id'=>1,'action'=>1]);
+	$content = $modinst->DoActionBase($action, $id, $params, null, $smarty);
+
+	$themeobj = cms_utils::get_theme_object();
+	$themeobj->SetTitle($modinst->Lang('logintitle'));
+	$themeobj->set_content($content);
+
+	cms_admin_sendheaders();
+	header('Content-Language: ' . CmsNlsOperations::get_current_language());
+	echo $themeobj->do_loginpage('login');
 } else {
-    die('System error');
+	$themename = cms_siteprefs::get('logintheme');
+	$themeobj = cms_utils::get_theme_object($themename);
+	if ($themeobj) {
+		$themeobj->do_login();
+	} else {
+		throw new RuntimeException('Invalid login theme');
+	}
 }
