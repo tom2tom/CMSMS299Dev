e16d1e058dd0778428a68085dcba48c72c83127f
tom2tom <tpgww@onepost.net>
2020-01-04 20:52:02 +1100

    refresh preferences

--- lib/modules/CoreTextEditing/method.upgrade.php
+++ lib/modules/CoreTextEditing/method.upgrade.php
@@ -1,7 +1,7 @@
 <?php
 /*
 CoreTextEditing module method: upgrade
-Copyright (C) 2018-2019 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+Copyright (C) 2018-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
 This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 
 This program is free software; you can redistribute it and/or modify
@@ -18,3 +18,45 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
 if (!isset($gCms)) exit;
+
+$me = $this->GetName().'::';
+$all = $this->ListEditors();
+
+$val = cms_siteprefs::get('syntax_editor');
+if ($val && startswith($val, $me)) { //only if was in this module
+    $def = reset($all);
+    cms_siteprefs::set('syntax_editor', $def);
+}
+// TODO user-preferences too?
+
+// refresh all editor-related prefs
+$base = __DIR__.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'editor.';
+foreach ($all as $editor => $val) {
+    $n = strtolower($editor);
+    $val = $this->GetPreference($n.'_source_url');
+    if (!$val) {
+        $val = $this->GetPreference($n.'_url', 'MIS_SING'); //try old format
+        if ($val) {
+            if ($val !== 'MIS_SING') {
+                $this->RemovePreference($n.'_url');
+                $this->SetPreference($n.'_source_url', $val);
+            } else {
+                $val = '';
+            }
+        }
+    }
+    if (!$val || !startswith($val, CMS_ROOT_URL)) {//only if non-site url
+        $fp = $base.$editor.'.php';
+        require_once $fp;
+        $P = (!empty($const_prefix)) ? $const_prefix : strtoupper($editor).'_';
+        $val = @constant($P.'CDN');
+        if ($val !== null) {
+            $this->SetPreference($n.'_source_url', $val);
+        }
+        $val = @constant($P.'THEME');
+        if ($val !== null) {
+            $this->SetPreference($n.'_theme', $val);
+        }
+    }
+}
+//TODO remove any now-irrelevant-editor prefs
