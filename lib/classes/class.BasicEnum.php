515a763f36e2626acb565590ccc8e712ad8fd69a
tom2tom <tpgww@onepost.net>
2020-02-25 14:32:39 +1100

    getName() bugfix

--- lib/classes/class.BasicEnum.php
+++ lib/classes/class.BasicEnum.php
@@ -14,11 +14,11 @@ abstract class BasicEnum
 {
     private static $constCacheArray = null;
 
-	/**
-	 * Get all names in the enum
-	 * @private
-	 * @return array
-	 */
+    /**
+     * Get all names in the enum
+     * @private
+     * @return array
+     */
     private static function getConstants()
     {
         if (self::$constCacheArray === null) {
@@ -32,22 +32,22 @@ abstract class BasicEnum
         return self::$constCacheArray[$calledClass];
     }
 
-	/**
-	 * Get names of all members of the enum
-	 * @return array
-	 */
+    /**
+     * Get names of all members of the enum
+     * @return array
+     */
     public static function getAll()
     {
         return self::getConstants();
     }
 
-	/**
-	 * Check whether $name is a valid enum-member
-	 * @param type $name
-	 * @param type $strict
-	 * @return bool
-	 */
-	public static function isValidName($name, $strict = false) : bool
+    /**
+     * Check whether $name is a valid enum-member
+     * @param mixed string|const $name
+     * @param bool $strict optional flag whether to require exact case-match, default false
+     * @return bool
+     */
+    public static function isValidName($name, $strict = false) : bool
     {
         $constants = self::getConstants();
         if ($strict) {
@@ -57,39 +57,38 @@ abstract class BasicEnum
         return in_array(strtolower($name), $keys);
     }
 
-	/**
-	 * Get the enum-name for the provided $value
-	 * @param type $value
-	 * @param type $strict
-	 * @return mixed enum-name | null
-	 */
+    /**
+     * Get the enum-name for the provided $value
+     * @param mixed const|int|string $value
+     * @param bool $strict optional parameter for array_search(), default false
+     * @return mixed enum-name | null
+     */
     public static function getName($value, $strict = false)
     {
-        $values = array_values(self::getConstants());
-		$key = array_search($value, $values, $strict);
+        $key = array_search($value, self::getConstants(), $strict);
         if ($key !== false) {
             return $key;
-		}
-	}
+        }
+    }
 
-	/**
-	 * Check whether $value is a valid enum-member
-	 * @param type $value
-	 * @param type $strict
-	 * @return bool
-	 */
+    /**
+     * Check whether $value is a valid enum-member
+     * @param mixed const|int|string $value
+     * @param bool $strict optional parameter for in_array(), default false
+     * @return bool
+     */
     public static function isValidValue($value, $strict = false) : bool
     {
         $values = array_values(self::getConstants());
         return in_array($value, $values, $strict);
     }
 
-	/**
-	 * Get the enum-value for the provided $name
-	 * @param type $name
-	 * @param type $strict
-	 * @return mixed enum-value | null
-	 */
+    /**
+     * Get the enum-value for the provided $name
+     * @param mixed string|const $name
+     * @param bool $strict optional flag whether to require exact case-match, default false
+     * @return mixed enum-value | null
+     */
     public static function getValue($name, $strict = false)
     {
         $constants = self::getConstants();
