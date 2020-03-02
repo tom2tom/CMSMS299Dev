9bf063f471e2da9ecd94fd11232ce14fc8ef759f
tom2tom <tpgww@onepost.net>
2020-03-03 07:43:54 +1100

    renamed class

---
+++ lib/classes/class.FileSystemProfile.php
@@ -0,0 +1,217 @@
+<?php
+# Class defining folder-specific properties and permissions
+# Copyright (C) 2016-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+# Thanks to Robert Campbell and all other contributors from the CMSMS Development Team.
+# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+namespace CMSMS;
+
+use CmsInvalidDataException;
+use CMSMS\FileType;
+use function cms_to_bool;
+
+/**
+ * A simple class that defines a suite of properties and permissions,
+ * for use by e.g. a filepicker to indicate how it should behave and what
+ * functionality should be provided.
+ *
+ * This is an immutable class.
+ *
+ * The constructor and overrideWith methods of this class accept an associative array of parameters (see the properties below)
+ * to allow building or altering a profile object.  Ths is the only time when properties of a profile can be adjusted.
+ *
+ * ```php
+ * $obj = new CMSMS\FilePickerProfile( [ 'type'=>FileType::TYPE_IMAGE,
+ *    'exclude_prefix'=>'foo' ] );
+ *
+ * @package CMS
+ * @license GPL
+ * @author Robert Campbell <calguy1000@cmsmadesimple.org>
+ * @since 2.3
+ * @since 2.2 as FilePickerProfile
+ * @property-read string $top The top directory for the filepicker (relative to the CMSMS uploads directory, or ... TODO)
+ * @property-read FileType $type A FileType enumerator representing files which may be used.
+ * @property-read string $typename enum identifier corresponding to $type.
+ * @property-read string $match_prefix Identifier of filenames to be processed. Default ''
+ * @property-read string exclude_prefix  Identifier of filenames to be skipped. Default ''
+ * @property-read int $can_mkdir  Whether new directories may be created here.
+ * @property-read int $can_upload  Whether new files (of the specified type) may be uploaded to here. 
+ * @property-read int $can_delete  Whether files/folders may be removed from here.
+ * @property-read bool $show_thumbs Whether image thumbnails should be used. Default true.
+ * @property-read bool $show_hidden Whether hidden files should be included when processing folder content. Default false.
+ * @property-read bool $sort Whether files here should be sorted before listing. Default true.
+ */
+class FileSystemProfile
+{
+    const FLAG_NONE = 0;
+    const FLAG_NO = 0;
+    const FLAG_YES = 1;
+    const FLAG_BYGROUP = 2;
+
+    /**
+     * @ignore
+     */
+    protected $_data = [
+       'top'=>'',
+       'type'=>FileType::ANY,
+       'can_upload'=>self::FLAG_YES,
+       'can_delete'=>self::FLAG_YES,
+       'can_mkdir'=>self::FLAG_YES,
+       'match_prefix'=>'',
+       'exclude_prefix'=>'',
+       'show_thumbs'=>true,
+       'show_hidden'=>false,
+       'sort'=>true
+    ];
+
+    /**
+     * Constructor
+     *
+     * @param array $params Optional associative array of params suitable for the setValue method. Default null
+      */
+    public function __construct( array $params = null )
+    {
+        if( !is_array($params) || !count($params) ) return;
+        foreach( $params as $key => $val ) {
+            $this->setValue($key,$val);
+        }
+    }
+
+    /**
+     * @ignore
+     * @param string $key
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        switch( $key ) {
+        case 'top':
+            return trim($this->_data[$key]);
+
+        case 'type': // FileType enum member
+        case 'can_mkdir': // self::FLAG_* value, sometimes non-0 handled just as true
+        case 'can_upload':
+        case 'can_delete':
+            return (int) $this->_data[$key];
+
+        case 'show_thumbs':
+        case 'show_hidden':
+        case 'sort':
+            return (bool) $this->_data[$key];
+
+        case 'typename':
+            return FileType::getName($this->_data['type']);
+        }
+    }
+
+    /**
+     * @ignore
+     * @param string $key
+     * @param mixed $val
+     */
+    public function __set( $key, $val )
+    {
+        $this->setValue($key, $val);
+    }
+
+    /**
+     * Set a value into this profile
+     *
+     * @param string $key The key to set
+     * @param mixed $val The value to set
+     */
+    protected function setValue( $key, $val )
+    {
+        switch( $key ) {
+        case 'top':
+           //TODO relevant path-checks
+        case 'match_prefix':
+        case 'exclude_prefix':
+            $this->_data[$key] = trim($val);
+            break;
+
+        case 'type':
+            if( is_numeric($val) ) {
+                $n = (int)$val;
+                if( FileType::isValidValue($n) ) {
+                    $this->_data[$key] = $n;
+                    break;
+                }
+                throw new CmsInvalidDataException("$val is not a valid value for $key in ".self::class);
+            }
+            // no break here
+        case 'typename':
+            $s = strtoupper(trim($val));
+            if( ($n = FileType::getValue($s)) !== null ) {
+                $this->_data['type'] = $n;
+                break;
+            }
+            throw new CmsInvalidDataException("$val is not a valid value for $key in ".self::class);
+
+        case 'can_mkdir':
+        case 'can_delete':
+        case 'can_upload':
+            if( is_string($val) ) {
+                $n = (cms_to_bool($val)) ? self::FLAG_YES : self::FLAG_NONE;
+            } else {
+                $n = (int) $val;
+            }
+            switch( $n ) {
+            case self::FLAG_NONE:
+            case self::FLAG_YES:
+            case self::FLAG_BYGROUP:
+                $this->_data[$key] = $val;
+                break;
+            default:
+                throw new CmsInvalidDataException("$val is not a valid value for $key in ".self::class);
+            }
+            break;
+
+        case 'sort':
+           //TODO relevant checks
+        case 'show_thumbs':
+        case 'show_hidden':
+            $this->_data[$key] = cms_to_bool( $val );
+            break;
+        }
+    }
+
+    /**
+     * Create a new profile object based on the current one, with
+     *  property-adjustments per $params.
+     *
+     * @param array $params Associative array of parameters for the setValue method
+     * @return FileSystemProfile
+     */
+    public function overrideWith( array $params )
+    {
+        $obj = clone $this;
+        foreach( $params as $key => $val ) {
+            $obj->setValue( $key, $val );
+        }
+        return $obj;
+    }
+
+    /**
+     * Get the raw data of this profile
+     *
+     * @internal
+     * @return array
+     */
+    public function getRawData()
+    {
+        return $this->_data;
+    }
+} // class
