740867878d4b12487a0db9c97d5548e0b2cd7b77
tom2tom <tpgww@onepost.net>
2020-03-12 19:24:34 +1100

    namespace cleanup

--- lib/classes/class.ProfileValue.php
+++ lib/classes/class.ProfileValue.php
@@ -16,8 +16,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
-
 namespace CMSMS;
+
+use CMSMS\BasicEnum;
 
 class ProfileValue extends BasicEnum
 {
