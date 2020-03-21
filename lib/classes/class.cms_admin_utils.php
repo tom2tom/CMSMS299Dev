0ebc1d22c3e0aa242f1ee2f096d20fc661b4f6ea
tom2tom <tpgww@onepost.net>
2020-03-21 15:19:04 +1100

    rationalize class aliasing

--- lib/classes/class.cms_admin_utils.php
+++ lib/classes/class.cms_admin_utils.php
@@ -1,4 +1,4 @@
 <?php
 assert(empty(CMS_DEPREC), new DeprecationNotice('class','CMSMS\\AdminUtils'));
 require_once __DIR__.DIRECTORY_SEPARATOR.'class.AdminUtils.php';
-\class_alias('CMSMS\\AdminUtils', 'cms_admin_utils', false);
+\class_alias(AdminUtils::class, 'cms_admin_utils', false);
--- lib/classes/class.cms_mailer.php
+++ lib/classes/class.cms_mailer.php
@@ -1,4 +1,4 @@
 <?php
 assert(empty(CMS_DEPREC), new DeprecationNotice('class','CMSMS\\Mailer'));
 require_once __DIR__.DIRECTORY_SEPARATOR.'class.Mailer.php';
-\class_alias('CMSMS\\Mailer', 'cms_mailer', false);
+\class_alias(Mailer::class, 'cms_mailer', false);
