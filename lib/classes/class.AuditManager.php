e2e591fca36ab6ecda5733c0d019a983c2bef788
tom2tom <tpgww@onepost.net>
2020-03-23 11:03:39 +1100

    add more 2.3 aliasers

---
+++ lib/classes/class.AuditManager.php
@@ -0,0 +1,5 @@
+<?php
+//deprecated stub file - see replacement classfile instead
+assert(empty(CMS_DEPREC), new DeprecationNotice('Class file '.basename(__FILE__).' used'));
+require_once __DIR__.DIRECTORY_SEPARATOR.'class.AuditOperations.php';
+\class_alias('CMSMS\AuditOperations', 'AuditManager', false);
