0f73c2971937198a54e8104d2656ff7708adf2f7
tom2tom <tpgww@onepost.net>
2020-03-28 19:29:36 +1100

    conform moved class file

--- lib/classes/internal/class.global_cachable.php
+++ lib/classes/internal/class.global_cachable.php
@@ -1,4 +1,4 @@
 <?php
-assert(empty(CMS_DEPREC), new DeprecationNotice('class', 'CMSMS\\internal\\SysDataCacheDriver'));
-require_once __DIR__.DIRECTORY_SEPARATOR.'class.SysDataCacheDriver.php';
-\class_alias('CMSMS\internal\SysDataCacheDriver', 'CMSMS\internal\global_cachable', false);
+assert(empty(CMS_DEPREC), new DeprecationNotice('class', 'CMSMS\\SysDataCacheDriver'));
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'class.SysDataCacheDriver.php';
+\class_alias('CMSMS\SysDataCacheDriver', 'CMSMS\internal\global_cachable', false);
--- lib/classes/internal/class.global_cache.php
+++ lib/classes/internal/class.global_cache.php
@@ -1,4 +1,4 @@
 <?php
-assert(empty(CMS_DEPREC), new DeprecationNotice('class', 'CMSMS\\internal\\SysDataCache'));
-require_once __DIR__.DIRECTORY_SEPARATOR.'class.SysDataCache.php';
-\class_alias('CMSMS\internal\SysDataCache', 'CMSMS\internal\global_cache', false);
+assert(empty(CMS_DEPREC), new DeprecationNotice('class', 'CMSMS\\SysDataCache'));
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'class.SysDataCache.php';
+\class_alias('CMSMS\SysDataCache', 'CMSMS\internal\global_cache', false);
