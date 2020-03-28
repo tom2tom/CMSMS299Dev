cd67f8343288569f9710868018f1c2fb0ee31791
tom2tom <tpgww@onepost.net>
2020-03-28 13:16:54 +1100

    import from 2.3DEV unused here

---
+++ lib/classes/class.frontend_theme_placeholder.php
@@ -0,0 +1,19 @@
+<?php
+//deprecated stub file
+//class introduced in 2.3, pretty-much useless, unused since 2.8
+namespace CMSMS;
+
+use DeprecationNotice;
+use const CMS_DEPREC;
+
+assert(empty(CMS_DEPREC), new DeprecationNotice('Class file '.basename(__FILE__).' used'));
+
+class frontend_theme_placeholder
+{
+    public function get_exported_page_templates() {}
+    public function get_location() {}
+    public function get_urlbase() {}
+    public function has_template() {}
+    public function get_template_file() {}
+}
+
