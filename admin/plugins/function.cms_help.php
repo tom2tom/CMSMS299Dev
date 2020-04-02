0e5b3060d24e586335b71070839981d73c03be02
tom2tom <tpgww@onepost.net>
2020-04-03 08:26:25 +1100

    update API text

--- admin/plugins/function.cms_help.php
+++ admin/plugins/function.cms_help.php
@@ -1,6 +1,6 @@
 <?php
-#Plugin to...
-#Copyright (C) 2004-2019 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+#Plugin to generate page content enabling popup help
+#Copyright (C) 2004-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
 #Thanks to Ted Kulp and all other contributors from the CMSMS Development Team.
 #This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
