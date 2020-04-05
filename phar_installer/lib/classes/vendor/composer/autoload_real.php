e01546c73d5dc3a8db8e9a42f94acace068a013e
tom2tom <tpgww@onepost.net>
2020-04-05 13:22:16 +1000

    re-install PHParchive via composer

--- phar_installer/lib/classes/vendor/composer/autoload_real.php
+++ phar_installer/lib/classes/vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit761e0cd0b509946f5f906763270c2fa4
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -47,24 +50,6 @@ class ComposerAutoloaderInit761e0cd0b509946f5f906763270c2fa4
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit761e0cd0b509946f5f906763270c2fa4::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire761e0cd0b509946f5f906763270c2fa4($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequire761e0cd0b509946f5f906763270c2fa4($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
--- phar_installer/lib/classes/vendor/composer/autoload_static.php
+++ phar_installer/lib/classes/vendor/composer/autoload_static.php
@@ -6,12 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit761e0cd0b509946f5f906763270c2fa4
 {
-    public static $files = array (
+    public static $prefixLengthsPsr4 = array (
+        's' => 
+        array (
+            'splitbrain\\PHPArchive\\' => 22,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'splitbrain\\PHPArchive\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/splitbrain/php-archive/src',
+        ),
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit761e0cd0b509946f5f906763270c2fa4::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit761e0cd0b509946f5f906763270c2fa4::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
