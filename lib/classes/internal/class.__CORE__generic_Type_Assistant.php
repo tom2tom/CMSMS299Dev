a41ff59f3c29de4a8672dbe3aaaf87488f7d32c7
tom2tom <tpgww@onepost.net>
2020-04-07 23:43:20 +1000

    add license

--- lib/classes/internal/class.__CORE__generic_Type_Assistant.php
+++ lib/classes/internal/class.__CORE__generic_Type_Assistant.php
@@ -1,8 +1,20 @@
 <?php
+#Class for getting template help
+#Copyright (C) 2016-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-/**
- * @ignore
- */
 namespace CMSMS\internal;
 
 use CMSMS\Layout\TemplateTypeAssistant;
@@ -14,7 +26,6 @@ class __CORE__generic_Type_Assistant extends TemplateTypeAssistant
 {
     public function &get_type()
     {
-
     }
 
     public function get_usage_string($name)
