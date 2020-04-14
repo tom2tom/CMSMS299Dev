20456db4ae731f1455b815489c93c52532a65377
tom2tom <tpgww@onepost.net>
2020-04-14 18:58:10 +1000

    improved config data processing

--- phar_installer/lib/classes/wizard/class.wizard_step8.php
+++ phar_installer/lib/classes/wizard/class.wizard_step8.php
@@ -5,14 +5,14 @@ namespace cms_installer\wizard;
 use cms_config;
 use cms_installer\wizard\wizard_step;
 use cms_siteprefs;
-use cms_utils;
 use CmsApp;
 use CMSMS\AppState;
 use Exception;
+use Throwable;
 use function cms_installer\get_app;
 use function cms_installer\lang;
 use function cms_installer\smarty;
-use function GetDb;
+use function cms_installer\GetDb;
 
 class wizard_step8 extends wizard_step
 {
@@ -22,39 +22,38 @@ class wizard_step8 extends wizard_step
     }
 
     /**
+     * Get a handle to the system database.
+     * Without using CmsApp::GetDb(), which doesn't accept $config data
+     * and effectively assumes $config (including db connection params)
+     * is already set. But here it aint so!
+     *
      * @param array $destconfig parameters for db connection
      * @return mixed Connection-class object or error message string
      */
     private function db_connect(array $destconfig)
     {
-        require_once dirname(__DIR__,2).DIRECTORY_SEPARATOR.'dbaccessor.functions.php';
+//      if( !defined('CMS_DB_PREFIX') ) define('CMS_DB_PREFIX',$destconfig['db_prefix']);
+        require_once dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'dbaccessor.functions.php';
         try {
             $db = GetDb($destconfig);
         }
-        catch( Exception $e ) {
-            return $e->getMessage();
+        catch( Throwable $t ) {
+            return $t->getMessage();
         }
-        if( !defined('CMS_DB_PREFIX') ) define('CMS_DB_PREFIX',$destconfig['db_prefix']);
-        $db->Execute("SET NAMES 'utf8'");
+        AppState::add_state(AppState::STATE_INSTALL);
         CmsApp::get_instance()->_setDb($db);
         return $db;
     }
 
+    /**
+     * Setup and initialize the CMSMS API's
+     * @param string $destdir filepath of top-directory of installation tree
+     */
     private function connect_to_cmsms(string $destdir)
     {
-/* downstream included file sets this
-        global $CMS_VERSION;
-
-        $info = $this->get_wizard()->get_data('version_info'); // N/A during install
-        if( $info && !empty($info['version']) ) {
-            $CMS_VERSION = $info['version'];
-        } else {
-            $CMS_VERSION = '2.8.900'; //TODO include version.php file?
-        }
-*/
         require_once $destdir.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'class.AppState.php';
         AppState::add_state(AppState::STATE_INSTALL);
-        // setup and initialize the CMSMS API's
+
         $fp = $destdir.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'include.php';
         if( is_file($fp) ) {
             include_once $fp;
@@ -66,75 +65,98 @@ class wizard_step8 extends wizard_step
 
     /**
      * @ignore
-     * @throws Exception
      */
     private function do_install()
     {
         $app = get_app();
-        $destdir = $app->get_destdir();
-        if( !$destdir ) throw new Exception(lang('error_internal',800));
-
         $wiz = $this->get_wizard();
-        $adminaccount = $wiz->get_data('adminaccount');
-        if( !$adminaccount ) throw new Exception(lang('error_internal',801));
 
-        $destconfig = $wiz->get_data('config');
-        if( !$destconfig ) throw new Exception(lang('error_internal',802));
-
-        $siteinfo = $wiz->get_data('siteinfo');
-        if( !$siteinfo ) throw new Exception(lang('error_internal',803));
-
-        $cachtype = $wiz->get_data('cachemode');
-
-        // create new config.php file to enable database connection
-        $this->write_config();
-
-        $this->connect_to_cmsms($destdir);
-
-        // connect to the database, if possible
-        $db = $this->db_connect($destconfig);
-
-        $dir = dirname(__DIR__,2).DIRECTORY_SEPARATOR.'install';
-/*
-        $fn = dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'assets'.DIRECTORY_SEPARATOR.'install'.DIRECTORY_SEPARATOR;
-        require_once $fn.'base.php';
-        require_once $fn.'schema.php';
-        require_once $fn.'createseq.php';
-*/
+        $destdir = $app->get_destdir();
         try {
-            if( !is_object($db) ) {
-                throw new Exception($db); //report the error
-            }
+            if( !$destdir ) throw new Exception(lang('error_internal', 800));
 
+            $adminaccount = $wiz->get_data('adminaccount');
+            if( !$adminaccount ) throw new Exception(lang('error_internal', 801));
+
+            $destconfig = $wiz->get_data('config');
+            if( !$destconfig ) throw new Exception(lang('error_internal', 802));
+
+            $siteinfo = $wiz->get_data('siteinfo');
+            if( !$siteinfo ) throw new Exception(lang('error_internal', 803));
+
+            // create temporary dummy config file, to enable database connection
+            // during CMSMS init
+            $dmycfg = $destdir.DIRECTORY_SEPARATOR.'config.php';
+
+            $app_config = $app->get_config();
+            $admin = ( !empty($app_config['admindir']) && $app_config['admindir'] != 'admin' ) ? $app_config['admindir'] : '';
+            $assets = ( !empty($app_config['assetsdir']) && $app_config['assetsdir'] != 'assets' ) ? $app_config['assetsdir'] : '';
+            $tags = ( !empty($app_config['pluginsdir']) && $app_config['pluginsdir'] != 'simple_plugins' ) ? $app_config['pluginsdir'] : '';
+            $host = trim($destconfig['db_hostname']);
+            $name = trim($destconfig['db_name']);
+            if( empty($destconfig['db_port']) ) {
+                $port = "''"; // filter it out
+            } else {
+                $port = (int)$destconfig['db_port'];
+                if( $port < 1 ) $port = "''";
+            }
+            $user = trim($destconfig['db_username']);
+            $pass = trim($destconfig['db_password']);
+            if( empty($destconfig['db_prefix']) ) {
+                $pref = 'cms_';
+            } else {
+                $pref = trim($destconfig['db_prefix']);
+            }
+            $zone = (!empty($destconfig['timezone'])) ? trim($destconfig['timezone']) : '';
+            $set = $zone ? true : "''";
+            file_put_contents($dmycfg, <<<EOS
+<?php
+\$config = array_filter([
+'admin_dir' => '$admin',
+'assets_dir' => '$assets',
+'db_hostname' => '$host',
+'db_name' => '$name',
+'db_port' => $port,
+'db_username' => '$user',
+'db_password' => '$pass',
+'db_prefix' => '$pref',
+'simpletags_dir' => '$tags',
+'timezone' => '$zone',
+'set_db_timezone' => $set,
+'set_names' => true,
+], function(\$v){ return \$v !== ''; }) + (\$config ?? []);
+
+EOS
+            , LOCK_EX);
+            $this->connect_to_cmsms($destdir);
+
+            // now we can save the 'real' config file
+            $this->write_config($destconfig);
+            $fp = str_replace('config.php','bak.config.php', $dmycfg);
+            @unlink($fp);
+
+            // connect to the database, if possible
+            $db = $this->db_connect($destconfig);
+            if( !is_object($db) ) throw new Exception($db);
+
+            $dir = dirname(__DIR__, 2);
             // install main tables
-            $fn = $dir.DIRECTORY_SEPARATOR.'schema.php';
-            if( !is_file($fn) ) throw new Exception(lang('error_internal',805));
-            require_once $fn;
+            $fp = $dir.DIRECTORY_SEPARATOR.'method.schema.php';
+            if( !is_file($fp) ) throw new Exception(lang('error_internal', 805));
+            require_once $fp;
 
             // install sequence tables
-            require_once $dir.DIRECTORY_SEPARATOR.'createseq.php';
+            $fp = $dir.DIRECTORY_SEPARATOR.'method.sequence.php';
+            if( !is_file($fp) ) throw new Exception(lang('error_internal', 806));
+            require_once $fp;
 
-            // create tmp directories
-            $this->verbose(lang('install_createtmpdirs'));
-            $fp = constant('TMP_CACHE_LOCATION');
-            if( !$fp ) $fp = $destdir.DIRECTORY_SEPARATOR.'tmp/cache';
-            @mkdir($fp,0771,TRUE);
-            touch($fp.DIRECTORY_SEPARATOR.'index.html');
-            $fp = constant('PUBLIC_CACHE_LOCATION');
-            if( !$fp ) $fp = $destdir.DIRECTORY_SEPARATOR.'tmp/cache/public';
-            @mkdir($fp,0771,TRUE);
-            touch($fp.DIRECTORY_SEPARATOR.'index.html');
-            $fp = constant('TMP_TEMPLATES_C_LOCATION');
-            if( !$fp ) $fp = $destdir.DIRECTORY_SEPARATOR.'tmp/templates_c';
-            @mkdir($fp,0771,TRUE);
-            touch($fp.DIRECTORY_SEPARATOR.'index.html');
-
-            // site-preferences, permisssions etc
-            require_once $dir.DIRECTORY_SEPARATOR.'base.php';
+            // site-places, properties, user, groups, permisssions etc
+            $fp = $dir.DIRECTORY_SEPARATOR.'method.settings.php';
+            if( !is_file($fp) ) throw new Exception(lang('error_internal', 807));
+            require_once $fp;
         }
-        catch( Exception $e ) {
-            die('exception: '.$e->GetMessage());
-//            $this->error($e->GetMessage());
+        catch( Throwable $t ) {
+            die('Error: '.$t->GetMessage());
         }
     }
 
@@ -145,20 +167,20 @@ class wizard_step8 extends wizard_step
      */
     private function do_upgrade(array $version_info)
     {
-        global $CMS_VERSION;
+//        global $CMS_VERSION; don't pollute globals
 
-        $info = $this->get_wizard()->get_data('version_info');
-        $CMS_VERSION = $info['version'];
+//        $info = $this->get_wizard()->get_data('version_info');
+//        $CMS_VERSION = $info['version'];
 
         // get the list of all available versions that this upgrader knows about
         $app = get_app();
-        $dir = $app->get_assetsdir().DIRECTORY_SEPARATOR.'upgrade';
-        if( !is_dir($dir) ) throw new Exception(lang('error_internal',810));
+        $dir = dirname(__DIR__2).DIRECTORY_SEPARATOR.'upgrade';
+        if( !is_dir($dir) ) throw new Exception(lang('error_internal', 810));
         $destdir = $app->get_destdir();
-        if( !$destdir ) throw new Exception(lang('error_internal',811));
+        if( !$destdir ) throw new Exception(lang('error_internal', 811));
 
         $dh = opendir($dir);
-        if( !$dh ) throw new Exception(lang('error_internal',812));
+        if( !$dh ) throw new Exception(lang('error_internal', 812));
         $versions = [];
         while( ($file = readdir($dh)) !== false ) {
             if( $file == '.' || $file == '..' ) continue;
@@ -168,10 +190,10 @@ class wizard_step8 extends wizard_step
         if( $versions ) usort($versions,'version_compare');
 
         $destconfig = $this->get_wizard()->get_data('config');
-        if( !$destconfig ) throw new Exception(lang('error_internal',820));
+        if( !$destconfig ) throw new Exception(lang('error_internal', 820));
 
         $siteinfo = $this->get_wizard()->get_data('siteinfo');
-        if( !$siteinfo ) throw new Exception(lang('error_internal',821));
+        if( !$siteinfo ) throw new Exception(lang('error_internal', 821));
 
         // setup and initialize the CMSMS API's
         require_once $destdir.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.'class.AppState.php';
@@ -182,7 +204,6 @@ class wizard_step8 extends wizard_step
         }
         else {
             require_once $destdir.DIRECTORY_SEPARATOR.'include.php';
-
         }
 
         // setup database connection
@@ -195,26 +216,26 @@ class wizard_step8 extends wizard_step
             $current_version = $version_info['version'];
             foreach( $versions as $ver ) {
                 if( version_compare($current_version,$ver) >= 0 ) continue;
-                $fn = "$dir/$ver/upgrade.php";
-                if( !is_file($fn) ) continue;
-                include_once $fn;
+                $fp = "$dir/$ver/upgrade.php";
+                if( !is_file($fp) ) continue;
+                include_once $fp;
             }
 
-            $this->write_config();
+            $this->write_config($destconfig);
         }
-        catch( Exception $e ) {
-            $this->error($e->GetMessage());
+        catch( Throwable $t ) {
+            $this->error($t->GetMessage());
         }
 
+        // some settings should be automatically updated after every upgrade
         $corenames = $app->get_config()['coremodules'];
         $cores = implode(',',$corenames);
-        $uuid = trim(base64_encode(cms_utils::random_string(24)), '='); //db hates storing some chars
+        $schema = $app->get_dest_schema();
         $arr = [
             'coremodules' => $cores, // aka ModuleOperations::CORENAMES_PREF
-            'site_uuid' => $uuid, // almost-certainly-unique signature of this site
-            'ultraroles' => json_encode(['Modify Restricted Files','Modify DataBase Direct','Remote Administration']),
+            'schema_version' => $schema,
         ];
-        if( issset($siteinfo['supporturl']) ) { //TODO only if verbose etc
+        if( !empty($siteinfo['supporturl']) ) {
             $arr['site_help_url'] = $siteinfo['supporturl'];
         }
         foreach ($arr as $name=>$val) {
@@ -227,19 +248,16 @@ class wizard_step8 extends wizard_step
         // nothing here
     }
 
-    private function write_config()
+    private function write_config($destconfig)
     {
-        $destconfig = $this->get_wizard()->get_data('config');
-        if( !$destconfig ) throw new Exception(lang('error_internal',830));
-
         $destdir = get_app()->get_destdir();
-        if( !$destdir ) throw new Exception(lang('error_internal',831));
+        if( !$destdir ) throw new Exception(lang('error_internal', 831));
 
-        $fn = $destdir.DIRECTORY_SEPARATOR.'config.php';
-        if( is_file($fn) ) {
+        $fp = $destdir.DIRECTORY_SEPARATOR.'config.php';
+        if( is_file($fp) ) {
             $this->verbose(lang('install_backupconfig'));
             $destfn = $destdir.DIRECTORY_SEPARATOR.'bak.config.php';
-            if( !copy($fn,$destfn) ) throw new Exception(lang('error_backupconfig'));
+            if( !copy($fp,$destfn) ) throw new Exception(lang('error_backupconfig'));
         }
 
         $this->message(lang('install_createconfig'));
@@ -250,19 +268,21 @@ class wizard_step8 extends wizard_step
         AppState::add_state(AppState::STATE_INSTALL); //enable $config property-setting
         require_once $fp.'classes'.DIRECTORY_SEPARATOR.'class.cms_config.php';
         $newconfig = cms_config::get_instance();
+
 //      $newconfig['dbms'] = 'mysqli'; //trim($destconfig['db_type']); redundant always mysqli
         $newconfig['db_hostname'] = trim($destconfig['db_hostname']);
+        if( !empty($destconfig['db_port']) ) {
+            $num = (int)$destconfig['db_port'];
+            if( $num > 0 ) $newconfig['db_port'] = $num;
+        }
         $newconfig['db_username'] = trim($destconfig['db_username']);
         $newconfig['db_password'] = trim($destconfig['db_password']);
         $newconfig['db_name'] = trim($destconfig['db_name']);
         $newconfig['db_prefix'] = trim($destconfig['db_prefix']);
         $newconfig['timezone'] = trim($destconfig['timezone']);
-        if( $destconfig['query_var'] ) $newconfig['query_var'] = trim($destconfig['query_var']);
-        if( isset($destconfig['db_port']) ) {
-            $num = (int)$destconfig['db_port'];
-            if( $num > 0 ) $newconfig['db_port'] = $num;
-        }
-        $newconfig->save(true,$fn);
+        if( !empty($destconfig['query_var']) ) $newconfig['query_var'] = trim($destconfig['query_var']);
+
+        $newconfig->save();
     }
 
     protected function display()
@@ -286,11 +306,11 @@ class wizard_step8 extends wizard_step
                 $this->do_install();
             }
             else {
-                throw new Exception(lang('error_internal',840));
+                throw new Exception(lang('error_internal', 840));
             }
         }
-        catch( Exception $e ) {
-            $this->error($e->GetMessage());
+        catch( Throwable $t ) {
+            $this->error($t->GetMessage());
         }
 
         $this->finish();
