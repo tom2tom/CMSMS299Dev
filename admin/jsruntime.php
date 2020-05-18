280f902b9fafd5b07f13b0b1f8b8839fd69920c0
tom2tom <tpgww@onepost.net>
2020-05-18 17:26:41 +1000

    new version handles runtime variables differently

--- admin/jsruntime.php
+++ admin/jsruntime.php
@@ -15,55 +15,55 @@
 #You should have received a copy of the GNU General Public License
 #along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+use CMSMS\AppParams;
+
 $urlext = get_secure_param();
 
-$data = [];
-$data['admin_url'] = $config['admin_url'];
-$data['ajax_alerts_url'] = $config['admin_url'].'/ajax_alerts.php'.$urlext;
-$data['ajax_help_url'] = $config['admin_url'].'/ajax_help.php'.$urlext;
-$data['job_key'] = CMS_JOB_KEY;
-$data['lang_alert'] = lang('alert');
-$data['lang_cancel'] = lang('cancel');
-$data['lang_choose'] = lang('choose'); //filepicker-specific
-$data['lang_close'] = lang('close');
-$data['lang_confirm'] = lang('confirm');
-$data['lang_confirm_leave'] = lang('confirm_leave');
-$data['lang_disabled'] = lang('disabled');
-$data['lang_error'] = lang('error');
-$data['lang_gotit'] = lang('gotit');
-$data['lang_largeupload'] = lang('upload_largeupload');
-$data['lang_no'] = lang('no');
-$data['lang_none'] = lang('none');
-$data['lang_ok'] = lang('ok');
-$data['lang_select_file'] = lang('select_file'); //filepicker-specific
-$data['lang_title_help'] = lang('help');
-$data['lang_yes'] = lang('yes');
-$data['max_upload_size'] = $config['max_upload_size'];
-$data['root_url'] = CMS_ROOT_URL;
-$data['secure_param_name'] = CMS_SECURE_PARAM_NAME;
-$data['uploads_url'] = $config['uploads_url'];
-$data['user_key'] = $_SESSION[CMS_USER_KEY];
+if (!isset($vars)) { $vars = []; }
 
-$c = count($data) - 1; // special-case the last member
+$vars['root_url'] = CMS_ROOT_URL;
+$vars['admin_url'] = $config['admin_url'];
+$vars['ajax_alerts_url'] = $config['admin_url'].'/ajax_alerts.php'.$urlext;
+$vars['ajax_help_url'] = $config['admin_url'].'/ajax_help.php'.$urlext;
+$vars['job_key'] = CMS_JOB_KEY;
+$vars['secure_param_name'] = CMS_SECURE_PARAM_NAME;
+/* NOPE!!  hack-facilitator
+$vars['user_key'] = $_SESSION[CMS_USER_KEY];
+*/
+$vars['lang_alert'] = lang('alert');
+$vars['lang_cancel'] = lang('cancel');
+$vars['lang_close'] = lang('close');
+$vars['lang_confirm'] = lang('confirm');
+$vars['lang_confirm_leave'] = lang('confirm_leave');
+$vars['lang_disabled'] = lang('disabled');
+$vars['lang_error'] = lang('error');
+$vars['lang_gotit'] = lang('gotit');
+$vars['lang_no'] = lang('no');
+$vars['lang_none'] = lang('none');
+$vars['lang_ok'] = lang('ok');
+$vars['lang_title_help'] = lang('help');
+$vars['lang_yes'] = lang('yes');
 
-$_out_ = <<<EOS
-var ex = {
+// is the website down for maintenance?
+if (AppParams::get('site_downnow')) {
+    $vars['lang_maintenance_warning'] = lang('maintenance_warning');
+    $vars['sitedown'] = true;
+} else {
+    $vars['sitedown'] = false;
+}
+
+$obj = new stdClass();
+reset($vars);
+foreach ($vars as $key => $value) {
+    $obj->$key = $value;
+}
+$enc = json_encode($obj, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+$js = <<<EOS
+<script type="text/javascript">
+//<![CDATA[
+ var cms_runtime = '$enc',
+  cms_data = {};
+//]]>
+</script>
 
 EOS;
-for ($i=0; $i<$c; ++$i) {
-    $key = key($data);
-    $value = json_encode(current($data));
-    $_out_ .= " $key: $value,\n";
-    next($data);
-}
-$key = key($data);
-$value = json_encode(current($data));
-$_out_ .= <<<EOS
- $key: $value
-};
-if(typeof cms_data === 'undefined') {
- var cms_data = {};
-}
-Object.keys(ex).forEach(function(key) { cms_data[key] = ex[key]; });
-EOS;
-
