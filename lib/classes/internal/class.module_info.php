3ba3923d3deca5e58ec0f260e0017daf44f3108c
tom2tom <tpgww@onepost.net>
2020-05-21 23:05:03 +1000

    namespace correction

--- lib/classes/internal/class.module_info.php
+++ lib/classes/internal/class.module_info.php
@@ -20,8 +20,8 @@ namespace CMSMS\internal;
 
 use ArrayAccess;
 use CmsLogicException;
+use CMSMS\ModuleOperations;
 use CMSMS\SysDataCache;
-use ModuleOperations;
 use const CMS_ASSETS_PATH;
 use const CMS_VERSION;
 use function cms_join_path;
@@ -57,7 +57,7 @@ class module_info implements ArrayAccess
 
     public function __construct($module_name,$can_load = true)
     {
-		$arr = $this->_read_from_module_cache($module_name);
+        $arr = $this->_read_from_module_cache($module_name);
         if( $arr ) {
             $arr2 = $this->_check_modulecustom($module_name);
             $this->_setData(array_merge($arr2, $arr));
@@ -155,7 +155,7 @@ class module_info implements ArrayAccess
                 return ['has_custom' => true];
             }
 //        } else {
-			//TODO exception?
+            //TODO exception?
         }
 
         $path = cms_join_path(CMS_ASSETS_PATH,'module_custom',$module_name,'');
@@ -195,41 +195,41 @@ class module_info implements ArrayAccess
         $arr['lazyloadfrontend'] = cms_to_bool(get_parameter_value($data,'lazyloadfrontend',FALSE));
 
         if( isset($inidata['depends']) ) {
-			$arr['depends'] = $inidata['depends'];
-		}
+            $arr['depends'] = $inidata['depends'];
+        }
 
-		foreach( [
-			'changelog.inc',
-			'changelog.htm',
-			'doc'.DIRECTORY_SEPARATOR.'changelog.inc',
-			'doc'.DIRECTORY_SEPARATOR.'changelog.htm',
-		] as $one ) {
-	        $path = cms_join_path($dir,$one);
-			if( is_file($path) ) {
-				$arr['changelog'] = file_get_contents($path);
-				break;
-			}
-		}
+        foreach( [
+            'changelog.inc',
+            'changelog.htm',
+            'doc'.DIRECTORY_SEPARATOR.'changelog.inc',
+            'doc'.DIRECTORY_SEPARATOR.'changelog.htm',
+        ] as $one ) {
+            $path = cms_join_path($dir,$one);
+            if( is_file($path) ) {
+                $arr['changelog'] = file_get_contents($path);
+                break;
+            }
+        }
 
-		foreach( [
-			'modhelp.inc',
-			'modhelp.htm',
-			'help.inc',
-			'help.htm',
-			'doc'.DIRECTORY_SEPARATOR.'modhelp.inc',
-			'doc'.DIRECTORY_SEPARATOR.'modhelp.htm',
-			'doc'.DIRECTORY_SEPARATOR.'help.inc',
-			'doc'.DIRECTORY_SEPARATOR.'help.htm',
-		] as $one ) {
-	        $path = cms_join_path($dir,$one);
-			if( is_file($path) ) {
-				$arr['help'] = file_get_contents($path);
-				break;
-			}
-		}
-		if( !isset($arr['help']) ) {
-		//TODO $mod = ; $arr['help'] = $mod->GetHelp();
-		}
+        foreach( [
+            'modhelp.inc',
+            'modhelp.htm',
+            'help.inc',
+            'help.htm',
+            'doc'.DIRECTORY_SEPARATOR.'modhelp.inc',
+            'doc'.DIRECTORY_SEPARATOR.'modhelp.htm',
+            'doc'.DIRECTORY_SEPARATOR.'help.inc',
+            'doc'.DIRECTORY_SEPARATOR.'help.htm',
+        ] as $one ) {
+            $path = cms_join_path($dir,$one);
+            if( is_file($path) ) {
+                $arr['help'] = file_get_contents($path);
+                break;
+            }
+        }
+        if( !isset($arr['help']) ) {
+        //TODO $mod = ; $arr['help'] = $mod->GetHelp();
+        }
 
         $arr['has_meta'] = TRUE;
         return $arr;
@@ -270,39 +270,39 @@ class module_info implements ArrayAccess
     /* return mixed array or null */
     private function _read_from_module_cache(string $module_name)
     {
-		$tmp = SysDataCache::get_instance()->get('modules');
-		if( is_array($tmp) ) {
-			if( isset($tmp[$module_name]) ) {
-				if( $tmp[$module_name]['status'] != 'installed' || !$tmp[$module_name]['active'] ) {
-					return null;
-				}
-				if( isset($tmp[$module_name][self::PROPNAMES[1]]) ) { // anything not in raw table data
-					return $tmp[$module_name];
-				}
-				$mod = ModuleOperations::get_instance()->get_module_instance($module_name,'',TRUE);
-				if( is_object($mod) ) {
-					unset($tmp[$module_name]['version']); // we will use the version reported by the module
-					$tmp[$module_name] += ['name' => $module_name];
+        $tmp = SysDataCache::get_instance()->get('modules');
+        if( is_array($tmp) ) {
+            if( isset($tmp[$module_name]) ) {
+                if( $tmp[$module_name]['status'] != 'installed' || !$tmp[$module_name]['active'] ) {
+                    return null;
+                }
+                if( isset($tmp[$module_name][self::PROPNAMES[1]]) ) { // anything not in raw table data
+                    return $tmp[$module_name];
+                }
+                $mod = ModuleOperations::get_instance()->get_module_instance($module_name,'',TRUE);
+                if( is_object($mod) ) {
+                    unset($tmp[$module_name]['version']); // we will use the version reported by the module
+                    $tmp[$module_name] += ['name' => $module_name];
 
-					$arr = [];
-					$arr['description'] = $mod->GetDescription();
-					if( !$arr['description'] ) { $arr['description'] = $mod->GetAdminDescription(); }
-					$arr['version'] = $mod->GetVersion(); //might be different from database version
-					$arr['depends'] = $mod->GetDependencies();
-					$arr['mincmsversion'] = $mod->MinimumCMSVersion();
-					$arr['author'] = $mod->GetAuthor();
-					$arr['authoremail'] = $mod->GetAuthorEmail();
-//					$arr['lazyloadadmin'] = $mod->LazyLoadAdmin();
-//					$arr['lazyloadfrontend'] = $mod->LazyLoadFrontend();
-					$arr['help'] = $mod->GetHelp();
-					$arr['changelog'] = $mod->GetChangelog();
+                    $arr = [];
+                    $arr['description'] = $mod->GetDescription();
+                    if( !$arr['description'] ) { $arr['description'] = $mod->GetAdminDescription(); }
+                    $arr['version'] = $mod->GetVersion(); //might be different from database version
+                    $arr['depends'] = $mod->GetDependencies();
+                    $arr['mincmsversion'] = $mod->MinimumCMSVersion();
+                    $arr['author'] = $mod->GetAuthor();
+                    $arr['authoremail'] = $mod->GetAuthorEmail();
+//                  $arr['lazyloadadmin'] = $mod->LazyLoadAdmin();
+//                  $arr['lazyloadfrontend'] = $mod->LazyLoadFrontend();
+                    $arr['help'] = $mod->GetHelp();
+                    $arr['changelog'] = $mod->GetChangelog();
 
-					$tmp[$module_name] += $arr;
-					SysDataCache::get_instance()->set('modules', $tmp);
-					return $tmp[$module_name];
-				}
-			}
-		}
-		return null;
-	}
+                    $tmp[$module_name] += $arr;
+                    SysDataCache::get_instance()->set('modules', $tmp);
+                    return $tmp[$module_name];
+                }
+            }
+        }
+        return null;
+    }
 } // class
