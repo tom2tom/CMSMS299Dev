b64a58d783831dce5f2879188f1d1577c1757187
tom2tom <tpgww@onepost.net>
2020-05-22 16:19:06 +1000

    relocated file

--- lib/moduleinterface.php
+++ lib/moduleinterface.php
@@ -16,10 +16,12 @@
 #You should have received a copy of the GNU General Public License
 #along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+use CMSMS\AppSingle;
 use CMSMS\AppState;
 use CMSMS\Events;
 use CMSMS\internal\GetParameters;
 use CMSMS\ModuleOperations;
+use CMSMS\Utils;
 
 //REMINDER: vars defined here might be used as globals by downstream hook functions
 $orig_memory = (function_exists('memory_get_usage') ? memory_get_usage() : 0);
@@ -29,23 +31,33 @@ require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'cla
 $CMS_APP_STATE = AppState::STATE_ADMIN_PAGE; // in scope for inclusion, to set initial state
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'include.php';
 
-$CMS_JOB_TYPE = CmsApp::get_instance()->JOBTYPE;
+/*
+TODO CIRCULAR WITH include.php
+$ops = new GetParameters();
+$params = $ops->decode_action_params();
+$CMS_JOB_TYPE = $params[CMS_JOB_KEY] ?? 0
+CmsApp::get_instance()->JOBTYPE = $CMS_JOB_TYPE;
+
+BACK HERE ...
 if ($CMS_JOB_TYPE < 2) {
 	check_login();
-//  $userid = get_userid();
-} else {
-	//TODO security for async ? $params[CMS_SECURE_PARAM_NAME] == $_SESSION[CMS_USER_KEY] etc
 }
+*/
 
 $ops = new GetParameters();
 $params = $ops->decode_action_params();
+
 if ($params) {
 	$modops = ModuleOperations::get_instance();
 	$module = $params['module'] ?? '';
 	$modinst = $modops->get_module_instance($module);
 	if (!$modinst) {
 		trigger_error('Module '.$module.' not found. This could indicate that the module is awaiting upgrade, or that there are other problems');
-		redirect('index.php');
+		if ($CMS_JOB_TYPE == 0) {
+			redirect('menu.php'); //TODO CMS_ROOT_URL | 'menu.php'
+		} else {
+			exit;
+		}
 	}
 //	$params['id'] =
 	$id = ($params['id'] ?? 'm1_');
@@ -56,7 +68,11 @@ if ($params) {
 	$params += $ops->retrieve_general_params($id);
 } else {
 	trigger_error('Module-action parameters not found');
-	redirect('index.php');
+	if ($CMS_JOB_TYPE == 0) {
+		redirect('menu.php'); //TODO CMS_ROOT_URL | 'menu.php'
+	} else {
+		exit;
+	}
 }
 
 if ($modinst->SuppressAdminOutput($_REQUEST)) {
@@ -67,52 +83,52 @@ if ($modinst->SuppressAdminOutput($_REQUEST)) {
 
 switch ($CMS_JOB_TYPE) {
 	case 0:
-		$themeObject = cms_utils::get_theme_object();
+		$themeObject = Utils::get_theme_object();
 		$themeObject->set_action_module($module);
-		// create a dummy template to be a proxy-parent for the action
-		$smarty = CmsApp::get_instance()->GetSmarty();
-		$template = $smarty->createTemplate('string:DUMMY PARENT');
-
+		AppSingle::insert('Theme', $themeObject);
+		$base = AppSingle::Config()['admin_path'].DIRECTORY_SEPARATOR;
+		// create a dummy template to be a proxy-parent for the action's template
+//		$smarty = AppSingle::Smarty();
+//		$template = $smarty->createTemplate('string:DUMMY PARENT');
+		$template = null;
 		// retrieve and park the action-output first, in case the action also generates header content
-		ob_start();
 		$content = $modinst->DoActionBase($action, $id, $params, null, $template);
-		if (($content && $content !== 1) || is_numeric($content)) { //ignore PHP's 'inclusion' status indicators
-			echo $content;
-		}
-		$content = ob_get_clean();
 
-		include_once 'header.php';
+		require $base.'header.php';
 		// back into the buffer,  now that 'pre-content' things are in place
 		echo $content;
 
 		if (!empty($params['module_error'])) $themeObject->RecordNotice('error', $params['module_error']);
 		if (!empty($params['module_message'])) $themeObject->RecordNotice('success', $params['module_message']);
 
-		include_once 'footer.php';
+		require $base.'footer.php';
 		break;
 	case 1: // not full-page output
-		$smarty =  CmsApp::get_instance()->GetSmarty();
-		$template = $smarty->createTemplate('string:DUMMY PARENT');
-		$content = $modinst->DoActionBase($action, $id, $params, null, $template);
-		if (($content && $content !== 1) || is_numeric($content)) { //ignore PHP's 'inclusion' status indicators
-			echo $content;
-		} else {
-			echo '';
-		}
+		$themeObject = Utils::get_theme_object();
+		$themeObject->set_action_module($module);
+		AppSingle::insert('Theme', $themeObject);
+//		$smarty =  AppSingle::Smarty();
+//		$template = $smarty->createTemplate('string:DUMMY PARENT');
+		$template = null;
+		echo $modinst->DoActionBase($action, $id, $params, null, $template);
 		break;
 	case 2:	//minimal
 		$fp = $modinst->GetModulePath().DIRECTORY_SEPARATOR.'action.'.$action.'.php';
 		if (is_file($fp)) {
 			$dojob = Closure::bind(function($filepath, $id, $params)
 			{
-				// variables in scope for convenience
+				// variables in scope for convenience c.f. CMSModeule::DoAction()
 				$gCms = CmsApp::get_instance();
 				$db = $gCms->GetDb();
 				$config = $gCms->GetConfig();
+				//no $smarty (no template-processing)
+				$uuid = $gCms->GetSiteUUID(); //since 2.9
 				include $filepath;
 			}, $modinst, $modinst);
 			$dojob($fp, $id, $params);
 		}
+		Events::SendEvent('Core', 'PostRequest');
+		exit;
 }
 
 Events::SendEvent('Core', 'PostRequest');
