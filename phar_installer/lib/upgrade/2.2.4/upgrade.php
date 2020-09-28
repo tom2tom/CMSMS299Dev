873c1530c55639528b32f92de24d5f76c6fa84f3
tom2tom <tpgww@onepost.net>
2020-09-28 10:15:46 +1000

    update permission-description

--- phar_installer/lib/upgrade/2.2.4/upgrade.php
+++ phar_installer/lib/upgrade/2.2.4/upgrade.php
@@ -1,6 +1,6 @@
 <?php
 $sql = 'SELECT permission_id FROM '.CMS_DB_PREFIX.'permissions WHERE permission_name = ?';
-$tmp = (int) $db->GetOne($sql,[ 'Manage Groups'] );
+$tmp = (int) $db->GetOne($sql, [ 'Manage Groups' ] );
 if( $tmp < 1 ) {
     status_msg('Create missing "Manage Users" Permission');
     $new_id = (int) $db->GenID(CMS_DB_PREFIX.'permissions_seq');
@@ -8,5 +8,5 @@ if( $tmp < 1 ) {
     $sql = 'INSERT INTO '.CMS_DB_PREFIX.'permissions
 (permission_id,permission_name,permission_text,permission_source,create_date)
 VALUES (?,?,?,?,NOW())';
-    $db->Execute( $sql, [ $new_id, 'Manage Groups', 'Manage Groups', 'Core'] );
+    $db->Execute( $sql, [ $new_id, 'Manage Groups', 'Manage user-group existence, properties, membership', 'Core' ] );
 }
