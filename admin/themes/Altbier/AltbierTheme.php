5c60d5c3fc157c43baa660bfa5bf191c6f83ae3a
tom2tom <tpgww@onepost.net>
2020-10-09 10:04:19 +1100

    cleanup/revert license text

--- admin/themes/Altbier/AltbierTheme.php
+++ admin/themes/Altbier/AltbierTheme.php
@@ -1,22 +1,22 @@
 <?php
-# Altbier- an Admin Console theme for CMS Made Simple
-# Derived in 2018 by John Beatrice <johnnyb [AT] cmsmadesimple [DOT] org>
-# from the work provided to the community by:
-#   Goran Ilic (ja@ich-mach-das.at)
-#   Robert Campbell (calguy1000@cmsmadesimple.org)
-# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#Altbier- an Admin Console theme for CMS Made Simple
+#Derived in 2018 by John Beatrice <johnnyb [AT] cmsmadesimple [DOT] org>
+#from the work provided to the community by:
+# Goran Ilic (ja@ich-mach-das.at)
+# Robert Campbell (calguy1000@cmsmadesimple.org)
+#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 namespace CMSMS;
 
--- assets/modules/DesignManager/DesignManager.module.php
+++ assets/modules/DesignManager/DesignManager.module.php
@@ -1,20 +1,20 @@
 <?php
-# Module: DesignManager - A CMSMS addon module to provide designs management.
-# Copyright (C) 2012-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
-# Thanks to Robert Campbell and all other contributors from the CMSMS Development Team.
-# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+#Module: DesignManager - A CMSMS addon module to provide designs management.
+#Copyright (C) 2012-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+#Thanks to Robert Campbell and all other contributors from the CMSMS Development Team.
+#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License along
+#with this program. If not, see <https://www.gnu.org/licenses/>.
 
 final class DesignManager extends CMSModule
 {
