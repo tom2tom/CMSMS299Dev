f5bb295d4ca9d454a949f2325f0c99055b76b138
tom2tom <tpgww@onepost.net>
2020-10-14 15:43:43 +1100

    typo

--- lib/modules/CmsJobManager/method.install.php
+++ lib/modules/CmsJobManager/method.install.php
@@ -1,19 +1,8 @@
 <?php
-# installation-process for CmsJobManager module.
+# installation-process for CMS Made Simple module: CmsJobManager
 # Copyright (C) 2016-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
 # Thanks to Robert Campbell and all other contributors from the CMSMS Development Team.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# See license details at the top of file CmsJobManager.module.php
 
 use CMSMS\AppParams;
 use CMSMS\Database\DataDictionary;
@@ -32,7 +21,7 @@ name C(255) NOT NULL,
 module C(128),
 created I NOT NULL,
 start I UNSIGNED NOT NULL,
-until I UNSUGNED,
+until I UNSIGNED,
 recurs I(4) UNSIGNED,
 errors I(4) UNSIGNED DEFAULT 0 NOT NULL,
 data X(16383)
@@ -40,7 +29,7 @@ data X(16383)
 $sqlarray = $dict->CreateTableSQL(CmsJobManager::TABLE_NAME, $flds, $taboptarray);
 $dict->ExecuteSQLArray($sqlarray);
 
-AppParams::set('jobinterval',180); //min-seconds between job-processing's 1 .. 10
+AppParams::set('jobinterval',180); //seconds, min-gap between job-processing's 1 .. 600
 AppParams::set('jobtimeout',30); //seconds, max jobs execution-time 30 .. 1800
 
 $this->SetPreference('enabled',1); //whether async job-processing by this module is currently enabled
