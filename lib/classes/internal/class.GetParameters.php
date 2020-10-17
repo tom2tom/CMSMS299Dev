da2881ed7631b459a02840d5077a185cd4a16135
tom2tom <tpgww@onepost.net>
2020-10-17 14:41:57 +1100

    update licence & API text

--- lib/classes/internal/class.GetParameters.php
+++ lib/classes/internal/class.GetParameters.php
@@ -1,19 +1,20 @@
 <?php
-#Class to create and interpret get-parameters used in an action-URL
-#Copyright (C) 2019-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
-#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
-#
-#This program is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#(at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License
-#along with this program. If not, see <https://www.gnu.org/licenses/>.
+/*
+Class to create get-parameters for use in an URL, and retreive request parameters
+Copyright (C) 2019-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+
+CMS Made Simple is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 2 of that License, or (at your option) any later version.
+
+CMS Made Simple is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of that along with CMS Made Simple. If not, see
+<https://www.gnu.org/licenses/>.
+*/
 
 namespace CMSMS\internal;
 
@@ -25,10 +26,11 @@ use const CMS_ROOT_PATH;
 use function cms_build_query;
 
 /**
- * Class to create and interpret get-parameters used in an action-URL.
+ * Class to create get-parameters for use in an URL, and retrieve request
+ * parameters.
  * Supports plaintext and obscured parameters. The latter, if used, provide
  * a jot of protection against injection, mainly for sites without https support.
- * @since 2.3
+ * @since 2.9
  */
 class GetParameters
 {
@@ -303,9 +305,9 @@ class GetParameters
     }
 
     /**
-     * Return action-parameters interpreted from obscured parameters in the current request
+     * Return action-parameters interpreted from obscured parameters in $_REQUEST[]
      * Anything in the obscured data that is not a specific action-parameter is
-     * migrated to $_REQUEST[]. Otherwise, request parameters are ignored.
+     * migrated as-is to $_REQUEST[]. Otherwise, request parameters are ignored.
      *
      * @param bool $clear Optional flag whether to clear the cached password
      *  and processed $_REQUEST[] members. Default false.
@@ -365,7 +367,7 @@ class GetParameters
     }
 
     /**
-     * Return parameters interpreted from plaintext parameters in the current request
+     * Return parameters interpreted from plaintext parameters in $_REQUEST[]
      * Non-action parameters are ignored.
      *
      * @param bool $clear Optional flag whether to clear processed $_REQUEST[]
@@ -384,7 +386,9 @@ class GetParameters
         }
 
         if (isset($parms['id']) && $parms['id'] !== '') {
-            if (!$clear) { $tmp = $_REQUEST['mact'] ?? null; }
+            if (!$clear) {
+                $tmp = $_REQUEST['mact'] ?? null;
+            }
             unset($_REQUEST['mact']);
 
             $id = $parms['id'];
