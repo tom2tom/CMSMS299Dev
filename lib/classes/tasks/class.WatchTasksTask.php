4779c45b362a6dfc5bb8a4aa322a3e3456b1ef67
tom2tom <tpgww@onepost.net>
2020-10-22 20:23:26 +1100

    conform API change

--- lib/classes/tasks/class.WatchTasksTask.php
+++ lib/classes/tasks/class.WatchTasksTask.php
@@ -6,15 +6,15 @@ This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 
 CMS Made Simple is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 2 of that license, or
 (at your option) any later version.
 
 CMS Made Simple is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along
-with CMS Made Simple. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of that license along with CMS Made Simple.
+If not, see <https://www.gnu.org/licenses/>.
 */
 namespace CMSMS\tasks;
 
@@ -44,17 +44,19 @@ class WatchTasksTask extends CronJob
     {
         $mod = AppSingle::App()->GetJobManager();
         if ($mod) {
+			//check for changes in this same folder
             $sig = '';
             $files = scandir(__DIR__);
             foreach( $files as $file ) {
                 $fp = __DIR__.DIRECTORY_SEPARATOR.$file;
                 $sig .= filesize($fp).filemtime($fp);
             }
+			//TODO check for changed module-jobs, if event-processing is bad ...
             $sig = Crypto::hash_string($sig);
             $saved = AppParams::get(self::STATUS_SITEPREF,'');
             if ($saved != $sig) {
                 AppParams::set(self::STATUS_SITEPREF,$sig);
-                $mod->check_for_jobs_or_tasks(true);
+                $mod->refresh_jobs(true);
             }
         }
     }
