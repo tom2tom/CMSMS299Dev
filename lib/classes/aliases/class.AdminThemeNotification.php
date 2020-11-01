e13224aa01500f5e5d8c2e1b23fca4fdd002e2fe
tom2tom <tpgww@onepost.net>
2020-11-01 17:50:33 +1100

    add alias CmsAdminThemeNotification

--- lib/classes/aliases/class.AdminThemeNotification.php
+++ lib/classes/aliases/class.AdminThemeNotification.php
@@ -2,3 +2,4 @@
 assert(empty(CMS_DEPREC), new CMSMS\DeprecationNotice('class', 'CMSMS\\internal\\AdminNotification'));
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'internal'.DIRECTORY_SEPARATOR.'class.AdminNotification.php';
 class_alias('CMSMS\internal\AdminNotification', 'CMSMS\internal\AdminThemeNotification', false);
+class_alias('CMSMS\internal\AdminNotification', 'CmsAdminThemeNotification', false);
