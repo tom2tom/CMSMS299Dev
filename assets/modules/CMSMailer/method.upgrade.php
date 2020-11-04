7ff3a8658d0cae4905896d43c8f29b4f1dc93633
tom2tom <tpgww@onepost.net>
2020-11-04 18:40:53 +1100

    conform current version no.

--- assets/modules/CMSMailer/method.upgrade.php
+++ assets/modules/CMSMailer/method.upgrade.php
@@ -2,19 +2,21 @@
 /*
 CMSMailer module upgrade process
 Copyright (C) 2004-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
-This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+This CMSMailer module is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+
+This CMSMailer module is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 3 of that license, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+This CMSMailer module is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+You should have received a copy of that license along with CMS Made Simple.
+If not, see <https://www.gnu.org/licenses/>.
 */
 
 use CMSMS\AppParams;
@@ -22,7 +24,7 @@ use CMSMS\Crypto;
 
 if (!function_exists('cmsms')) exit;
 
-if (version_compare($oldversion,'6.3.0') < 0) {
+if (version_compare($oldversion,'6.0') < 0) {
     // migrate 'core' serialized-settings to this module
     $val = AppParams::get('mailprefs');
     if ($val) {
