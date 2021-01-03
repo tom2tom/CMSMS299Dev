26baebefb9bc5b579f55b68c7a0f448a33498cb4
tom2tom <tpgww@onepost.net>
2021-01-03 17:37:38 +1100

    use 2.99 API

--- lib/modules/ModuleManager/action.local_chmod.php
+++ lib/modules/ModuleManager/action.local_chmod.php
@@ -1,25 +1,25 @@
 <?php
 if( !isset($gCms) ) exit;
 if( !$this->CheckPermission('Modify Modules') ) exit;
-$this->SetCurrentTab('installed');
-if( !isset($params['mod']) ) {
-  $this->SetError($this->Lang('error_missingparam'));
-  $this->RedirectToAdminTab();
-}
-$module = get_parameter_value($params,'mod');
 
+$this->SetCurrentTab('installed');
+if( empty($params['mod']) ) {
+    $this->SetError($this->Lang('error_missingparam'));
+    $this->RedirectToAdminTab();
+}
+
+$result = false;
+$module = $params['mod'];
 $dir = cms_module_path($module);
 if( $dir ) {
-  $result = chmod_r($dir,0777); //TODO BAD PERM FOR NON-DIRS
-} else {
-  $result = false;
+    $result = recursive_chmod($dir); //default perms for dirs & others
 }
 
 if( $result ) {
-  audit('',$this->GetName(),'Changed permissions on '.$module.' directory');
-  $this->SetMessage($this->Lang('msg_module_chmod'));
+    audit('',$this->GetName(),'Changed permissions on '.$module.' directory');
+    $this->SetMessage($this->Lang('msg_module_chmod'));
 } else {
-  $this->SetError($this->Lang('error_chmodfailed'));
+    $this->SetError($this->Lang('error_chmodfailed'));
 }
 
 $this->RedirectToAdminTab();
