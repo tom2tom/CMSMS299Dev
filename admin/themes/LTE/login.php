179bd0110825b9eb40bd05f0884878345a7caf5a
tom2tom <tpgww@onepost.net>
2021-01-05 09:38:38 +1100

    update license format

--- admin/themes/LTE/login.php
+++ admin/themes/LTE/login.php
@@ -1,30 +1,29 @@
 <?php
-#------------------------------------------------------------------------
-# Plugin: AdminLTE
-# Purpose: An Admin theme for CMS Made Simple
-# Author: CMS Made Simple Development Team
-# Plugins page: https://cmsmadesimple.org
-#------------------------------------------------------------------------
+/*
+-----------------------------------------------------------------------
+Plugin: AdminLTE
+Purpose: An Admin theme for CMS Made Simple
+Author: CMS Made Simple Development Team
+Plugins page: https://cmsmadesimple.org
+------------------------------------------------------------------------
+Based on the OneEleven/Marigold theme for CMS Made Simple tm
+Equally released under GNU General Public License version 2+
+Original author: Goran Ilic
+------------------------------------------------------------------------
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-#------------------------------------------------------------------------
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
-#------------------------------------------------------------------------
-# Based on the OneEleven/Marigold theme for CMS Made Simple tm
-# Equally released under GNU General Public License version 2+
-# Original author: Goran Ilic
-#------------------------------------------------------------------------
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see http://www.gnu.org/licenses/licenses.html#GPL
+------------------------------------------------------------------------
+*/
 
 $gCms = \CmsApp::get_instance();
 $config = $gCms->GetConfig();
@@ -45,7 +44,3 @@ if ($changepwhash != '')
 
 $smarty->assign('encoding', CmsNlsOperations::get_encoding());
 $smarty->assign('config', $config);
-
-#
-# EOF
-#
