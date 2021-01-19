6133043870f8b1f14238e573eb69bfdf5b35705d
tom2tom <tpgww@onepost.net>
2021-01-19 12:53:09 +1100

    update license format

--- lib/modules/AdminSearch/action.defaultadmin.php
+++ lib/modules/AdminSearch/action.defaultadmin.php
@@ -1,20 +1,24 @@
 <?php
-# AdminSearch module action: defaultadmin
-# Copyright (C) 2012-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
-# Thanks to Robert Campbell and all other contributors from the CMSMS Development Team.
-# This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
+/*
+AdminSearch module action: defaultadmin
+Copyright (C) 2012-2021 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+Thanks to Robert Campbell and all other contributors from the CMSMS Development Team.
+
+This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+
+CMS Made Simple is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of that license, or
+(at your option) any later version.
+
+CMS Made Simple is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of that license along with CMS Made Simple.
+If not, see <https://www.gnu.org/licenses/>.
+*/
 
 use AdminSearch\tools;
 
@@ -100,7 +104,7 @@ $(function() {
       success: function (data, textStatus, jqXHR) {
        var \$el = $('#searchresults_cont');
        \$el.hide();
-	   var \$c = \$el.find('#searchresults');
+       var \$c = \$el.find('#searchresults');
        \$c.html(data);
        process_results(\$c);
        \$el.show();
