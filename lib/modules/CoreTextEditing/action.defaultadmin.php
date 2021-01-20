ceb19e6272181467f788467dc334aa1f5aa997d8
tom2tom <tpgww@onepost.net>
2021-01-20 17:32:39 +1100

    revise url sanitize

--- lib/modules/CoreTextEditing/action.defaultadmin.php
+++ lib/modules/CoreTextEditing/action.defaultadmin.php
@@ -1,21 +1,25 @@
 <?php
 /*
 CoreTextEditing module action: defaultadmin
-Copyright (C) 2018-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+Copyright (C) 2018-2021 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+
 This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
 
-This program is free software; you can redistribute it and/or modify
+CMS Made Simple is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 2 of that license, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+CMS Made Simple is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+You should have received a copy of that license along with CMS Made Simple.
+If not, see <https://www.gnu.org/licenses/>.
 */
+
+use CMSMS\Url;
 use const CoreTextEditing\Ace\ACE_CDN;
 use const CoreTextEditing\Ace\ACE_THEME;
 use const CoreTextEditing\CodeMirror\CM_CDN;
@@ -25,10 +29,11 @@ if (!isset($gCms)) exit;
 if (!$this->CheckPermission('Modify Site Preferences')) exit;
 
 if (isset($params['apply'])) {
-    $url = filter_var($params['ace_url'], FILTER_SANITIZE_URL); //TODO handle error
+	$url_ob = new Url();
+    $url = $url_ob->sanitize($params['ace_url']);
     $this->SetPreference('ace_source_url', $url);
     $this->SetPreference('ace_theme', $params['ace_theme']);
-    $url = filter_var($params['codemirror_url'], FILTER_SANITIZE_URL); //TODO handle error
+    $url = $url_ob->sanitize($params['codemirror_url']);
     $this->SetPreference('codemirror_source_url', $url);
     $this->SetPreference('codemirror_theme', $params['codemirror_theme']);
     $this->ShowMessage($this->Lang('settings_success'));
