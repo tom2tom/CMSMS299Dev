62fbd250c763d44d1cdb7e0a95a017eb0286ab5d
tom2tom <tpgww@onepost.net>
2021-01-31 19:13:04 +1100

    import code from module class

---
+++ lib/modules/AdminLogin/method.fetchpanel.php
@@ -0,0 +1,53 @@
+<?php
+/*
+AdminLogin module method to generate login-panel content
+Copyright (C) 2018-2021 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+
+This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+
+CMS Made Simple is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of that license, or
+(at your option) any later version.
+
+CMS Made Simple is distributed in the hope that it will be useful,
+BUT WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of that license along with CMS Made Simple.
+If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use CMSMS\AppSingle;
+use CMSMS\Crypto;
+
+//variables for included method
+$config = AppSingle::Config();
+$login_url = $config['admin_url'].'/login.php';
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'method.process.php';
+
+$csrf = Crypto::random_string(16, true); //encryption-grade hash not needed
+$_SESSION[$csrf_key] = $csrf;
+
+$smarty = AppSingle::Smarty();
+$tpl = $smarty->CreateTemplate($this->GetTemplateResource( 'login-form.tpl' )); //, null, null, $smarty );
+$tpl->assign([
+	'mod' => $this,
+	'actionid' => '',
+	'loginurl' => 'login.php',
+	'forgoturl' => 'login.php?forgotpw=1',
+	'csrf' => ($csrf ?? null),
+	'changepwhash' => ($changepwhash ?? ''),
+	'iserr' => !empty($errmessage),
+]);
+if (!empty($tplvars)) { $tpl->assign($tplvars); }
+$data = ['form' => $tpl->fetch()];
+//some results from the included method also for upstream
+if (!empty($tplvars)) { $data += $tplvars; }
+if (!empty($infomessage)) { $data['infomessage'] = $infomessage; }
+if (!empty($warnmessage)) { $data['warnmessage'] = $warnmessage; }
+if (!empty($errmessage)) { $data['errmessage'] = $errmessage; }
+if (!empty($changepwhash)) { $data['changepwhash'] = $changepwhash; }
+if (!empty($changepwtoken)) { $data['changepwtoken'] = $changepwtoken; }
