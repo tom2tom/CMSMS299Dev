b98c625cdf6218d509d34687c1b391b7856c0b75
tom2tom <tpgww@onepost.net>
2021-01-31 19:11:07 +1100

    rename API methods export some method content

--- lib/modules/AdminLogin/AdminLogin.module.php
+++ lib/modules/AdminLogin/AdminLogin.module.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Module: AdminLogin - standalone and theme-managed login/out
+CMSMS module: AdminLogin - supports module-managed and theme-managed login/out
 Copyright (C) 2018-2021 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
 
 This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
@@ -21,8 +21,8 @@ If not, see <https://www.gnu.org/licenses/>.
 
 use CMSMS\AppSingle;
 use CMSMS\CoreCapabilities;
-use CMSMS\Crypto;
-use CMSMS\IAuthModuleInterface;
+//use CMSMS\Crypto;
+use CMSMS\IAuthModule;
 
 /**
  * Module: admin login/out processor
@@ -30,25 +30,21 @@ use CMSMS\IAuthModuleInterface;
  * @package CMS
  * @license GPL
  */
-class AdminLogin extends CMSModule implements IAuthModuleInterface
+class AdminLogin extends CMSModule implements IAuthModule
 {
-    // minimum methods used by metafile processor
-    public function GetAuthor() { return 'Robert Campbell'; }
-    public function GetAuthorEmail() { return 'calguy1000@cmsmadesimple.org'; }
+//    public function GetAdminDescription() { return $this->Lang('publictip'); }
+//    public function GetAdminSection() { return 'extensions'; }
+    public function GetAuthor() { return ''; }
+    public function GetAuthorEmail() { return ''; }
+    public function GetChangeLog() { return ''.@file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'changelog.htm'); }
+    public function GetFriendlyName() { return $this->Lang('publicname'); }
+    public function GetHelp() { return ''.@file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'modhelp.htm'); }
     public function GetName() { return 'AdminLogin'; }
     public function GetVersion() { return '0.2'; }
     public function IsAdminOnly() { return true; }
-    public function MinimumCMSVersion() { return '2.8.900'; }
-
-    public function GetChangeLog()
-    {
-        return ''.@file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'changelog.htm');
-    }
-
-    public function GetHelp()
-    {
-        return ''.@file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'modhelp.htm');
-    }
+    public function MinimumCMSVersion() { return '2.99.0'; }
+//    public function HasAdmin() { return false; }
+//    public function VisibleToAdminUser() { return false; }
 
     public function HasCapability($capability, $params = [])
     {
@@ -61,59 +57,29 @@ class AdminLogin extends CMSModule implements IAuthModuleInterface
         }
     }
 
-    // interface methods
+    // IAuthModule interface methods
 
     /**
      * Process the current login 'phase', and generate appropriate
-     * page-content for use by caller
-     * No header / footer inclusions (js, css) are done (i.e. assumes upstream does that)
-     * @return array including login-form content and related parameters
+     * login-page-content for use by caller
+     *
+     * @return array with member 'form' comprising login-form content,
+     *  plus related parameters
      */
-    public function StageLogin() : array
+    public function fetch_login_panel() : array
     {
-        //parameters for included function
-        $usecsrf = true;
-        $config = AppSingle::Config();
-
-        $fp = cms_join_path(__DIR__, 'function.login.php');
-        require_once $fp;
-
-        $csrf = Crypto::random_string(16, true); //encryption-grade hash not needed
-        $_SESSION[$csrf_key] = $csrf;
-
-        $smarty = AppSingle::App()->GetSmarty();
-        $smarty->assign('mod', $this)
-         ->assign('actionid', '')
-         ->assign('loginurl', 'login.php')
-         ->assign('forgoturl', 'login.php?forgotpw=1')
-         ->assign('csrf', $csrf)
-         ->assign('changepwhash', $changepwhash ?? '')
-         ->assign('iserr', !empty($errmessage));
-        if (!empty($tplvars)) $smarty->assign($tplvars);
-
-        $saved = $smarty->template_dir;
-        $smarty->template_dir = __DIR__ . DIRECTORY_SEPARATOR . 'templates';
-        $data = ['form' => $smarty->fetch('core.tpl')];
-        $smarty->template_dir = $saved;
-
-        //some results from included function also for upstream
-        if (!empty($tplvars)) $data += $tplvars;
-        if (!empty($infomessage)) $data['infomessage'] = $infomessage;
-        if (!empty($warnmessage)) $data['warnmessage'] = $warnmessage;
-        if (!empty($errmessage)) $data['errmessage'] = $errmessage;
-        if (!empty($changepwhash)) $data['changepwhash'] = $changepwhash;
-        if (!empty($changepwtoken)) $data['changepwtoken'] = $changepwtoken;
-
+        $id = '';
+        require_once __DIR__ . DIRECTORY_SEPARATOR . 'method.fetchpanel.php';
         return $data;
     }
 
     /**
-     * Perform the entire login process without theme involvement
+     * Perform the current stage of the login process
      */
-    public function RunLogin()
+    public function display_login_page()
     {
-        $id = '__';
+        $id = 'm1_';
         $params = AppSingle::ModuleOperations()->GetModuleParameters($id);
-        $this->DoAction('admin_login', $id, $params, null);
+        echo $this->DoAction('login', $id, $params, null);
     }
 } // class
