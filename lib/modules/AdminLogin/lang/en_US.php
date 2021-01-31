8878ec23f8d305ba20e519d3bc55bc345b13ca28
tom2tom <tpgww@onepost.net>
2021-01-31 18:57:46 +1100

    new & revised strings

--- lib/modules/AdminLogin/lang/en_US.php
+++ lib/modules/AdminLogin/lang/en_US.php
@@ -3,19 +3,22 @@
 $lang['cancel'] = 'Cancel';
 $lang['close'] = 'Close';
 
-//$lang['error_badfield'] = 'Invalid %s given!';
+$lang['error_badchars'] = 'Invalid content in %s';
+$lang['error_badfield'] = 'Invalid %s value';
+$lang['error_csrf'] = 'Invalid security token';
 $lang['error_invalid'] = 'Unrecognised username and/or password';
-$lang['error_logintoken'] = 'Invalid login-session token';
+//$lang['error_logintoken'] = 'Invalid login-session token'; see error_csrf
+$lang['error_missingdata'] = 'Some required information was not submitted. Please fill out the form completely.';
 $lang['error_nofield'] = 'No %s given!';
 $lang['error_nomatch'] = 'Passwords do not match';
 $lang['error_nouser'] = 'User not found';
 $lang['error_passwordinvalid'] = 'The password is not suitable';
-$lang['error_sendemail'] = 'There was an error sending the email.  Contact your administrator.';
-$lang['forgotpwprompt'] = 'Enter your admin username.  After submitting, an email containing new login information will be sent to the address associated with the entered username.';
+$lang['error_sendemail'] = 'There was an error sending the email. Please notify the site\'s administrator about this.';
 $lang['goto'] = 'Go to: %s';
-$lang['info_recover'] = 'Enter your admin username.  After submitting, an email containing new login information will be sent to the address associated with the entered username.';
-$lang['info_replace'] = 'Your password has expired.  Choose and submit a replacement.'; // see also 'renewpwprompt'
+$lang['info_recover'] = 'Enter your admin username. After submitting, an email containing new login information will be sent to the address associated with the entered username.';
+$lang['info_replace'] = 'Your password has expired. Enter and submit a replacement.';
 $lang['login_info'] = 'For the admin console to work properly';
+$lang['login_info_ipandcookies'] = '<strong>Warning:</strong> some admin operations use cookies and/or track your IP address';
 $lang['login_info_params'] = <<<EOS
 <ol>
  <li>Cookies must be enabled in your browser</li>
@@ -23,6 +26,10 @@ $lang['login_info_params'] = <<<EOS
  <li>Popup windows must be allowed for:</li>
 </ol>
 EOS;
+$lang['login_info_title'] = 'Information';
+$lang['login_sitetitle'] = 'Log in to<br />%s Admin&nbsp;Console';
+//$lang['logintitle'] = 'Log in to CMS Made Simple&trade;';
+$lang['loginto'] = 'Log in to %s';
 $lang['lostpw'] = 'Forgot your password?';
 $lang['lostpwemail'] = '
 <h3>Hello</h3>
@@ -31,22 +38,25 @@ To proceed to reset the password, paste the recovery link (below) into the addre
 <p>%s</p>
 <p>If you consider this is incorrect, ignore this email.</p>';
 $lang['lostpwemailsubject'] = '[%s] Password Recovery';
-$lang['nopasswordforrecovery'] = 'No email address is recorded for this user. Password recovery is not possible. Please contact your administrator.';
+
+$lang['norecoveryaddress'] = 'No email address is recorded for this user. Password recovery is not possible. Please contact your administrator.';
 $lang['open'] = 'Open';
+
 $lang['password'] = 'Password';
 $lang['passwordagain'] = 'Password (again)';
 $lang['passwordchange'] = 'Please provide the new password';
-$lang['passwordchangedlogin'] = 'Password changed.  Please log in using the new credentials.';
+$lang['passwordchangedlogin'] = 'Password changed. Please log in using the new credentials.';
 $lang['power_by'] = 'Powered by';
+$lang['publicname'] = 'Login Processor';
+$lang['publictip'] = 'Default processor of admin-console logins';
+
 $lang['recover_start'] = 'Start recovery';
-$lang['recoveryemailsent'] = 'Email sent to recorded address.  Please check your inbox for further instructions.';
-$lang['renewpwprompt'] = 'Your password has expired. Enter and submit a replacement.'; // see also 'info_replace'
-$lang['renewpwtitle'] = 'Renew password for<br />%s'; // see also 'title_replace'
-$lang['settings_title'] = 'Login-Module Settings';
+$lang['recoveryemailsent'] = 'An email has been sent to the recorded address. Please check your inbox for further instructions.';
+
+//$lang['settings_title'] = 'Login-Module Settings';
 $lang['submit'] = 'Submit';
-$lang['title_login'] = 'Log in to<br />website Admin&nbsp;Console';
-$lang['title_login_named'] = 'Log in to<br />%s Admin&nbsp;Console';
+
 $lang['title_recover'] = 'Recover login credentials for<br />%s';
 $lang['title_replace'] = 'Renew password for<br />%s'; // see also 'renewpwtitle'
+
 $lang['username'] = 'Username';
-$lang['warn_ipandcookies'] = '<strong>Warning:</strong> some admin operations use cookies and/or track your IP address';
