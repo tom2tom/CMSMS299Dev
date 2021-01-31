b613173802ff3fe4a61241b58c7f9545cfffa4d6
tom2tom <tpgww@onepost.net>
2021-01-31 20:47:02 +1100

    create specific realm for jobs-related strings

---
+++ lib/lang/jobs/en_US.php
@@ -0,0 +1,37 @@
+<?php
+$lang['apply'] = 'Apply';
+$lang['cancel'] = 'Cancel';
+$lang['core'] = 'Core';
+$lang['created'] = 'Created';
+$lang['errors'] = 'Errors';
+$lang['frequency'] = 'Frequency';
+$lang['publicname'] = 'Background Jobs';
+$lang['info_background_jobs'] = 'This page lists information about all currently known background jobs.<br />
+Such jobs normally appear in and disappear from this list frequently.<br />
+If a job has a high error count or its next-start is not sensible, some investigation is needed.';
+//<br />
+//If no request has occurred since a job was due to start, its displayed next-start will be in the past. That job will have been been started after the end of this current request. Re-//display this page to see the job\'s actual next-start';
+$lang['info_no_jobs'] = 'There are currently no jobs in the queue';
+$lang['jobs'] = 'Jobs';
+$lang['name'] = 'Name';
+$lang['origin'] = 'Originator';
+$lang['pollgap'] = 'Polled every %s';
+$lang['processing_freq'] = 'Maximum processing frequency (seconds)';
+$lang['recur_120m'] = 'Every 2 hours';
+$lang['recur_15m'] = 'Every 15 minutes';
+$lang['recur_180m'] = 'Every 3 hours';
+$lang['recur_30m'] = 'Every 30 minutes';
+$lang['recur_daily'] = 'Daily';
+$lang['recur_hourly'] = 'Hourly';
+$lang['recur_monthly'] = 'Monthly';
+$lang['recur_weekly'] = 'Weekly';
+$lang['settings'] = 'Settings';
+$lang['start'] = 'Next Start';
+$lang['until'] = 'Until';
+
+//$lang['prompt_frequency'] = 'Minimum interval between processing of jobs (<em>minutes</em>)';
+//$lang['help_frequency'] = 'Enter a value from 1 to 10. Lower is better, but not so low that performance of the website is noticeably degraded.';
+//$lang['prompt_timelimit'] = 'Jobs timeout (<em>seconds</em>)';
+//$lang['help_timelimit'] = 'Enter a value from 2 to 120. This is akin to PHP’s maximum execution time setting.';
+//$lang['prompt_joburl'] = 'Custom URL for job processing';
+//$lang['help_joburl'] = 'Enter a suitable URL to replace the default internal URL, if that cannot be used. Leave blank to use the default.';
