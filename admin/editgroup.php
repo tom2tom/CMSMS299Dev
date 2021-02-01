fdd49bc0e6b45f181072db886c53b66ca64b6912
tom2tom <tpgww@onepost.net>
2021-02-01 18:06:52 +1100

    sanitize inputs

--- admin/editgroup.php
+++ admin/editgroup.php
@@ -1,23 +1,28 @@
 <?php
-#procedure to modify a users-group
-#Copyright (C) 2004-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
-#Thanks to Ted Kulp and all other contributors from the CMSMS Development Team.
-#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
-#
-#This program is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#(at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License
-#along with this program. If not, see <https://www.gnu.org/licenses/>.
+/*
+Procedure to modify a users-group
+Copyright (C) 2004-2021 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+Thanks to Ted Kulp and all other contributors from the CMSMS Development Team.
+
+This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+
+CMS Made Simple is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of that license, or
+(at your option) any later version.
+
+CMS Made Simple is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of that license along with CMS Made Simple.
+If not, see <https://www.gnu.org/licenses/>.
+*/
 
 use CMSMS\AppSingle;
 use CMSMS\AppState;
+use CMSMS\Error403Exception;
 use CMSMS\Events;
 use CMSMS\Group;
 use CMSMS\UserOperations;
@@ -48,8 +53,7 @@ $userid = get_userid();
 $themeObject = Utils::get_theme_object();
 
 if (!check_permission($userid, 'Manage Groups')) {
-//TODO some pushed popup c.f. javascript:cms_notify('error', lang('no_permission') OR lang('needpermissionto', lang('perm_Manage_Groups')), ...);
-    return;
+    throw new Error403Exception(lang('permissiondenied')); // OR display error.tpl ?
 }
 
 $groupobj = new Group();
@@ -58,20 +62,27 @@ if( $group_id > 0 ) {
 }
 
 if (isset($_POST['editgroup'])) {
+    $errors = [];
+    cms_specialchars_decode_array($_POST);
     $group_id = (int)$_POST['group_id'];
-    $description = trim(cleanValue($_POST['description']));
+
+    $tmp = trim($_POST['group']);
+    $group = sanitizeVal($tmp, 1); // OR 21 ?
+    if ($group !== $tmp) {
+        $errors[] = lang('illegalcharacters', lang('groupname'));
+    } elseif (!$group) {
+        $errors[] = lang('nofieldgiven', lang('groupname'));
+    }
+    // not compulsory
+    $description = trim($_POST['description']); // AND sanitizeVal(, 0) ? nl2br() ? striptags() ?
+
     if ($group_id != 1) {
-        $active = (isset($_POST['active'])) ? 1 : 0;
+        $active = (!empty($_POST['active'])) ? 1 : 0;
+    } else {
+        $active = 1;
     }
 
-    $validinfo = true;
-    $group = trim(cleanValue($_POST['group']));
-    if ($group == '') {
-        $validinfo = false;
-        $themeObject->RecordNotice('error', lang('nofieldgiven', lang('groupname')));
-    }
-
-    if ($validinfo) {
+    if (!$errors) {
         $groupobj->name = $group;
         $groupobj->description = $description;
         $groupobj->active = $active;
@@ -81,15 +92,22 @@ if (isset($_POST['editgroup'])) {
             // put mention into the admin log
             audit($groupobj->id, 'Admin User Group: '.$groupobj->name, 'Edited');
             redirect('listgroups.php'.$urlext);
-            return;
         } else {
-            $themeObject->RecordNotice('error', lang('errorupdatinggroup'));
+            $errors[] = lang('errorupdatinggroup');
         }
     }
+    $themeObject->RecordNotice('error', $errors);
+
+    $group = cms_specialchars($group);
+    if ($description) $description = cms_specialchars($description);
 } elseif ($group_id != -1) {
-    $group = $groupobj->name;
-    $description = $groupobj->description;
+    $group = cms_specialchars($groupobj->name);
+    $description = cms_specialchars($groupobj->description);
     $active = $groupobj->active;
+} else { // id == -1 should never happen when editing ?
+    $group = '';
+    $description = '';
+    $active = 1;
 }
 
 $selfurl = basename(__FILE__);
@@ -110,6 +128,7 @@ $smarty->assign([
 ]);
 
 $content = $smarty->fetch('editgroup.tpl');
-require './header.php';
+$sep = DIRECTORY_SEPARATOR;
+require ".{$sep}header.php";
 echo $content;
-require './footer.php';
+require ".{$sep}footer.php";
