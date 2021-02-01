ceb190c846441f9e91a82d0c0c704e16eb80593b
tom2tom <tpgww@onepost.net>
2021-02-01 17:58:04 +1100

    update security checks

--- lib/classes/internal/class.LoginOperations.php
+++ lib/classes/internal/class.LoginOperations.php
@@ -1,21 +1,24 @@
 <?php
-#Singleton class of login methods
-#Copyright (C) 2016-2020 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
-#Thanks to Robert Campbell and all other contributors from the CMSMS Development Team.
-#This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
-#
-#This program is free software; you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation; either version 2 of the License, or
-#(at your option) any later version.
-#
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License
-#along with this program. If not, see <https://www.gnu.org/licenses/>.
+/*
+Singleton class of login methods
+Copyright (C) 2016-2021 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
+Thanks to Robert Campbell and all other contributors from the CMSMS Development Team.
 
+This file is a component of CMS Made Simple <http://www.cmsmadesimple.org>
+
+CMS Made Simple is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of that license, or
+(at your option) any later version.
+
+CMS Made Simple is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of that license along with CMS Made Simple.
+If not, see <https://www.gnu.org/licenses/>.
+*/
 namespace CMSMS\internal;
 
 use CMSMS\AppParams;
@@ -37,7 +40,7 @@ final class LoginOperations
 	/* *
 	 * @ignore
 	 */
-//    private static $_instance = null;
+//	private static $_instance = null;
 	/**
 	 * @ignore
 	 */
@@ -52,7 +55,7 @@ final class LoginOperations
 	 */
 	public function __construct()
 	{
-		if (!isset($this->_loginkey)) $this->_loginkey = $this->_get_salt();
+		if (!isset($this->_loginkey)) $this->_loginkey = $this->get_salt();
 	}
 
 	/**
@@ -62,7 +65,7 @@ final class LoginOperations
 
 	/**
 	 * Get the singleton instance of this class.
-	 * @deprecated since 2.9 instead use CMSMS\AppSingle::LoginOperations()
+	 * @deprecated since 2.99 instead use CMSMS\AppSingle::LoginOperations()
 	 * @return self i.e. LoginOperations
 	 */
 	public static function get_instance() : self
@@ -74,7 +77,7 @@ final class LoginOperations
 	public function deauthenticate()
 	{
 		(new SignedCookieOperations())->erase($this->_loginkey);
-		unset($_SESSION[$this->_loginkey],$_SESSION[CMS_USER_KEY]);
+		unset($_SESSION[$this->_loginkey], $_SESSION[CMS_USER_KEY]);
 	}
 
 	/**
@@ -90,28 +93,24 @@ final class LoginOperations
 		if ($user->id < 1 || empty($user->password)) {
 			throw new RuntimeException('User information invalid for '.__METHOD__);
 		}
-		$data = get_object_vars($user);
-		unset($data['password']); //actual (maybe-changed) P/W hash not useful here
-//		$data[] = __FILE__; kinda salt
-
 		$private_data = [
 		'uid' => $user->id,
 		'username' => $user->username,
 		'eff_uid' => null,
 		'eff_username' => null,
-		'hash' => Crypto::hash_string(json_encode($data, JSON_PARTIAL_OUTPUT_ON_ERROR)), //for validation
+		'hash' => $user->password,
 		];
-		//From 2.9 - changing to super-admin-user (1) is not supported
+		//From 2.99 - changing to super-admin-user (1) is not supported
 		if ($effective_user && $effective_user->id != $user->id && $effective_user->id > 1) {
 			$private_data['eff_uid'] = $effective_user->id;
 			$private_data['eff_username'] = $effective_user->username;
 		}
 
-		$pw = hash_hmac('tiger128,3', $this->_get_salt(), AppSingle::Config()['db_password']);
+		$pw = hash_hmac('tiger128,3', $this->get_salt(), AppSingle::Config()['db_password']);
 		$enc = Crypto::encrypt_string(json_encode($private_data, JSON_PARTIAL_OUTPUT_ON_ERROR), $pw);
-		$hash = Crypto::hash_string($this->_get_salt() . $enc);
+		$hash = Crypto::hash_string($this->get_salt() . $enc);
 		$_SESSION[$this->_loginkey] = $hash . '::' . $enc;
-		(new SignedCookieOperations())->set($this->_loginkey, base64_encode($enc)); // handle all $enc chars
+		(new SignedCookieOperations())->set($this->_loginkey, base64_encode($enc), time()+84600); // handle all $enc chars
 		$this->_data = null;
 		return true;
 	}
@@ -119,6 +118,26 @@ final class LoginOperations
 	public function create_csrf_token() : string
 	{
 		return Crypto::random_string(16, true);
+	}
+
+	/**
+	 * Get current or newly-generated salt
+	 * @since 2.99 access public
+	 * @return string
+	 */
+	public function get_salt() : string
+	{
+		if (!AppState::test_state(AppState::STATE_INSTALL)) {
+			$salt = AppParams::get('loginsalt');
+			if (!$salt) {
+				$salt = $this->create_csrf_token();
+				AppParams::set('loginsalt', $salt);
+				SysDataCache::get_instance()->release('site_preferences');
+			}
+			return $salt;
+		} else {  //must avoid siteprefs circularity
+			return $this->create_csrf_token();
+		}
 	}
 
 	/**
@@ -168,21 +187,6 @@ final class LoginOperations
 		}
 	}
 
-	/**
-	 * Check whether the current user's password-lifetime has expired
-	 * @since 2.9
-	 * @param mixed $user User object | null to process current user
-	 * @return bool
-	 */
-	public function pass_expired($user = null) : bool
-	{
-		if (!$user) {
-			$user = $this->get_loggedin_user();
-		}
-		//TODO check now >= users-table :: passmodified_date + no. of days policy
-		return false;
-	}
-
 	/*
 	 * Effective user support
 	 * If an effective-user has been recorded in the session, AND the primary/
@@ -210,7 +214,7 @@ final class LoginOperations
 	/**
 	 * Get login-name for the session 'effective' user (if any) or else for the primary user
 	 *
-	 * @return mixed int | null
+	 * @return mixed string | null
 	 */
 	public function get_effective_username()
 	{
@@ -223,33 +227,21 @@ final class LoginOperations
 		}
 	}
 
-	public function set_effective_user(User $e_user = null)
+	/**
+	 * Change the current admin user.
+	 * @since 2.99 this works only if the current user is a member of the superusers group
+	 *
+	 * @param User $e_user
+	 */
+	public function set_effective_user(User $e_user)
 	{
 		if ($e_user) {
 			$li_user = $this->get_loggedin_user();
 			if ($e_user->id != $li_user->id) {
-				//TODO check vulnerability risk
-				$this->save_authentication($li_user, $e_user);
+				if (AppSingle::UserOperations()->UserInGroup($li_user->id, 1)) {
+					$this->save_authentication($li_user, $e_user);
+				}
 			}
-		}
-	}
-
-	/**
-	 * Get current or newly-generated salt
-	 * @ignore
-	 */
-	private function _get_salt() : string
-	{
-		if (!AppState::test_state(AppState::STATE_INSTALL)) {
-			$salt = AppParams::get('loginsalt');
-			if (!$salt) {
-				$salt = $this->create_csrf_token();
-				AppParams::set('loginsalt', $salt);
-				SysDataCache::get_instance()->release('site_preferences');
-			}
-			return $salt;
-		} else {  //must avoid siteprefs circularity
-			return $this->create_csrf_token();
 		}
 	}
 
@@ -261,23 +253,12 @@ final class LoginOperations
 	 */
 	private function _check_passhash(int $uid, string $hash) : bool
 	{
-		// we already validated that payload was not corrupt
+		// we already confirmed that the payload is not corrupt
 		$user = AppSingle::UserOperations()->LoadUserByID($uid);
-		if (!$user) {
-			return false;
+		if (!$user || !$user->active) {
+			return hash_equals(__DIR__, __FILE__); // waste some time
 		}
-		if (!$user->active) {
-			return false;
-		}
-		if (!$hash) {
-			return false;
-		}
-
-		$data = get_object_vars($user);
-		unset($data['password']);
-//		$data[] = __FILE__;
-
-		return hash_equals(Crypto::hash_string(json_encode($data, JSON_PARTIAL_OUTPUT_ON_ERROR)), $hash);
+		return hash_equals($user->password, $hash);
 	}
 
 	// @return mixed array | null : previously- or currently-generated data
@@ -298,18 +279,20 @@ final class LoginOperations
 		if (!$private_data) {
 			return;
 		}
-		$pw = hash_hmac('tiger128,3', $this->_get_salt(), AppSingle::Config()['db_password']);
+		$pw = hash_hmac('tiger128,3', $this->get_salt(), AppSingle::Config()['db_password']);
 		if ($split) {
 			$parts = explode('::', $private_data, 2);
 			if (count($parts) != 2) {
 				return;
 			}
-			if ($parts[0] != Crypto::hash_string($this->_get_salt() . $parts[1])) {
+			if ($parts[0] != Crypto::hash_string($this->get_salt() . $parts[1])) {
 				return; // payload corrupted
 			}
 			$private_data = json_decode(Crypto::decrypt_string($parts[1], $pw), true);
 		} else {
-			$private_data = base64_decode(json_decode(Crypto::decrypt_string($private_data, $pw), true));
+//			$private_data = json_decode(Crypto::decrypt_string(base64_decode($private_data), $pw), true);
+			$s1 = base64_decode($private_data);
+			$private_data = json_decode(Crypto::decrypt_string($s1, $pw), true);
 		}
 
 		if (!is_array($private_data)) {
@@ -325,7 +308,7 @@ final class LoginOperations
 			return;
 		}
 
-		// authenticate the passhash
+		// authenticate
 		if (/*!AppSingle::App()->is_frontend_request() && */!$this->_check_passhash($private_data['uid'], $private_data['hash'])) {
 			return;
 		}
