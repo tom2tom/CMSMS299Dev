08fe35cb44658cbd407cedb986c9bc8d85de497b
tom2tom <tpgww@onepost.net>
2021-05-27 22:10:16 +1000

    new & revised strings

--- lib/modules/AdminSearch/lang/en_US.php
+++ lib/modules/AdminSearch/lang/en_US.php
@@ -7,8 +7,11 @@ $lang = [
 'desc_css_search' => 'Search for matching text in stylesheets',
 'desc_content_search' => 'Search for matching text in content pages',
 'desc_filter_all' => 'Toggle all filters',
-'desc_oldmodtemplate_search' => 'Search old module templates',
+'desc_modtemplate_search' => 'Search for matching text in module templates',
+'desc_save_search' => 'Record current selections for use next time',
 'desc_template_search' => 'Search for matching text in templates',
+'desc_udt_search' => 'Search for matching text in User Defined Tags',
+'desc_verbatim_search' => 'Do not try to convert UTF8-incompatible characters (if any), before searching',
 
 // E
 'error_nosearchtext' => 'Please enter a search term',
@@ -23,13 +26,13 @@ $lang = [
 
 // H
 'help' => <<<'EOS'
-<h3>What does this do?</h3>
-<p>This module provides the ability to quickly find places in templates, content pages, and other database where a text string occurs.  It is particularly useful for finding smarty tags, class names, ids or other bits of HTML code that might be hard to find in a large website.</p>
-<p>This module has no frontend interaction it is designed for use by CMSMS site developers or editors to find sub strings of text or code.  Not for use in finding text on the frontend of websites.</p>
+<h3>What does this module do?</h3>
+<p>It provides the ability to quickly find places in templates, content pages, and other database tables where a text string occurs. It is particularly useful for finding smarty tags, class names, ids or other bits of content that might be hard to find in a large website.</p>
+<p>This module is designed for use by CMSMS site developers or editors to find sub strings of text or code. The module is not for frontend use on websites, it has no frontend interaction.</p>
 <h3>How is it used</h3>
-<p>This module is visible to most administrators of the website with at least some permissions to edit templates, stylesheets, or some content   Though the list of what can be searched might be reduced.</p>
-<p>The module provides a text field where a single string can be entered (the string is not divided into words or otherwise parsed).  It also provides the ability to only search certain subsections of the website.</p>
-<p>Searching will generate a nested, expandable list of sections where matches were found.  Under each section a description of the match is displayed.  Usually with a link that will direct you to a form to edit the item.</p>
+<p>This module is visible to administrators of the website with at least some permissions to edit templates, stylesheets, or some content. Though the list of what can be searched might be reduced.</p>
+<p>The module provides a text field where a single string can be entered (the string is not divided into words or otherwise parsed). It also provides the ability to only search certain subsections of the website.</p>
+<p>Searching will generate a nested, expandable list of sections where matches were found. Under each section a description of the match is displayed. Usually with a link that will direct you to a form to edit the item.</p>
 <h3>Support</h3>
 <p>As per the GPL, this software is provided as-is. Please read the text of the license for the full disclaimer.</p>
 <h3>Copyright and License</h3>
@@ -39,13 +42,16 @@ EOS
 ,
 
 // L
-'lbl_cased_search' => 'Search is case-sensitive',
+'lbl_cased_search' => 'Search is Case-Sensitive',
 'lbl_content_search' => 'Content Pages',
 'lbl_css_search' => 'Stylesheets',
-'lbl_gcb_search' => 'Global Content Blocks', //deprecated - UDT's?
-'lbl_oldmodtemplate_search' => 'Module Templates',
+//'lbl_gcb_search' => 'Global Content Blocks', //deprecated - among other-templates?
+'lbl_modtemplate_search' => 'Module Templates',
+'lbl_save_search' => 'Save Parameters',
 'lbl_search_desc' => 'Descriptions <em>(where applicable)</em>',
-'lbl_template_search' => 'Templates',
+'lbl_template_search' => 'Other Templates',
+'lbl_udt_search' => 'User Plugins',
+'lbl_verbatim_search' => 'Search Text as Provided',
 
 // M
 'moddescription' => 'Search the website database for specified text',
@@ -64,7 +70,7 @@ EOS
 'search_text' => 'Search Text',
 'search_typed' => 'Search %s',
 'search_results' => 'Search Results',
-'sectiondesc_oldmodtemplates' => 'Results in this section are not clickable, as each module provides its own Admin panel interface for editing templates',
+'sectiondesc_modtemplates' => 'Results in this section are not clickable, as each module provides its own Admin panel interface for editing templates',
 'settings' => 'Settings',
 'starting' => 'Starting',
 
